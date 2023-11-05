a780b230fb39b28a0892b2ae07faf3405b8caf42
junha <goodjunha@gmail.com>
2023-11-05 19:10:06 +0900

    🚫:: #3 필요없는 주석 삭제

--- Projects/Modules/DSKit/Demo/Sources/Demo+/UIFont+.swift
+++ Projects/Modules/DSKit/Demo/Sources/Demo+/UIFont+.swift
@@ -1,11 +1,3 @@
-//
-//  UIFont+.swift
-//  DSKitDemoApp
-//
-//  Created by Junho Lee on 2022/11/27.
-//  Copyright © 2022 SOPT-Stamp-iOS. All rights reserved.
-//
-
 import UIKit
 
 extension UIFont {
