451987477a32b10700d1d95dff18046b03a36ece
junha <goodjunha@gmail.com>
2023-11-06 11:40:25 +0900

    ⚙️:: [#12] 디자인 시스템 font 추가

--- Projects/Modules/DSKit/Sources/UIFont+.swift
+++ Projects/Modules/DSKit/Sources/UIFont+.swift
@@ -1,12 +1,41 @@
-//
-//  UIFont+.swift
-//  DSKit
-//
-//  Created by 양수빈 on 2022/10/13.
-//  Copyright © 2022 SOPT-Stamp-iOS. All rights reserved.
-//
-
 import UIKit
 
 extension UIFont {
+    public struct Pretendard {
+         @nonobjc public static var headlineLarge: UIFont {
+             return DSKitFontFamily.Pretendard.extraBold.font(size: 64.0)
+         }
+         
+         @nonobjc public static var headlineMedium: UIFont {
+             return DSKitFontFamily.Pretendard.bold.font(size: 48.0)
+         }
+        
+        @nonobjc public static var headlineSmall: UIFont {
+            return DSKitFontFamily.Pretendard.semiBold.font(size: 40.0)
+        }
+        
+        @nonobjc public static var titleLarge: UIFont {
+            return DSKitFontFamily.Pretendard.semiBold.font(size: 36.0)
+        }
+        
+        @nonobjc public static var titleMedium: UIFont {
+            return DSKitFontFamily.Pretendard.semiBold.font(size: 24.0)
+        }
+        
+        @nonobjc public static var titleSmall: UIFont {
+            return DSKitFontFamily.Pretendard.semiBold.font(size: 16.0)
+        }
+        
+        @nonobjc public static var bodyLarge: UIFont {
+            return DSKitFontFamily.Pretendard.regular.font(size: 20.0)
+        }
+        
+        @nonobjc public static var bodyMedium: UIFont {
+            return DSKitFontFamily.Pretendard.regular.font(size: 16.0)
+        }
+        
+        @nonobjc public static var bodySmall: UIFont {
+            return DSKitFontFamily.Pretendard.regular.font(size: 14.0)
+        }
+     }
 }
