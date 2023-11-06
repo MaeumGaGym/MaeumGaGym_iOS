0a2d72c10265123600db802c2b88e7c2e6701c6a
junha <goodjunha@gmail.com>
2023-11-06 14:46:12 +0900

    ⚓️:: [#12] AppFlow 추가

--- Projects/Features/RootFeature/Sources/AppFlow.swift
+++ Projects/Features/RootFeature/Sources/AppFlow.swift
@@ -15,39 +15,44 @@ import UIKit
 
 public class AppFlow: Flow {
     
-    var window: UIWindow
-    
     public var root: Presentable {
-        return self.window
+        return self.rootViewController
     }
-    
-    public init(window: UIWindow) {
-        self.window = window
-    }
-    
+
+    private lazy var rootViewController: UITabBarController = {
+        let tabBarController = UITabBarController()
+        tabBarController.tabBar.tintColor = UIColor.black
+        return tabBarController
+    }()
+
     public func navigate(to step: Step) -> FlowContributors {
         guard let step = step as? AppStep else { return .none }
+
         switch step {
-        case .loginIsRequired:
-            return self.navigateToLogin()
-        case .homeIsRequired:
-            return self.navigateToHome()
+        case .tabBarIsRequired:
+            return navigateToTabBarScreen()
+        default:
+            return .none
         }
     }
-    
-    private func navigateToLogin() -> FlowContributors {
-        let loginFlow = LoginFlow()
-        Flows.use(loginFlow, when: .created) { (root) in
-            self.window.rootViewController = root
-        }
-        return .one(flowContributor: .contribute(withNextPresentable: loginFlow, withNextStepper: OneStepper(withSingleStep: AppStep.loginIsRequired)))
-    }
-    
-    private func navigateToHome() -> FlowContributors {
+
+    private func navigateToTabBarScreen() -> FlowContributors {
         let homeFlow = HomeFlow()
-        Flows.use(homeFlow, when: .created) { (root) in
-            self.window.rootViewController = root
+        let postureFlow = PostureFlow()
+        let selfCareFlow = SelfCareFlow()
+        let shopFlow = ShopFlow()
+
+        Flows.whenReady(flow1: homeFlow, flow2: postureFlow, flow3: selfCareFlow, flow4: shopFlow) { [unowned self] homeRoot, postureRoot, selfCareRoot, shopRoot in
+            self.rootViewController.viewControllers = [homeRoot, postureRoot, selfCareRoot, shopRoot]
         }
-        return .one(flowContributor: .contribute(withNextPresentable: homeFlow, withNextStepper: OneStepper(withSingleStep: AppStep.homeIsRequired)))
+
+        return .multiple(flowContributors: [.contribute(withNextPresentable: homeFlow, withNextStepper: OneStepper(withSingleStep: AppStep.homeIsRequired)),
+                                            .contribute(withNextPresentable: postureFlow, withNextStepper: OneStepper(withSingleStep: AppStep.postureIsRequired)),
+                                            .contribute(withNextPresentable: selfCareFlow, withNextStepper: OneStepper(withSingleStep: AppStep.selfCareIsRequired)),
+                                             .contribute(withNextPresentable: shopFlow, withNextStepper: OneStepper(withSingleStep: AppStep.shopIsRequired))])
+    }
+    
+    public init() {
+        
     }
 }
