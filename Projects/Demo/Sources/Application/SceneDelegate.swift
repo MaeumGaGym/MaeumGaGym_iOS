c3eb17060be8f4187917efbcce6259cda742b11c
junha <goodjunha@gmail.com>
2023-11-06 14:21:20 +0900

    ⚓️:: [#12] Demo 앱델리, 신델리 추가

--- Projects/Demo/Sources/Application/SceneDelegate.swift
+++ Projects/Demo/Sources/Application/SceneDelegate.swift
@@ -6,22 +6,22 @@
 //
 
 import UIKit
+import RxFlow
+import RootFeature
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
+    var coordinator = FlowCoordinator()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }        
-        let mainViewController = UIViewController()
-        
-        let navigationController = UINavigationController(rootViewController: mainViewController)
-
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
-        window?.windowScene = windowScene
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        let appFlow = AppFlow(window: window)
+        self.coordinator.coordinate(flow: appFlow, with: AppStepper())
+        window.makeKeyAndVisible()
     }
         
     func sceneDidDisconnect(_ scene: UIScene) {
