9734d657eae7692cef9e1dd3cb529b8da1b23590
junha <goodjunha@gmail.com>
2023-11-06 14:45:59 +0900

    ⚓️:: [#12] SelfCareFlow 추가

---
+++ Projects/Features/RootFeature/Sources/SelfCareFlow.swift
@@ -0,0 +1,23 @@
+import UIKit
+import RxFlow
+import RxSwift
+import RxCocoa
+
+class SelfCareFlow: Flow {
+    var root: Presentable {
+        return self.rootViewController
+    }
+
+    private let rootViewController = UINavigationController()
+
+    init() {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .white
+        viewController.tabBarItem = UITabBarItem(title: "SelfCare", image: UIImage(systemName: "gearshape"), selectedImage: nil)
+        rootViewController.setViewControllers([viewController], animated: false)
+    }
+
+    func navigate(to step: Step) -> FlowContributors {
+        return .none
+    }
+}
