fd9834ac5b5f67f508cd09b532b97dd86d2f9db6
junha <goodjunha@gmail.com>
2023-11-06 14:21:55 +0900

    ⚓️:: [#12] login 코드 추가

---
+++ Projects/Features/AuthFeature/Sources/SignInScene/ViewModel/LoginViewModel.swift
@@ -0,0 +1,30 @@
+import Foundation
+import RxFlow
+import RxCocoa
+import RxSwift
+import Core
+
+public class LoginViewModel: BaseViewModel, Stepper {
+    
+    public struct Input {
+        let loginButtonTap: Observable<Void>
+    }
+    
+    public struct Output {
+        let clickedMessage: Driver<String>
+    }
+    
+    func transform(input: Input) -> Output {
+        let clickedMessage = input.loginButtonTap.map { _ in
+            return "클릭됨"
+        }
+        .asDriver(onErrorJustReturn: "")
+        
+        return Output(clickedMessage: clickedMessage)
+    }
+    
+    func loginButtonTapped() {
+         let nextStep = AppStep.homeIsRequired
+         self.steps.accept(nextStep)
+     }
+}
