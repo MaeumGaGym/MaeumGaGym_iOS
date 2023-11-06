e23b25a88a4ec5e0c22c851754fceebbb86ed0d5
junha <goodjunha@gmail.com>
2023-11-06 09:15:48 +0900

    ⚓️:: #7 TokenManager 모듈 추가

---
+++ Projects/Modules/TokenManager/Project.swift
@@ -0,0 +1,18 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 박준하 on 11/6/23.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project.makeModule(
+    name: "TokenManager",
+    targets: [.unitTest, .staticFramework],
+    internalDependencies: [
+        .Modules.csLogger
+    ]
+)
