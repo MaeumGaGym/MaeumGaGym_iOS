b029644f350a1aa8aeb9448c827309fdcfd19256
junha <goodjunha@gmail.com>
2023-11-06 14:44:43 +0900

    🚫:: [#12] 안쓰는 코드 삭제

--- Projects/Features/HomeFeature/Sources/HomeScene/View/HomeViewController.swift
+++ Projects/Features/HomeFeature/Sources/HomeScene/View/HomeViewController.swift
@@ -9,29 +9,9 @@ public class HomeViewController: UIViewController, Stepper {
 
     public var steps = PublishRelay<Step>()
     
-    var button: UIButton = {
-        let button = UIButton()
-        button.setTitle("로그아웃", for: .normal)
-        button.backgroundColor = .systemRed
-        button.addTarget(self, action: #selector(logoutButtonDidTap), for: .touchUpInside)
-        return button
-    }()
-    
     public override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Home"
         self.view.backgroundColor = .systemBackground
-
-        self.view.addSubview(self.button)
-        
-        self.button.snp.makeConstraints {
-            $0.centerX.centerY.equalToSuperview()
-            $0.height.equalTo(100)
-        }
-    }
-    
-    @objc
-    func logoutButtonDidTap() {
-        self.steps.accept(AppStep.loginIsRequired)
     }
 }
