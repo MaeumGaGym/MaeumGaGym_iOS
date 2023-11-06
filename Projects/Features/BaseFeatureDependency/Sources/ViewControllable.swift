fc22372b95d2cd95c35256598bd03eeb6767df2f
junha <goodjunha@gmail.com>
2023-11-06 14:53:40 +0900

    🍗:: [#12] BaseFeatureDependency 리펙토링

--- Projects/Features/BaseFeatureDependency/Sources/ViewControllable.swift
+++ Projects/Features/BaseFeatureDependency/Sources/ViewControllable.swift
@@ -1,11 +1,3 @@
-//
-//  ViewControllable.swift
-//  BaseFeatureDependency
-//
-//  Created by 김영인 on 2023/03/16.
-//  Copyright © 2023 SOPT-iOS. All rights reserved.
-//
-
 import UIKit
 
 public protocol ViewControllable {
