faa4e5ba1f4e99fc9a3ffc7b005f06fe80f5c576
s <tylereunholee010203@gmail.com>
2023-11-21 22:00:42 +0900

    ⚓️ :: [#32] 커스텀 라인 제작

--- Projects/Modules/DSKit/Sources/Components/MaeumGaGymDecorate/Auth/MaeumGaGymLine.swift
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymDecorate/Auth/MaeumGaGymLine.swift
@@ -1,9 +1,34 @@
-//
-//  MaeumGaGymLine.swift
-//  DSKit
-//
-//  Created by 이은호 on 11/21/23.
-//  Copyright © 2023 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
+import Then
+import SnapKit
 
-import Foundation
+open class MaeumGaGymLine: UIView {
+    private let lineView = UIView()
+    
+    public init(
+        lineColor: UIColor? = DSKitAsset.Colors.gray50.color,
+        lineWidth: Double? = 390.0,
+        lineHeight: Double? = 2.0
+    ) {
+        super.init(frame: .zero)
+        
+        lineView.backgroundColor = lineColor
+        snp.makeConstraints {
+            $0.width.equalTo(lineWidth!)
+            $0.height.equalTo(lineHeight!)
+        }
+        
+        setupContraints()
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupContraints() {
+        addSubview(lineView)
+        lineView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
