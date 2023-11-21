59042fa1775658b293519cd808e46b6d1bb02464
s <tylereunholee010203@gmail.com>
2023-11-21 22:01:35 +0900

    ⚓️ :: [#32] line VC 추가

--- Projects/Modules/DSKit/Demo/Sources/DemoViewController/Decorate/Auth/lineViewController.swift
+++ Projects/Modules/DSKit/Demo/Sources/DemoViewController/Decorate/Auth/lineViewController.swift
@@ -1,9 +1,34 @@
-//
-//  lineViewController.swift
-//  DSKit
-//
-//  Created by 이은호 on 11/21/23.
-//  Copyright © 2023 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
+import SnapKit
+import Then
 
-import Foundation
+open class LineViewController: UIViewController {
+    
+    private let decorateLine1 = MaeumGaGymLine(lineColor: DSKitAsset.Colors.gray50.color, lineWidth: 390.0, lineHeight: 2.0)
+    private let decorateLine2 = MaeumGaGymLine(lineColor: DSKitAsset.Colors.red700.color, lineWidth: 390.0, lineHeight: 10.0)
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        layout()
+    }
+    
+    func layout() {
+        [
+            decorateLine1,
+            decorateLine2,
+        ].forEach { view.addSubview($0) }
+        
+        decorateLine1.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20.0)
+            $0.leading.equalToSuperview().offset(20.0)
+            $0.trailing.equalToSuperview().inset(20.0)
+        }
+        
+        decorateLine2.snp.makeConstraints {
+            $0.top.equalTo(decorateLine1.snp.bottom).offset(50.0)
+            $0.leading.equalToSuperview().offset(20.0)
+            $0.trailing.equalToSuperview().inset(20.0)        }
+    }
+}
