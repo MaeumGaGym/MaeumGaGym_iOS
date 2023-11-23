b907abe9296a2107ce7ff2f73448ba19a8c29866
박준하 <102890390+jjunhaa0211@users.noreply.github.com>
2023-11-23 10:36:53 +0900

    ⚓️:: [#34] 캐시코드 추가

---
+++ Projects/Features/PickleFeature/Sources/Pickle/PickleImagePrefetcher.swift
@@ -0,0 +1,58 @@
+import UIKit
+import AVFoundation
+
+final class PickleImagePrefetcher: PicklePrefetcher {
+    
+    var prefetchURLs: [URL] = []
+    var completedItems: [Response] = []
+    var failedErrors: [Error] = []
+    var isTasking: Bool = false
+    var queue: DispatchQueue = .init(label: "video.reels.prefetch")
+    var pendingQueue: PriorityQueue<PendingItem> = {
+        let queue = PriorityQueue<PendingItem> { (lhs: PendingItem, rhs: PendingItem) -> Bool in
+            return lhs > rhs
+        }
+        return queue
+    }()
+    var priorityCount = 1
+    private var asset: AVAsset?
+    
+    func load(url: URL, completion: @escaping (Response) -> Void) {
+        guard PickleCacheProvider.shared.getImage(url: url) == nil else {
+            completion(.fail(FetchError.image))
+            return
+        }
+        
+        self.asset = AVAsset(url: url)
+        self.asset?.generateThumbnail { [weak self] (image: UIImage?) in
+            self?.asset = nil
+            if let image = image {
+                PickleCacheProvider.shared.saveImage(url: url, image: image)
+                completion(.success)
+                return
+            }
+            completion(.fail(FetchError.image))
+        }
+    }
+}
+
+
+private extension AVAsset {
+    func generateThumbnail(completion: @escaping (UIImage?) -> Void) {
+        let imageGenerator = AVAssetImageGenerator(asset: self)
+        let time = CMTime(seconds: 0.0, preferredTimescale: 600)
+        let times = [NSValue(time: time)]
+        imageGenerator.appliesPreferredTrackTransform = true
+        imageGenerator.generateCGImagesAsynchronously(
+            forTimes: times,
+            completionHandler: { (_, image, _, _, _) in
+                if let image = image {
+                    completion(UIImage(cgImage: image))
+                } else {
+                    completion(nil)
+                }
+            }
+        )
+    }
+}
+
