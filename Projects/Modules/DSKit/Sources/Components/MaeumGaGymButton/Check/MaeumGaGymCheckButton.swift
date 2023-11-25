6a401550d9897773b4b676ebfc5871b5c0057e6b
은호 <127753071+Eunho0922@users.noreply.github.com>
2023-11-25 19:57:27 +0900

    ⚓️ :: [#33] 확인 버튼 제작

--- Projects/Modules/DSKit/Sources/Components/MaeumGaGymButton/Check/MaeumGaGymCheckButton.swift
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymButton/Check/MaeumGaGymCheckButton.swift
@@ -1,9 +1,49 @@
-//
-//  MaeumGaGymCheckButton.swift
-//  DSKitDemo
-//
-//  Created by 이은호 on 11/25/23.
-//  Copyright © 2023 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
+import DSKit
+import RxSwift
+import RxCocoaRuntime
+import Then
+import SnapKit
+import Core
 
-import Foundation
+open class MaeumGaGymCheckButton: UIButton {
+    
+    private let textLabel = UILabel().then {
+        $0.textAlignment = .center
+        $0.numberOfLines = 1
+        $0.font = UIFont.Pretendard.labelLarge
+    }
+    
+    public init (
+        text: String,
+        radius: Double? = 8.0,
+        textColor: UIColor? = DSKitAsset.Colors.gray200.color,
+        backColor: UIColor? =  DSKitAsset.Colors.gray400.color
+    ) {
+        
+        super.init(frame: .zero)
+        textLabel.text = text
+        backgroundColor = backColor
+        textLabel.textColor = textColor
+        layer.cornerRadius = radius ?? 8
+        setupUI()
+        
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        addSubview(textLabel)
+        
+        textLabel.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
+        
+        snp.makeConstraints {
+            $0.width.equalTo(390.0)
+            $0.height.equalTo(58.0)
+        }
+    }
+}
