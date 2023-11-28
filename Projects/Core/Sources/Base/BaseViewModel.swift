fa6cc780d5c60e312b01254d13b3096d30cda456
s <tylereunholee010203@gmail.com>
2023-11-28 20:11:48 +0900

    🍗 :: [#41] BaseViewModel 리펙

--- Projects/Core/Sources/Base/BaseViewModel.swift
+++ Projects/Core/Sources/Base/BaseViewModel.swift
@@ -3,11 +3,9 @@ import RxSwift
 import RxCocoa
 import RxFlow
 
-open class BaseViewModel {
-    public var disposeBag = DisposeBag()
-    public var steps = PublishRelay<Step>()
-    
-    public init() {
-        
-    }
+public protocol BaseViewModel {
+    associatedtype Input
+    associatedtype Output
+
+    func transform(_ input: Input) -> Output
 }
