943e57a2fba71ac22b25e307fa57b71bf60472aa
s <tylereunholee010203@gmail.com>
2023-11-28 20:00:52 +0900

    ⚓️ :: [#41] 이용약관 버튼 text case

---
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymButton/Agree/AgreeButtonType.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+public enum agreeButtonTextType {
+    case allAgreeText
+    case privacyAgreeText
+    case termsAgreeText
+    case ageAgreeText
+    case marketingAgreeText
+    
+    var message: String {
+        switch self {
+        case .privacyAgreeText:
+            return "개인정보 수집 이용 동의"
+        case .termsAgreeText:
+            return "이용 약관 동의"
+        case .ageAgreeText:
+            return "만 14세 이상"
+        case .marketingAgreeText:
+            return "마케팅 정보 수신 동의"
+        case .allAgreeText:
+            return "모두 동의해요"
+        }
+    }
+}
