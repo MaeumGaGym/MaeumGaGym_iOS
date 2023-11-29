edd6a7ee84fb2114cbd67651139297f469d3258c
junha <goodjunha@gmail.com>
2023-11-29 20:27:20 +0900

    💉:: [#48] tuist dependencies 수정

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -2,11 +2,11 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let spm = SwiftPackageManagerDependencies([
-    .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMinor(from: "5.0.0")),
+    .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMinor(from: "5.6.0")),
     .remote(url: "https://github.com/devxoul/Then", requirement: .upToNextMajor(from: "2")),
     .remote(url: "https://github.com/ReactiveX/RxSwift", requirement: .upToNextMajor(from: "6.5.0")),
     .remote(url: "https://github.com/Swinject/Swinject.git", requirement: .upToNextMajor(from: "2.8.0")),
-    .remote(url: "https://github.com/RxSwiftCommunity/RxFlow.git", requirement: .upToNextMajor(from: "2.13.0"))
+    .remote(url: "https://github.com/RxSwiftCommunity/RxFlow.git", requirement: .upToNextMajor(from: "2.10.0"))
 ])
 
 let dependencies = Dependencies(
