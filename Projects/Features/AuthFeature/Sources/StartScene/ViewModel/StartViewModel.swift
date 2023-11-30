560631deed9bef7aaf89789014c7af0204520766
junha <goodjunha@gmail.com>
2023-11-30 13:16:37 +0900

    🍗:: [#48] 접근 제어자 변경

--- Projects/Features/AuthFeature/Sources/StartScene/ViewModel/StartViewModel.swift
+++ Projects/Features/AuthFeature/Sources/StartScene/ViewModel/StartViewModel.swift
@@ -4,7 +4,7 @@ import RxCocoa
 import RxSwift
 import Core
 
-open class StartViewModel: BaseViewModel {
+public class StartViewModel: BaseViewModel {
     
     public struct Input {
         let kakaoButtonTap: Observable<Void>
