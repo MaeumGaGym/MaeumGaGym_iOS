06fd21454ae5e8fe2df517e35e512d658c587ae5
박준하 <102890390+jjunhaa0211@users.noreply.github.com>
2023-12-19 13:57:40 +0900

    💉:: [#60] 라이브러리 변경

--- Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -6,11 +6,14 @@ public extension TargetDependency {
 
 public extension TargetDependency.SPM {
     static let RxSwift = TargetDependency.external(name: "RxSwift")
-    static let RxRelay = TargetDependency.external(name: "RxRelay")
     static let RxCocoa = TargetDependency.external(name: "RxCocoa")
     static let SnapKit = TargetDependency.external(name: "SnapKit")
     static let Then = TargetDependency.external(name: "Then")
-    static let Swinject = TargetDependency.external(name: "Swinject")
     static let RxFlow = TargetDependency.external(name: "RxFlow")
+    static let Swinject = TargetDependency.external(name: "Swinject")
+    static let Moya = TargetDependency.external(name: "Moya")
+    static let RxMoya = TargetDependency.external(name: "RxMoya")
+    static let Lottie = TargetDependency.external(name: "Lottie")
+    static let kingfisher = TargetDependency.external(name: "Kingfisher")
 }
 
