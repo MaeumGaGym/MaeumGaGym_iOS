e9b1d27312da130f176f166e5d74df8dbc3383e5
박준하 <102890390+jjunhaa0211@users.noreply.github.com>
2023-12-20 16:49:56 +0900

    ⚓️:: [#61] alert supporter 추가

---
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymAlert/Supporter/AlertKitAPI.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+public enum AlertKitAPI {
+    
+    public static func present(view: AlertViewProtocol, completion: @escaping ()->Void = {}) {
+        guard let window = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first else { return }
+        view.present(on: window, completion: completion)
+    }
+    
+    public static func present(title: String? = nil, subtitle: String? = nil, icon: AlertIcon? = nil, style: AlertViewStyle, haptic: AlertHaptic? = nil) {
+        switch style {
+        case .iOS16AppleMusic:
+            guard let window = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first else { return }
+            let view = AlertAppleMusic16View(title: title, subtitle: subtitle, icon: icon)
+            view.haptic = haptic
+            view.present(on: window)
+        case .iOS17AppleMusic:
+            guard let window = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first else { return }
+            let view = AlertAppleMusic17View(title: title, subtitle: subtitle, icon: icon)
+            view.haptic = haptic
+            view.present(on: window)
+        }
+    }
+
+    public static func dismissAllAlerts(completion: (() -> Void)? = nil) {
+        
+        var alertViews: [AlertViewInternalDismissProtocol] = []
+        
+        for window in UIApplication.shared.windows {
+            for view in window.subviews {
+                if let view = view as? AlertViewInternalDismissProtocol {
+                    alertViews.append(view)
+                }
+            }
+        }
+        
+        if alertViews.isEmpty {
+            completion?()
+        } else {
+            for (index, view) in alertViews.enumerated() {
+                if index == .zero {
+                    view.dismiss(customCompletion: {
+                        completion?()
+                    })
+                } else {
+                    view.dismiss(customCompletion: nil)
+                }
+            }
+        }
+    }
+}
---
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymAlert/Supporter/AlertViewStyle.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+public enum AlertViewStyle {
+    case iOS16AppleMusic
+    case iOS17AppleMusic
+}
