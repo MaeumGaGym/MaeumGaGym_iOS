8d370ccecdda592eef7cf40e087f2fec0a34ce1e
박준하 <102890390+jjunhaa0211@users.noreply.github.com>
2023-12-20 16:46:40 +0900

    ⚓️:: [#61] UILabel extension

---
+++ Projects/Core/Sources/Utils/UILabel+Utils.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+public extension UILabel {
+    func layoutDynamicHeight(x: CGFloat, y: CGFloat, width: CGFloat) {
+        frame = CGRect.init(x: x, y: y, width: width, height: frame.height)
+        sizeToFit()
+        if frame.width != width {
+            frame = .init(x: x, y: y, width: width, height: frame.height)
+        }
+    }
+}
