8e278bd29919e3d170b79c4bada6b97ea5387fea
박준하 <102890390+jjunhaa0211@users.noreply.github.com>
2023-12-20 17:14:08 +0900

    🎮:: [#61] 알럿 테스트 코드 변경

--- Projects/Modules/DSKit/Demo/Sources/DemoViewController/Alert/AlertViewController.swift
+++ Projects/Modules/DSKit/Demo/Sources/DemoViewController/Alert/AlertViewController.swift
@@ -4,18 +4,23 @@ import Then
 
 public class AlertViewController: UIViewController {
     
-    let alertView1 = MaeumGaGymAlertBarView(title: "Added to Library", subtitle: nil, icon: .done)
-    let alertView2 = MaeumGaGymAlertBoxView(title: "asdf", subtitle: nil, icon: .heart)
+    let alertView1 = MaeumGaGymAlertBarView(title: "링크가 복사되었어요.", subtitle: nil, icon: .done)
+    let alertView2 = MaeumGaGymAlertBoxView(title: "알려주셔서 고마워요", subtitle: nil, icon: .done)
     
     public override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         
-        alertView1.titleLabel?.font = UIFont.systemFont(ofSize: 21)
-        alertView1.titleLabel?.textColor = .white
-        alertView1.backgroundColor = .black
+        alertView1.titleLabel?.font = UIFont.Pretendard.labelMedium
+        alertView1.titleLabel?.textColor = .black
+        alertView1.backgroundColor = .gray
+        
+        alertView2.titleLabel?.font = UIFont.Pretendard.labelMedium
+        alertView2.titleLabel?.textColor = .black
+        alertView2.backgroundColor = .gray
         
         alertView1.present(on: self.view)
+        alertView2.present(on: self.view)
     }
 }
