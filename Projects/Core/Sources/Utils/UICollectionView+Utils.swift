ef8d0c7c85f38ab3d29861c5729f8d13df3db012
junha <goodjunha@gmail.com>
2024-01-02 10:06:32 +0900

    🍗:: [#86] Uteill 리펙토링

--- Projects/Core/Sources/Utils/UICollectionView+Utils.swift
+++ Projects/Core/Sources/Utils/UICollectionView+Utils.swift
@@ -1,11 +1,3 @@
-//
-//  UICollectionView+Utils.swift
-//  Core
-//
-//  Created by 박준하 on 12/26/23.
-//  Copyright © 2023 MaeumGaGym-iOS. All rights reserved.
-//
-
 import UIKit
 
 protocol NibLoadable: AnyObject {
--- Projects/Core/Sources/Utils/UIStackView+Utils.swift
+++ Projects/Core/Sources/Utils/UIStackView+Utils.swift
@@ -1,11 +1,3 @@
-//
-//  UIStackView+Utils.swift
-//  Core
-//
-//  Created by 박준하 on 11/30/23.
-//  Copyright © 2023 MaeumGaGym-iOS. All rights reserved.
-//
-
 import Foundation
 import UIKit
 
