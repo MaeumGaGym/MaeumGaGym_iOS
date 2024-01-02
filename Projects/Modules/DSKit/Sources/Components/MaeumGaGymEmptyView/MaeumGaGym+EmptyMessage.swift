6fd2a3a095e34aaeeb475b4aa9026796d4eda3ec
junha <goodjunha@gmail.com>
2024-01-02 14:14:26 +0900

    🐞:: [#86]  setEmptyMessage 에러 해결

--- Projects/Modules/DSKit/Sources/Components/MaeumGaGymEmptyView/MaeumGaGym+EmptyMessage.swift
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymEmptyView/MaeumGaGym+EmptyMessage.swift
@@ -5,7 +5,7 @@ public extension UICollectionView {
 
     func setEmptyMessage(_ message: String, image: UIImage) {
         
-        let containerView = StatusLayoutView(message: "아직 사진이 없네요,\n첫 사진을 올려보세요.", image: UIImage(systemName: "square.and.arrow.up.on.square")!)
+        let containerView = StatusLayoutView(message: message, image: image)
         
         self.backgroundView = containerView
         
