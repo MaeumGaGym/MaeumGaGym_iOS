1f70a3123ff40dd13031859164da97842b9d9351
junha <goodjunha@gmail.com>
2024-01-02 16:05:45 +0900

    🍗:: [#86] StatusLayoutView 리펙토링

--- Projects/Modules/DSKit/Sources/Components/MaeumGaGymEmptyView/StatusLayoutView.swift
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymEmptyView/StatusLayoutView.swift
@@ -4,7 +4,7 @@ import SnapKit
 import Then
 import Core
 
-public class StatusLayoutView: UIView {
+public class StatusLayoutView: BaseView {
     
     private let imageView = UIImageView().then {
         $0.adjustsImageSizeForAccessibilityContentSizeCategory = true
@@ -13,7 +13,7 @@ public class StatusLayoutView: UIView {
 
     }
 
-    private let messageLabel = UILabel().then {
+    private var messageLabel = UILabel().then {
         $0.textColor = .red
         $0.numberOfLines = 0
         $0.textAlignment = .center
@@ -23,25 +23,32 @@ public class StatusLayoutView: UIView {
     
     public init(message: String, image: UIImage) {
         super.init(frame: .zero)
+        self.setupUI(message: message, image: image)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func layout() {
+        super.layout()
         
         self.addSubviews([imageView, messageLabel])
-        
-        imageView.image = image
-        messageLabel.text = message
-                
-        imageView.snp.makeConstraints {
+
+        self.imageView.snp.makeConstraints {
             $0.centerY.equalToSuperview().offset(-80.0)
             $0.centerX.equalToSuperview()
             $0.width.height.equalTo(80.0)
         }
 
-        messageLabel.snp.makeConstraints {
+        self.messageLabel.snp.makeConstraints {
             $0.top.equalTo(imageView.snp.bottom).offset(40.0)
             $0.centerX.equalToSuperview()
         }
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    private func setupUI(message: String, image: UIImage) {
+        imageView.image = image
+        messageLabel.text = message
     }
 }
