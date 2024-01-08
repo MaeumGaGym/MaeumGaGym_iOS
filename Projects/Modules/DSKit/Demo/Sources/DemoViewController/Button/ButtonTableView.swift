7834aacebcbb470c2271f961b059b6b535d03ec6
s <tylereunholee010203@gmail.com>
2024-01-08 19:59:46 +0900

    ⚓️ :: [#89] buttonTableViewController 추가

--- Projects/Modules/DSKit/Demo/Sources/DemoViewController/Button/ButtonTableView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ButtonTableView.swift
-//  DSKit
-//
-//  Created by 이은호 on 1/8/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
-
-import Foundation
---
+++ Projects/Modules/DSKit/Demo/Sources/DesignSystemDemo/TableView/ButtonTableView.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+fileprivate enum buttonType: String, CaseIterable {
+    case button
+}
+
+public class ButtonTableViewController: UITableViewController  {
+    
+    private var designSystems: [buttonType] = buttonType.allCases
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.largeContentTitle = "MaeumGaGym design System"
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        self.view.backgroundColor = .white
+    }
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        designSystems.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = self.designSystems[indexPath.item].rawValue
+        return cell
+    }
+    
+    public override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let designSystem = self.designSystems[indexPath.item]
+        switch designSystem {
+        case .button:
+            print("커스텀 버튼")
+            self.navigationController?.pushViewController(ButtonViewController(), animated: true)
+            break
+        }
+    }
+}
+
+
