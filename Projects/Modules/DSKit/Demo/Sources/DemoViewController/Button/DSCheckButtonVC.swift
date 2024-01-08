39f7bfe5364313cf27689338a69697c57d61f3b8
s <tylereunholee010203@gmail.com>
2024-01-08 20:19:07 +0900

    ⚓️ :: [#89] checkButton VC 추가

--- Projects/Modules/DSKit/Demo/Sources/DemoViewController/Button/DSCheckButtonVC.swift
+++ Projects/Modules/DSKit/Demo/Sources/DemoViewController/Button/DSCheckButtonVC.swift
@@ -1,9 +1,30 @@
-//
-//  DSCheckButtonVC.swift
-//  DSKit
-//
-//  Created by 이은호 on 1/8/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
+import DSKit
+import SnapKit
+import Then
 
-import Foundation
+public class DSCheckButtonVC: UIViewController {
+    
+    var checkButton = MaeumGaGymCheckButton(text: "asdf")
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        
+        layout()
+    }
+    
+    func layout() {
+        [
+            checkButton
+        ].forEach { view.addSubview($0) }
+        
+        checkButton.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(70.0)
+            $0.width.equalTo(48.0)
+        }
+    }
+}
+
