6951aca1740a36dda6a9611c51fccd34bd03e1b3
junha <goodjunha@gmail.com>
2024-01-08 19:43:28 +0900

    🍗:: [#88] Timer 추가

--- Projects/Features/RootFeature/Sources/HomeFlow.swift
+++ Projects/Features/RootFeature/Sources/HomeFlow.swift
@@ -14,7 +14,7 @@ public class HomeFlow: Flow {
     private let rootViewController = UINavigationController()
 
     init() {
-        let viewController = HomeViewController()
+        let viewController = TimerViewController()
         viewController.view.backgroundColor = .white
         viewController.tabBarItem = UITabBarItem(title: "홈", image: DSKitAsset.Assets.blackHome.image, selectedImage: DSKitAsset.Assets.blueHome.image)
         
