a28aa5f85ba4f3e865c2661c62ac10440c283820
s <tylereunholee010203@gmail.com>
2024-01-08 20:19:38 +0900

    ⚓️ :: [#89] 타이머 버튼 VC 추가

--- Projects/Modules/DSKit/Demo/Sources/DemoViewController/Button/DSTimerButtonVC.swift
+++ Projects/Modules/DSKit/Demo/Sources/DemoViewController/Button/DSTimerButtonVC.swift
@@ -1,9 +1,58 @@
-//
-//  DSTimerButtonVC.swift
-//  DSKit
-//
-//  Created by 이은호 on 1/8/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
+import DSKit
+import SnapKit
+import Then
 
-import Foundation
+public class DSTimerButtonVC: UIViewController {
+    
+    var cancelButton = MaeumGaGymTimerButton(type: .close)
+    var restartButton = MaeumGaGymTimerButton(type: .restart)
+    var startButton = MaeumGaGymTimerButton(type: .start)
+    var stopButton = MaeumGaGymTimerButton(type: .stop)
+
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        
+        layout()
+    }
+    
+    func layout() {
+        [
+            cancelButton,
+            restartButton,
+            startButton,
+            stopButton
+        ].forEach { view.addSubview($0) }
+
+        cancelButton.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(view.snp.width).offset(-40.0)
+            $0.height.equalTo(64.0)
+        }
+        
+        restartButton.snp.makeConstraints {
+            $0.top.equalTo(cancelButton.snp.bottom).offset(20.0)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(view.snp.width).offset(-40.0)
+            $0.height.equalTo(64.0)
+        }
+        
+        startButton.snp.makeConstraints {
+            $0.top.equalTo(restartButton.snp.bottom).offset(20.0)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(view.snp.width).offset(-40.0)
+            $0.height.equalTo(64.0)
+        }
+        
+        stopButton.snp.makeConstraints {
+            $0.top.equalTo(startButton.snp.bottom).offset(20.0)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(view.snp.width).offset(-40.0)
+            $0.height.equalTo(64.0)
+        }
+    }
+}
+
