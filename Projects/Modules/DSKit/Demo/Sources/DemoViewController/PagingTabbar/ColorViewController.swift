d92c673e38c572d9f5c106ebf8225f81a1382beb
s <tylereunholee010203@gmail.com>
2024-01-11 00:26:51 +0900

    ⚓️ :: [#97] test ViewController 추가

--- Projects/Modules/DSKit/Demo/Sources/DemoViewController/PagingTabbar/ColorViewController.swift
+++ Projects/Modules/DSKit/Demo/Sources/DemoViewController/PagingTabbar/ColorViewController.swift
@@ -1,9 +1,62 @@
-//
-//  ColorViewController.swift
-//  DSKit
-//
-//  Created by 이은호 on 1/10/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
+import SnapKit
+import Then
 
-import Foundation
+public class RedViewController: UIViewController {
+    
+    private lazy var textView = UIView().then {
+        $0.backgroundColor = .black
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        layout()
+    }
+    
+    func layout() {
+        view.addSubview(textView)
+        
+        textView.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+            $0.height.width.equalTo(100)
+        }
+    }
+}
+
+public class GreenViewController: UIViewController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+}
+
+public class BlueViewController: UIViewController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
+
+public class BlackViewController: UIViewController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
+
+public class WhiteViewController: UIViewController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
