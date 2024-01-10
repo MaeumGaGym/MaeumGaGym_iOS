5a4f74c4232d021423eea55198dcbcb827ff0c47
junha <goodjunha@gmail.com>
2024-01-10 19:23:44 +0900

    💉:: [#95] Lint 변경

--- Tuist/ProjectDescriptionHelpers/Scripts.swift
+++ Tuist/ProjectDescriptionHelpers/Scripts.swift
@@ -1,17 +1,27 @@
 import ProjectDescription
 
 public extension TargetScript {
-    static let SwiftLintString = TargetScript.pre(script: """
-if test -d "/opt/homebrew/bin/"; then
-    PATH="/opt/homebrew/bin/:${PATH}"
-fi
+    static let swiftLintScript = TargetScript.pre(
+        script: """
+                lintableFileCount=`ls -1 *.swift 2>/dev/null | wc -l`
+                if [ $lintableFileCount != 0 ]
+                then
+                    if test -d "/opt/homebrew/bin/"; then
+                        PATH="/opt/homebrew/bin/:${PATH}"
+                    fi
 
-export PATH
+                    export PATH
 
-if which swiftlint > /dev/null; then
-    swiftlint autocorrect && swiftlint
-else
-    echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-fi
-""", name: "SwiftLintString", basedOnDependencyAnalysis: false)
+                    if which swiftlint >/dev/null; then
+                        swiftlint autocorrect && swiftlint
+                    else
+                        echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+                    fi
+                else
+                    echo "No lintable files to check, skipping Swiftlint"
+                fi
+                """,
+        name: "SwiftLint",
+        basedOnDependencyAnalysis: false
+    )
 }
