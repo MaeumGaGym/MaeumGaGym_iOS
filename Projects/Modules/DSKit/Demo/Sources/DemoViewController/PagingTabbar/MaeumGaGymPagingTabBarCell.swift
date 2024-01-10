8edeafb36c9ced8b550c5bf795fb96446de29c6f
s <tylereunholee010203@gmail.com>
2024-01-11 00:26:00 +0900

    ⚓️ :: [#97] 마음가짐 paging tab bar cell 추가

--- Projects/Modules/DSKit/Demo/Sources/DemoViewController/PagingTabbar/MaeumGaGymPagingTabBarCell.swift
+++ Projects/Modules/DSKit/Demo/Sources/DemoViewController/PagingTabbar/MaeumGaGymPagingTabBarCell.swift
@@ -4,22 +4,28 @@ import SnapKit
 
 public class MaeumGaGymPagingTabBarCell: UICollectionViewCell {
     
-    static let identifier = "MaeumGaGymTabBarCell"
+    static let identifier = "MaeumGaGymPagingTabBarCell"
     
     private lazy var titleLabel = UILabel().then {
-        $0.font = .systemFont(ofSize: 15.0, weight: .semibold)
-        $0.textColor = .secondaryLabel
+        $0.font = UIFont.Pretendard.bodyLarge
+        $0.textColor = DSKitAsset.Colors.gray400.color
         $0.textAlignment = .center
     }
+    
+    private lazy var grayUnderline = UIView().then {
+        $0.backgroundColor = DSKitAsset.Colors.gray50.color
+        $0.alpha = 1.0
+    }
+
         
     private lazy var underline = UIView().then {
-        $0.backgroundColor = .black
+        $0.backgroundColor = DSKitAsset.Colors.blue500.color
         $0.alpha = 0.0
     }
     
     public override var isSelected: Bool {
         didSet {
-            titleLabel.textColor = isSelected ? .black : .secondaryLabel
+            titleLabel.textColor = isSelected ? .black : DSKitAsset.Colors.gray400.color
             underline.alpha = isSelected ? 1.0 : 0.0
         }
     }
@@ -34,15 +40,24 @@ private extension MaeumGaGymPagingTabBarCell {
     func setupLayout() {
         [
             titleLabel,
+            grayUnderline,
             underline
         ].forEach { addSubview($0) }
         titleLabel.snp.makeConstraints { make in
             make.leading.top.trailing.equalToSuperview()
         }
-        underline.snp.makeConstraints { make in
-            make.height.equalTo(3.0)
+        
+        grayUnderline.snp.makeConstraints { make in
+            make.height.equalTo(2.0)
             make.top.equalTo(titleLabel.snp.bottom).offset(4.0)
             make.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        underline.snp.makeConstraints { make in
+            make.height.equalTo(2.0)
+            make.top.equalTo(titleLabel.snp.bottom).offset(4.0)
+            make.leading.trailing.equalToSuperview().inset(14.0)
+            make.bottom.equalToSuperview()
         }
     }
 }
