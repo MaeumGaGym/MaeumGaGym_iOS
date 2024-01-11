9988855b774ab4ccd48c52fbc55accc67e524cb0
s <tylereunholee010203@gmail.com>
2024-01-12 00:39:25 +0900

    ⚓️ :: [#110] postureViewController 파일 추가

--- Projects/Features/PostureFeature/Sources/PostureScene/View/PostureViewController.swift
+++ Projects/Features/PostureFeature/Sources/PostureScene/View/PostureViewController.swift
@@ -1,1 +1,25 @@
 
+import UIKit
+import SnapKit
+import Then
+import Core
+import RxFlow
+import RxCocoa
+import RxSwift
+import DSKit
+import CSLogger
+import PostureFeatureInterface
+
+public class PostureViewController: BaseViewController<PostureViewModel> {
+    
+    private let titleView = PostureRecommandTitleView(text: "맨몸운동", image: DSKitAsset.Assets.arm.image)
+    
+    
+    private var checkButton = MaeumGaGymCheckButton(text: "확인")
+    
+    public override func layout() {
+        self.view.addSubviews([titleView])
+        
+    }
+}
+
