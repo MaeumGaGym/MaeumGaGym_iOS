015ba304163d4ebb7f55a543243a79f3ffa99f7b
junha <goodjunha@gmail.com>
2024-01-11 10:25:16 +0900

    🍗:: [#101] DSKit SceneDelegate 리펙토링

--- Projects/Modules/DSKit/Demo/Sources/Application/SceneDelegate.swift
+++ Projects/Modules/DSKit/Demo/Sources/Application/SceneDelegate.swift
@@ -6,8 +6,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions) {
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(frame: scene.coordinateSpace.bounds)
