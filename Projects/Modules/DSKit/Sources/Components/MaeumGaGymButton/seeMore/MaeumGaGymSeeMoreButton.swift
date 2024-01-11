a01a0104b176ecb694c3857c728a71c31407f08e
s <tylereunholee010203@gmail.com>
2024-01-11 12:07:19 +0900

    ⚓️ :: [#100] 마음가짐 더보기 버튼 추가

---
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymButton/seeMore/MaeumGaGymSeeMoreButton.swift
@@ -0,0 +1,44 @@
+import UIKit
+import Then
+import SnapKit
+import RxSwift
+import RxCocoa
+import Core
+
+
+public class MaeumGaGymSeeMoreButton: BaseButton {
+    
+    private let seemoreLabel = UILabel().then {
+        $0.font = UIFont.Pretendard.bodyMedium
+        $0.textColor = DSKitAsset.Colors.gray400.color
+        $0.text = "더보기"
+    }
+    
+    private let rigthArrowImageView = UIImageView().then {
+        $0.image = DSKitAsset.Assets.right.image
+    }
+    
+    public init() {
+        super.init(frame: .zero)
+        setupUI()
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        self.addSubviews([seemoreLabel, rigthArrowImageView])
+        seemoreLabel.snp.makeConstraints {
+            $0.width.equalTo(42.0)
+            $0.height.equalTo(20.0)
+            $0.leading.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        rigthArrowImageView.snp.makeConstraints {
+            $0.width.height.equalTo(24.0)
+            $0.trailing.equalToSuperview()
+        }
+    }
+}
