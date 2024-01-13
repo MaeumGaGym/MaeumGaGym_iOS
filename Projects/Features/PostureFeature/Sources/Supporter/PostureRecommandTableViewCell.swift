edf7c96a8c9a4d017ac206cfa6e9bbaa9bcad7d5
s <tylereunholee010203@gmail.com>
2024-01-13 19:52:18 +0900

    ⚓️ :: [#110] postureRecommandTableViewCell 추가

---
+++ Projects/Features/PostureFeature/Sources/Supporter/PostureRecommandTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  PostureRecommandTableViewCell.swift
+//  PostureFeatureInterface
+//
+//  Created by 이은호 on 1/13/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
