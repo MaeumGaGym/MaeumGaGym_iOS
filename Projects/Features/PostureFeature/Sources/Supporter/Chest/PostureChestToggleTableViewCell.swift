5b41a9022a042bd5b3bb56842d8fc35dae0f1afb
s <tylereunholee010203@gmail.com>
2024-01-14 23:41:24 +0900

    ⚓️ :: [#115] 자세 - 가슴 탭 - 토글 tableViewCell 파일 추가

---
+++ Projects/Features/PostureFeature/Sources/Supporter/Chest/PostureChestToggleTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  PostureChestToggleTableViewCell.swift
+//  PostureFeature
+//
+//  Created by 이은호 on 1/14/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
