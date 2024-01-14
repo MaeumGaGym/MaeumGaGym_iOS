11f1839f72950a2646cb3ea43a6736d49ba8248a
s <tylereunholee010203@gmail.com>
2024-01-15 04:06:52 +0900

    ⚓️ :: [#117] 자세 - 디테일 - 제목 tableViewCell 추가

--- Projects/Features/PostureFeature/Sources/Supporter/Detail/PostureDetailTitleTableViewCell.swift
+++ Projects/Features/PostureFeature/Sources/Supporter/Detail/PostureDetailTitleTableViewCell.swift
@@ -1,9 +1,50 @@
-//
-//  File.swift
-//  PostureFeatureInterface
-//
-//  Created by 이은호 on 1/15/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
+import SnapKit
+import Then
+import DSKit
 
-import Foundation
+public class PostureDetailTitleTableViewCell: UITableViewCell {
+    
+    static let identifier: String = "PostureDetailTitleTableViewCell"
+    
+    private var englishTitle = UILabel().then {
+        $0.font = UIFont.Pretendard.titleMedium
+        $0.textColor = DSKitAsset.Colors.gray600.color
+        $0.textAlignment = .left
+    }
+    
+    private var koreanTitle = UILabel().then {
+        $0.font = UIFont.Pretendard.titleLarge
+        $0.textColor = .black
+        $0.textAlignment = .left
+    }
+    
+    public func setup(englishText: String, koreanText: String) {
+        self.englishTitle.text = englishText
+        self.koreanTitle.text = koreanText
+        
+        addViews()
+        setupViews()
+    }
+    
+    private func addViews() {
+        [
+            englishTitle,
+            koreanTitle
+        ].forEach { contentView.addSubview($0) }
+    }
+    
+    private func setupViews() {
+        englishTitle.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.height.equalTo(32.0)
+            $0.top.trailing.leading.equalToSuperview()
+        }
+        
+        koreanTitle.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.height.equalTo(48.0)
+            $0.bottom.trailing.leading.equalToSuperview()
+        }
+    }
+}
