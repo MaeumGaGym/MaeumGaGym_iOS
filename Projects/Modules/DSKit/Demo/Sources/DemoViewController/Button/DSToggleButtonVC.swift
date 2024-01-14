33a242eadb2e9cb207b43c538648c9b9d80fdc2b
s <tylereunholee010203@gmail.com>
2024-01-14 16:11:03 +0900

    ⚓️ :: [#115] toggleButtonVC에 버튼 view 추가

--- Projects/Modules/DSKit/Demo/Sources/DemoViewController/Button/DSToggleButtonVC.swift
+++ Projects/Modules/DSKit/Demo/Sources/DemoViewController/Button/DSToggleButtonVC.swift
@@ -5,10 +5,18 @@ import Then
 
 public class DSToggleButtonVC: UIViewController {
     
-    var album = MaeumGaGymToggleButton(type: .album)
-    var image = MaeumGaGymToggleButton(type: .image)
+    var image = MaeumGaGymToggleButton(type: .album)
+    var album = MaeumGaGymToggleButton(type: .image)
     var metronome = MaeumGaGymToggleButton(type: .metronome)
     var timer = MaeumGaGymToggleButton(type: .timer)
+    var metronome1 = MaeumGaGymToggleButton(type: .metronomeHome)
+    var timer1 = MaeumGaGymToggleButton(type: .timerHome)
+    var morning = MaeumGaGymToggleButton(type: .morning)
+    var lunch = MaeumGaGymToggleButton(type: .lunch)
+    var dinner = MaeumGaGymToggleButton(type: .dinner)
+    var bareBody = MaeumGaGymToggleButton(type: .bareBody)
+    var marchine = MaeumGaGymToggleButton(type: .marchine)
+
 
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,38 +27,94 @@ public class DSToggleButtonVC: UIViewController {
     
     func layout() {
         [
-            album,
             image,
+            album,
             metronome,
-            timer
+            timer,
+            metronome1,
+            timer1,
+            morning,
+            lunch,
+            dinner,
+            bareBody,
+            marchine
         ].forEach { view.addSubview($0) }
-
-        album.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
-            $0.centerX.equalToSuperview()
-            $0.width.equalTo(view.snp.width).offset(-40.0)
-            $0.height.equalTo(64.0)
-        }
         
         image.snp.makeConstraints {
-            $0.top.equalTo(album.snp.bottom).offset(20.0)
-            $0.centerX.equalToSuperview()
-            $0.width.equalTo(view.snp.width).offset(-40.0)
-            $0.height.equalTo(64.0)
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.equalToSuperview().offset(20.0)
+            $0.width.equalTo(83.0)
+            $0.height.equalTo(40.0)
         }
         
-        metronome.snp.makeConstraints {
-            $0.top.equalTo(image.snp.bottom).offset(20.0)
-            $0.centerX.equalToSuperview()
-            $0.width.equalTo(view.snp.width).offset(-40.0)
-            $0.height.equalTo(64.0)
+        album.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.equalToSuperview().offset(120.0)
+            $0.width.equalTo(83.0)
+            $0.height.equalTo(40.0)
         }
         
         timer.snp.makeConstraints {
-            $0.top.equalTo(metronome.snp.bottom).offset(20.0)
-            $0.centerX.equalToSuperview()
-            $0.width.equalTo(view.snp.width).offset(-40.0)
-            $0.height.equalTo(64.0)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(100.0)
+            $0.leading.equalToSuperview().offset(20.0)
+            $0.width.equalTo(92.0)
+            $0.height.equalTo(40.0)
+        }
+        
+        metronome.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(100.0)
+            $0.leading.equalToSuperview().offset(120.0)
+            $0.width.equalTo(92.0)
+            $0.height.equalTo(40.0)
+        }
+        
+        timer1.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(200.0)
+            $0.leading.equalToSuperview().offset(20.0)
+            $0.width.equalTo(78.0)
+            $0.height.equalTo(32.0)
+        }
+        
+        metronome1.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(200.0)
+            $0.leading.equalToSuperview().offset(120.0)
+            $0.width.equalTo(78.0)
+            $0.height.equalTo(32.0)
+        }
+        
+        morning.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(300.0)
+            $0.leading.equalToSuperview().offset(20.0)
+            $0.width.equalTo(64.0)
+            $0.height.equalTo(32.0)
+        }
+        
+        lunch.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(300.0)
+            $0.leading.equalToSuperview().offset(120.0)
+            $0.width.equalTo(64.0)
+            $0.height.equalTo(32.0)
+        }
+        
+        dinner.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(300.0)
+            $0.leading.equalToSuperview().offset(220.0)
+            $0.width.equalTo(64.0)
+            $0.height.equalTo(32.0)
+        }
+        
+        bareBody.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(400.0)
+            $0.leading.equalToSuperview().offset(20.0)
+            $0.width.equalTo(60.0)
+            $0.height.equalTo(36.0)
+        }
+        
+        marchine.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(400.0)
+            $0.leading.equalToSuperview().offset(120.0)
+            $0.width.equalTo(60.0)
+            $0.height.equalTo(36.0)
         }
     }
 }
