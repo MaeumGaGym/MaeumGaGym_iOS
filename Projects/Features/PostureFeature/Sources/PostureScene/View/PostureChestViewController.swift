bb419a5b17a0f873c0c69685b4f9cf3e71543144
s <tylereunholee010203@gmail.com>
2024-01-15 00:58:37 +0900

    🍗 :: [#115] postureChestVC 리펙토링

--- Projects/Features/PostureFeature/Sources/PostureScene/View/PostureChestViewController.swift
+++ Projects/Features/PostureFeature/Sources/PostureScene/View/PostureChestViewController.swift
@@ -11,28 +11,28 @@ import PostureFeatureInterface
 
 public class PostureChestViewController: BaseViewController<PostureChestViewModel>, UITableViewDelegate, UITableViewDataSource {
     
-    var tableView: UITableView!
-    var chestModel: PostureChestModel!
+    private var postureChestTableView: UITableView!
+    private var chestModel: PostureChestModel!
     
     public override func viewDidLoad() {
         super.viewDidLoad()
         
         chestModel = PostureChestModel.first
         
-        tableView = UITableView().then {
+        postureChestTableView = UITableView().then {
             $0.dataSource = self
             $0.delegate = self
             $0.showsVerticalScrollIndicator = false
             $0.backgroundColor = .white
             $0.separatorStyle = .none
             $0.register(PostureChestToggleTableViewCell.self, forCellReuseIdentifier: PostureChestToggleTableViewCell.identifier)
-                   $0.register(PostureChestTableViewCell.self, forCellReuseIdentifier: PostureChestTableViewCell.identifier)
+            $0.register(PostureChestTableViewCell.self, forCellReuseIdentifier: PostureChestTableViewCell.identifier)
         }
         
-        view.addSubview(tableView)
+        view.addSubview(postureChestTableView)
         
-        tableView.snp.makeConstraints {
-            $0.leading.trailing.equalToSuperview()
+        postureChestTableView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(8) // 좌우 간격 설정
             $0.width.equalToSuperview()
             $0.height.equalTo(613.0)
             $0.bottom.equalToSuperview()
@@ -43,19 +43,30 @@ public class PostureChestViewController: BaseViewController<PostureChestViewMode
         return 8
     }
     
-   
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let firstCell = tableView.dequeueReusableCell(withIdentifier: PostureChestToggleTableViewCell.identifier, for: indexPath) as! PostureChestToggleTableViewCell
-        let secondCell = tableView.dequeueReusableCell(withIdentifier: PostureChestTableViewCell.identifier, for: indexPath) as! PostureChestTableViewCell
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { // 셀 높이 설정
         if indexPath.row == 0 {
-            firstCell.setup(firstType: .bareBody, secondType: .marchine)
-            firstCell.selectionStyle = .none
-            return firstCell
+            return 60
         } else {
-            let exercise = chestModel.data[indexPath.row - 1] // 첫 번째 행을 제외한 나머지 행에서 사용될 데이터를 가져옵니다.
-            secondCell.setup(exerciseImage: exercise.image, exerciseNameText: exercise.name)
-            secondCell.selectionStyle = .none
-            return secondCell
+            return 88
         }
     }
+   
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: PostureChestToggleTableViewCell.identifier, for: indexPath) as! PostureChestToggleTableViewCell
+            cell.setup(firstType: .bareBody, secondType: .marchine)
+            cell.selectionStyle = .none
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: PostureChestTableViewCell.identifier, for: indexPath) as! PostureChestTableViewCell
+            let exercise = chestModel.data[indexPath.row - 1] // 첫 번째 행을 제외한 나머지 행에서 사용될 데이터를 가져옵니다.
+            cell.setup(exerciseImage: exercise.image, exerciseNameText: exercise.name)
+            cell.selectionStyle = .none
+            return cell
+        }
+        
+        
+    }
+
 }
+
