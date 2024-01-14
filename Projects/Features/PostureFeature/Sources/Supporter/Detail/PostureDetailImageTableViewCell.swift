39754e7760e471a198d89207f2129d266b9cc64f
s <tylereunholee010203@gmail.com>
2024-01-15 02:56:32 +0900

    ⚓️ :: [#117] 자세 - 디테일 - 이미지 cell 추가

--- Projects/Features/PostureFeature/Sources/Supporter/Detail/PostureDetailImageTableViewCell.swift
+++ Projects/Features/PostureFeature/Sources/Supporter/Detail/PostureDetailImageTableViewCell.swift
@@ -1,9 +1,30 @@
-//
-//  PostureDetailImageTableViewCell.swift
-//  PostureFeatureInterface
-//
-//  Created by 이은호 on 1/15/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
+import SnapKit
+import Then
+import DSKit
 
-import Foundation
+public class PostureDetailImageTableViewCell: UITableViewCell {
+    static let identifier: String = "PostureDetailImageTableViewCell"
+    
+    private var postureImageView = UIImageView().then {
+        $0.backgroundColor = .gray
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    public func setup(image: UIImage) {
+        self.postureImageView.image = image
+        
+        addView()
+        setupView()
+    }
+    
+    private func addView() {
+        contentView.addSubview(postureImageView)
+    }
+    
+    private func setupView() {
+        postureImageView.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
+    }
+}
