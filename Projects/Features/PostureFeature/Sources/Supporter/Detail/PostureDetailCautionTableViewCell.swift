ab14be250aeeae4b8565e547dc179ebf5b3d41dd
s <tylereunholee010203@gmail.com>
2024-01-15 09:26:03 +0900

    ⚓️ :: [#117] 자세 - 디테일 - 주의사항

--- Projects/Features/PostureFeature/Sources/Supporter/Detail/PostureDetailCautionTableViewCell.swift
+++ Projects/Features/PostureFeature/Sources/Supporter/Detail/PostureDetailCautionTableViewCell.swift
@@ -1,9 +1,49 @@
-//
-//  PostureDetailCautionTableViewCell.swift
-//  PostureFeature
-//
-//  Created by 이은호 on 1/15/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
+import SnapKit
+import Then
+import DSKit
 
-import Foundation
+public class PostureDetailCautionTableViewCell: UITableViewCell {
+    static let identifier: String = "PostureDetailCautionTableViewCell"
+    
+    private var exerciseWay = UILabel().then {
+        $0.text = "주의사항"
+        $0.textColor = .black
+        $0.textAlignment = .left
+        $0.font = UIFont.Pretendard.titleMedium
+    }
+    
+    private var exerciseCaution1 = MaeumGaGymPostureInfoLabel(titleNumber: "01", text: "양팔을 가슴 옆에 두고 바닥에 엎드립니다.")
+    
+    private var exerciseCaution2 = MaeumGaGymPostureInfoLabel(titleNumber: "02", text: "양팔을 가슴 옆에 두고 바닥에 엎드립니다.", numberOfLines: 2)
+    
+    private func addViews() {
+        [
+            exerciseWay,
+            exerciseCaution1,
+            exerciseCaution2,
+        ].forEach { contentView.addSubview($0) }
+    }
+    
+    private func setupViews() {
+        
+        exerciseWay.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(12.0)
+            $0.leading.equalToSuperview()
+            $0.width.equalToSuperview()
+            $0.height.equalTo(32.0)
+        }
+        
+        exerciseCaution1.snp.makeConstraints {
+            $0.top.equalTo(exerciseWay.snp.bottom).offset(24.0)
+            $0.leading.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+        
+        exerciseCaution2.snp.makeConstraints {
+            $0.top.equalTo(exerciseCaution1.snp.bottom).offset(16.0)
+            $0.leading.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+    }
+}
