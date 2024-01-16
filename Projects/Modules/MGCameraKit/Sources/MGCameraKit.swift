770526daf9f439c396d2038f55b9ad3bcd6d2385
junha <goodjunha@gmail.com>
2024-01-16 22:11:42 +0900

    ⚓️:: [#121] 파일 추가

---
+++ Projects/Modules/MGCameraKit/Sources/MGCameraKit.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+import Then
+import AVFoundation
+
+open class MGCameraKit: UIView, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+}
