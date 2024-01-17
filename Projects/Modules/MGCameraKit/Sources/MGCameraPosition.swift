88dc3858596cab3df548eb857419493243416bae
junha <goodjunha@gmail.com>
2024-01-17 16:47:25 +0900

    ⚓️:: [#121] 카메라 방향 코드 추가

---
+++ Projects/Modules/MGCameraKit/Sources/MGCameraPosition.swift
@@ -0,0 +1,16 @@
+//
+//  MGCameraPosition.swift
+//  MGCameraKit
+//
+//  Created by 박준하 on 1/17/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public enum MGCameraPosition {
+    case front // 후방
+    case back // 셀카
+}
+
