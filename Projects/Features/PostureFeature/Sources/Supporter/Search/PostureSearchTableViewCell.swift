baf744e9d1a2fa6f7f9208b134c13f9c21b5cb6d
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-01-22 13:35:57 +0900

    ⚓️ :: [#131] 자세 - 검색 - tableViewCell 파일 추가

---
+++ Projects/Features/PostureFeature/Sources/Supporter/Search/PostureSearchTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  PostureSearchTableViewCell.swift
+//  PostureFeatureInterface
+//
+//  Created by 이은호 on 1/22/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
