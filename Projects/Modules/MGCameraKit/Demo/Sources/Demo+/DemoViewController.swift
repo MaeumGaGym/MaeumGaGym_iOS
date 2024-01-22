8f19376c550338bbfc153f8dc44062d229df7bec
junha <goodjunha@gmail.com>
2024-01-22 15:54:21 +0900

    ⚓️:: [#132] 사진 찍고 뒤집는 코드 추가

--- Projects/Modules/MGCameraKit/Demo/Sources/Demo+/DemoViewController.swift
+++ Projects/Modules/MGCameraKit/Demo/Sources/Demo+/DemoViewController.swift
@@ -1,18 +1,43 @@
 import UIKit
 import SnapKit
 import Then
+import RxSwift
+import RxCocoa
+import Core
 import MGCameraKit
-import MGLogger
 
 public class DemoViewController: UIViewController {
     
-    var cameraView: MGCamera!
-    var captureButton: UIButton!
-    var gridView: MGridView!
+    private var cameraView: MGCamera!
+    private var gridView: MGridView!
     
-    var isOn = false
-    var gridOn = false
-    var touchShootOn = false
+    private var captureButton = UIButton().then {
+        $0.backgroundColor = .red
+        $0.setTitle("사진", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+    }
+    
+    private var photoButton = UIButton().then {
+        $0.backgroundColor = .red
+        $0.setTitle("앨범", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+    }
+    private var chageButton = UIButton().then {
+        $0.backgroundColor = .red
+        $0.setTitle("뒤집기", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+    }
+    
+    private  var buttomStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.alignment = .center
+        $0.spacing = 83.0
+        $0.distribution = .fillEqually
+    }
+    
+    private var isOn = true
+    private var gridOn = false
+    private var touchShootOn = false
     
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +52,22 @@ public class DemoViewController: UIViewController {
         cameraView.setCameraPosition(.back)
         cameraView.startRunning()
         
+        view.addSubview(buttomStackView)
+        buttomStackView.addArrangedSubviews(photoButton, captureButton, chageButton)
+        
+        chageButton.rx.tap
+            .subscribe(with: self, onNext: { owner, _  in
+                owner.reversalDidTap()
+            })
+        
+        captureButton.rx.tap
+            .bind {
+                self.buttonDidTap()
+            }
+        
         title = "사진 촬영"
+        
+        buttonDidTap()
     }
     
     public override func viewDidLayoutSubviews() {
@@ -43,17 +83,36 @@ public class DemoViewController: UIViewController {
             $0.width.equalToSuperview()
             $0.height.equalTo(573.0)
         }
+        
+        captureButton.snp.makeConstraints {
+            $0.height.equalTo(buttomStackView.snp.height).dividedBy(3)
+        }
+        
+        photoButton.snp.makeConstraints {
+            $0.height.equalTo(buttomStackView.snp.height).dividedBy(3)
+        }
+        
+        chageButton.snp.makeConstraints {
+            $0.height.equalTo(buttomStackView.snp.height).dividedBy(3)
+        }
+        
+        buttomStackView.snp.makeConstraints {
+            $0.top.equalTo(cameraView.snp.bottom)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            $0.centerX.equalToSuperview()
+            $0.width.equalToSuperview().inset(40.0)
+        }
     }
     
-    func buttonDidTap(_ sender: Any) {
+    func buttonDidTap() {
         print("capture")
         cameraView.capturePhoto { result in
             switch result {
             case .success(let image):
-                MGLogger.debug("사진 저장")
+                print("사진 저장")
                 UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
             case .failure(let error):
-                MGLogger.error("저장 실패: \(error)")
+                print("저장 실패: \(error)")
             }
         }
     }
@@ -63,22 +122,22 @@ public class DemoViewController: UIViewController {
         
         if isOn {
             cameraView.setFlashMode(.on)
-            MGLogger.debug("on")
+            print("on")
         } else {
             cameraView.setFlashMode(.off)
-            MGLogger.debug("off")
+            print("off")
         }
     }
     
-    func reversalDidTap(_ sender: Any) {
+    func reversalDidTap() {
         isOn.toggle()
         
         if isOn {
             cameraView.setCameraPosition(.back)
-            MGLogger.debug("back")
+            print("back")
         } else {
             cameraView.setCameraPosition(.front)
-            MGLogger.debug("front")
+            print("front")
         }
     }
     
