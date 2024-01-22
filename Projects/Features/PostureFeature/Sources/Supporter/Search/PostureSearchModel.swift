1b989f8f0b7de723c39da1c3e067ea284a7c44ed
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-01-22 15:15:43 +0900

    ⚓️ :: [#131] 자세 - 검색 - Model 추가

--- Projects/Features/PostureFeature/Sources/Supporter/Search/PostureSearchModel.swift
+++ Projects/Features/PostureFeature/Sources/Supporter/Search/PostureSearchModel.swift
@@ -1,9 +1,48 @@
-//
-//  PostureSearchModel.swift
-//  PostureFeatureInterface
-//
-//  Created by 이은호 on 1/22/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import DSKit
+
+public struct SearchElement {
+    public var image: UIImage
+    public var exerciseName: String
+    public var exercisePart: String
+}
+
+public enum PostureSearchModel {
+    case first
+    case second
+    
+    public var data: [SearchElement] {
+        switch self {
+        case .first:
+            return [
+                SearchElement(image: DSKitAsset.Assets.postureChestTest6.image, exerciseName: "바벨 백스쿼트", exercisePart: "하체"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest7.image, exerciseName: "바벨 불가리안 스플릿 스쿼트", exercisePart: "하체"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest2.image, exerciseName: "바벨 힙 쓰러스트", exercisePart: "하체"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest3.image, exerciseName: "점프 스쿼트", exercisePart: "등"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest5.image, exerciseName: "인클라인 바벨 로우", exercisePart: "등"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest6.image, exerciseName: "바벨 백스쿼트", exercisePart: "하체"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest7.image, exerciseName: "바벨 불가리안 스플릿 스쿼트", exercisePart: "하체"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest2.image, exerciseName: "바벨 힙 쓰러스트", exercisePart: "하체"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest3.image, exerciseName: "점프 스쿼트", exercisePart: "등"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest5.image, exerciseName: "인클라인 바벨 로우", exercisePart: "등")
+                
+            ]
+        case .second:
+            return [
+                SearchElement(image: DSKitAsset.Assets.postureChestTest6.image, exerciseName: "바벨 컬", exercisePart: "필"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest7.image, exerciseName: "덤벨 컬", exercisePart: "필"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest2.image, exerciseName: "이지바 컬", exercisePart: "팔"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest3.image, exerciseName: "덤벨 헤머 컬", exercisePart: "팔"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest5.image, exerciseName: "C컬", exercisePart: "팔"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest6.image, exerciseName: "바벨 백스쿼트", exercisePart: "하체"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest7.image, exerciseName: "바벨 불가리안 스플릿 스쿼트", exercisePart: "하체"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest2.image, exerciseName: "바벨 힙 쓰러스트", exercisePart: "하체"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest3.image, exerciseName: "점프 스쿼트", exercisePart: "등"),
+                SearchElement(image: DSKitAsset.Assets.postureChestTest5.image, exerciseName: "인클라인 바벨 로우", exercisePart: "등")
+            ]
+        }
+    }
+
+    
+}
