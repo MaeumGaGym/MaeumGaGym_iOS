a6140bd4105a9bdba7b3b1b34101638c25427f5d
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-01-22 13:37:43 +0900

    ⚓️ :: [#131] 자세 - 검색 - tableViewCell 추가

--- Projects/Features/PostureFeature/Sources/Supporter/Search/PostureSearchTableViewCell.swift
+++ Projects/Features/PostureFeature/Sources/Supporter/Search/PostureSearchTableViewCell.swift
@@ -1,9 +1,67 @@
-//
-//  PostureSearchTableViewCell.swift
-//  PostureFeatureInterface
-//
-//  Created by 이은호 on 1/22/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
+import SnapKit
+import Then
+import DSKit
 
-import Foundation
+public class PostureSearchTableViewCell: UITableViewCell {
+    static let identifier: String = "PostureSearchTableViewCell"
+    
+    private var searchImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.backgroundColor = DSKitAsset.Colors.gray50.color
+        $0.layer.cornerRadius = 8.0
+    }
+    
+    private var exerciseNameLabel = UILabel().then {
+        $0.font = UIFont.Pretendard.bodyMedium
+        $0.textColor = .black
+        $0.textAlignment = .left
+    }
+    
+    private var exercisePartLabel = UILabel().then {
+        $0.font = UIFont.Pretendard.bodyMedium
+        $0.textColor = DSKitAsset.Colors.gray400.color
+        $0.textAlignment = .left
+    }
+    
+    public func setup(image: UIImage, name: String, part: String) {
+        searchImageView.image = image
+        exerciseNameLabel.text = name
+        exercisePartLabel.text = part
+        
+        addViews()
+        setupViews()
+    }
+    
+    private func addViews() {
+        addSubviews([searchImageView, exerciseNameLabel, exercisePartLabel])
+    }
+    
+    private func setupViews() {
+        searchImageView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(16.0)
+            $0.leading.equalToSuperview().offset(20.0)
+            $0.width.height.equalTo(64.0)
+        }
+        
+        exerciseNameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(26.0)
+            $0.leading.equalTo(searchImageView.snp.trailing).offset(18.0)
+            $0.width.equalToSuperview()
+            $0.height.equalTo(20.0)
+        }
+        
+        exercisePartLabel.snp.makeConstraints {
+            $0.top.equalTo(exerciseNameLabel.snp.bottom).offset(4.0)
+            $0.leading.equalTo(searchImageView.snp.trailing).offset(18.0)
+            $0.width.equalToSuperview()
+            $0.height.equalTo(20.0)
+        }
+    }
+    
+    
+    
+    
+    
+    
+}
