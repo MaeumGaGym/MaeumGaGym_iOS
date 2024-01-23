2cb77c0a546c0fe7af856945c29bfd82343aab48
s <tylereunholee010203@gmail.com>
2024-01-23 13:32:19 +0900

    🍗 :: [#135] 파일 구조 변경

--- Projects/Features/PostureFeature/Sources/PostureScene/ViewModel/Tab/PostureViewModel.swift
+++ Projects/Features/PostureFeature/Sources/PostureScene/ViewModel/Tab/PostureViewModel.swift
@@ -4,7 +4,7 @@ import RxCocoa
 import RxSwift
 import Core
 
-public class PostureViewModel: BaseViewModel {
+public class PostureRecommandViewModel: BaseViewModel {
     public func transform(_ input: Input) -> Output {
         return Output()
     }
