5065ca3d04e5ec5497944a5d2034ec420b8bec69
s <tylereunholee010203@gmail.com>
2024-01-23 19:39:44 +0900

    ⚓️ :: [#135] 자세 - 맨몸 임시 VM 추가

---
+++ Projects/Features/PostureFeature/Sources/PostureScene/ViewModel/Tab/PostureBodyViewModel.swift
@@ -0,0 +1,24 @@
+import Foundation
+import RxFlow
+import RxCocoa
+import RxSwift
+import Core
+
+public class PostureBodyViewModel: BaseViewModel {
+    public func transform(_ input: Input) -> Output {
+        return Output()
+    }
+
+    public struct Input {
+
+    }
+
+    public struct Output {
+
+    }
+
+    public init() {
+
+    }
+}
+
