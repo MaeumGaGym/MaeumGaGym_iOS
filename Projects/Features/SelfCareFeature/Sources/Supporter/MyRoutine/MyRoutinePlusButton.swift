4b84933a07b23b6f6c519f02c517ccbd21dd9662
s <tylereunholee010203@gmail.com>
2024-01-29 14:26:59 +0900

    ⚓️ :: [#146] 자기관리 - 내 루틴 추가 버튼 추가

--- Projects/Features/SelfCareFeature/Sources/Supporter/MyRoutine/MyRoutinePlusButton.swift
+++ Projects/Features/SelfCareFeature/Sources/Supporter/MyRoutine/MyRoutinePlusButton.swift
@@ -1,9 +1,35 @@
-//
-//  MyRoutinePlusButton.swift
-//  SelfCareFeature
-//
-//  Created by 이은호 on 1/29/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import SnapKit
+import Then
+
+import DSKit
+import Core
+
+public class MyRoutinePlusButton: UIButton {
+    
+    private var plusImage = UIImageView().then {
+        $0.image = DSKitAsset.Assets.selfCareAdd.image
+    }
+    
+    private var textLabel = UILabel().then {
+        $0.font = UIFont.Pretendard.labelLarge
+        $0.text = "루틴 추가하기"
+        $0.textColor = .white
+        $0.contentMode = .left
+    }
+    
+    public init (
+        text: String? = "루틴 추가하기"
+    ) {
+        super.init(frame: .zero)
+        
+        textLabel.text = text
+        layer.cornerRadius = 8.0
+        layer.backgroundColor = DSKitAsset.Colors.blue500.color.cgColor
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
