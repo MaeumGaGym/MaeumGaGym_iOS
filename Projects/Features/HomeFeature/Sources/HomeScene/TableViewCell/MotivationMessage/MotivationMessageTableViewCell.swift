0c2f25eddc2cd790ce57c1375cf5865afbb8861f
junha <goodjunha@gmail.com>
2024-01-29 14:26:12 +0900

    🍗:: [#148] MotivationMessageTableViewCell 리펙토링

--- Projects/Features/HomeFeature/Sources/HomeScene/TableViewCell/MotivationMessage/MotivationMessageTableViewCell.swift
+++ Projects/Features/HomeFeature/Sources/HomeScene/TableViewCell/MotivationMessage/MotivationMessageTableViewCell.swift
@@ -18,46 +18,52 @@ public class MotivationMessageTableViewCell: UITableViewCell {
         $0.font = UIFont.Pretendard.titleMedium
         $0.textColor = DSKitAsset.Colors.blue500.color
     }
+
     private let authorLabel = UILabel().then {
         $0.font = UIFont.Pretendard.labelMedium
         $0.textColor = DSKitAsset.Colors.gray500.color
     }
 
-     private var disposeBag = DisposeBag()
+    private var disposeBag = DisposeBag()
 
-     func configure(with message: MotivationMessageModel) {
-         messageLabel.text = "\"\(message.text)\""
-         authorLabel.text = "\(message.author)"
-     }
-
-     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-         super.init(style: style, reuseIdentifier: reuseIdentifier)
-         setupUI()
-     }
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
 
     required public init?(coder aDecoder: NSCoder) {
-         fatalError("init(coder:) has not been implemented")
-     }
+        fatalError("init(coder:) has not been implemented")
+    }
 
-     private func setupUI() {
-         self.backgroundColor = DSKitAsset.Colors.gray25.color
-         addSubview(messageLabel)
-         messageLabel.snp.makeConstraints {
-             $0.top.equalToSuperview()
-             $0.leading.trailing.equalToSuperview().inset(12)
-             $0.bottom.lessThanOrEqualToSuperview().inset(8).priority(.high)
-         }
+    func configure(with message: MotivationMessageModel) {
+        messageLabel.text = "\"\(message.text)\""
+        authorLabel.text = "\(message.author)"
+    }
 
-         addSubview(authorLabel)
-         authorLabel.snp.makeConstraints {
-             $0.top.equalTo(messageLabel.snp.bottom).offset(8)
-             $0.leading.equalTo(messageLabel.snp.leading)
-             $0.bottom.equalToSuperview()
-         }
-         rx.deallocated
-             .bind { [weak self] in
-                 self?.disposeBag = DisposeBag()
-             }
-             .disposed(by: disposeBag)
-     }
+    private func setupUI() {
+        backgroundColor = DSKitAsset.Colors.gray25.color
+
+        addSubviews([messageLabel, authorLabel])
+        messageLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(12)
+            $0.bottom.lessThanOrEqualToSuperview().inset(8).priority(.high)
+        }
+
+        authorLabel.snp.makeConstraints {
+            $0.top.equalTo(messageLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(messageLabel.snp.leading)
+            $0.bottom.equalToSuperview()
+        }
+
+        setupRxBindings()
+    }
+
+    private func setupRxBindings() {
+        rx.deallocated
+            .subscribe(onNext: { [weak self] in
+                self?.disposeBag = DisposeBag()
+            })
+            .disposed(by: disposeBag)
+    }
 }
