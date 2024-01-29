354eafafebf7a4c42800f6507e53d38b45772b60
junha <goodjunha@gmail.com>
2024-01-29 13:56:16 +0900

    🍗:: [#148] 홈 리펙토링

--- Projects/Features/HomeFeature/Sources/HomeScene/View/HomeViewController.swift
+++ Projects/Features/HomeFeature/Sources/HomeScene/View/HomeViewController.swift
@@ -16,6 +16,7 @@ enum HomeCell {
     case motivationMessage
     case stepNumber
     case routine
+    case extra
 }
 
 public class HomeViewController: BaseViewController<Any>, Stepper {
@@ -23,6 +24,8 @@ public class HomeViewController: BaseViewController<Any>, Stepper {
     public var steps = PublishRelay<Step>()
     private var cellList: [UITableViewCell] = []
     private var cells: [HomeCell] = []
+    
+    private lazy var naviBar = MainNavigationBar()
 
     // text 길이의 맞게 View가 유동적으로 늘어나야함
     let quotes: MotivationMessageModel = MotivationMessageModel(
@@ -39,13 +42,22 @@ public class HomeViewController: BaseViewController<Any>, Stepper {
         RoutineModel(exercise: "러닝", sets: 5, reps: 10)
     ]
 
+    var extras: [ExtrasModel] = [
+        ExtrasModel(image: DSKitAsset.Assets.basicProfile.image,
+                    titleName: "칼로리 계산기",
+                    description: "먹은 음식의 칼로리를\n계산해 보세요."),
+        ExtrasModel(image: DSKitAsset.Assets.basicProfile.image,
+                    titleName: "와카타임",
+                    description: "지금까지 한 운동 시간을\n확인해 보세요.")
+    ]
+
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero)
         tableView.delegate = self
         tableView.dataSource = self
         tableView.backgroundColor = DSKitAsset.Colors.gray25.color
-        tableView.showsHorizontalScrollIndicator = false
         tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
         tableView.register(
             MotivationMessageTableViewCell.self,
             forCellReuseIdentifier: MotivationMessageTableViewCell.identifier
@@ -56,26 +68,33 @@ public class HomeViewController: BaseViewController<Any>, Stepper {
         tableView.register(
             RoutineTableViewCell.self,
             forCellReuseIdentifier: RoutineTableViewCell.identifier)
+        tableView.register(ExtraTableViewCell.self,
+                           forCellReuseIdentifier: ExtraTableViewCell.identifier)
         return tableView
     }()
 
     public override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-
+        self.navigationController?.isNavigationBarHidden = true
         tableView.frame = tableView.frame.inset(by: UIEdgeInsets(top: .zero, left: 20, bottom: .zero, right: 20))
     }
 
     public override func attribute() {
-        self.title = "Home"
+        self.title = "asdfasdfas"
         self.view.backgroundColor = DSKitAsset.Colors.gray25.color
 
         addCells()
     }
 
     public override func layout() {
-        self.view.addSubview(self.tableView)
+        self.view.addSubviews([naviBar, tableView])
+        
+        naviBar.snp.makeConstraints { make in
+            make.leading.top.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+        
         self.tableView.snp.makeConstraints {
-            $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            $0.top.equalTo(naviBar.snp.bottom).offset(20.0)
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
             $0.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
@@ -86,11 +105,12 @@ public class HomeViewController: BaseViewController<Any>, Stepper {
         self.cells.append(.motivationMessage)
         self.cells.append(.stepNumber)
         self.cells.append(.routine)
+        self.cells.append(.extra)
     }
 
     func heightForCell(with routines: [RoutineModel]) -> CGFloat {
         let cellHeight: CGFloat = 64.0
-        return max(CGFloat(routines.count) * cellHeight, cellHeight) + 72
+        return max(CGFloat(routines.count) * cellHeight, cellHeight) + 92
     }
 }
 
@@ -100,9 +120,11 @@ extension HomeViewController: UITableViewDelegate {
         case .motivationMessage:
             return 60.0
         case .stepNumber:
-            return 80.0
+            return 120.0
         case .routine:
             return heightForCell(with: routines)
+        case .extra:
+            return 260.0
         }
     }
 
@@ -113,6 +135,8 @@ extension HomeViewController: UITableViewDelegate {
         case .stepNumber:
             cell.separatorInset = UIEdgeInsets(top: 24, left: 20, bottom: 0, right: 20)
         case .routine:
+            cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        case .extra:
             cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         }
     }
@@ -159,6 +183,15 @@ extension HomeViewController: UITableViewDataSource {
             }
             cell.routines = routines
             cell.layer.cornerRadius = 16.0
+
+            return cell
+        case .extra:
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: ExtraTableViewCell.identifier
+            ) as? ExtraTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.extras = extras
 
             return cell
         }
