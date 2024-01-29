c3b7463d77346db3189abff5b424aef21bc887e8
s <tylereunholee010203@gmail.com>
2024-01-29 14:26:22 +0900

    ⚓️ :: [#146] 마음가짐 공유 상태 View 추가

--- Projects/Modules/DSKit/Sources/Components/MaeumGaGymVIew/MG+shareStateView.swift
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymVIew/MG+shareStateView.swift
@@ -1,9 +1,50 @@
-//
-//  MG+shareStateView.swift
-//  DSKit
-//
-//  Created by 이은호 on 1/29/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import RxSwift
+import RxCocoa
+
+import SnapKit
+import Then
+
+open class MGShareStateView: UIView {
+    
+    private let sharedLabel = UILabel().then {
+        $0.text = "공유됨"
+        $0.font = UIFont.Pretendard.labelMedium
+        $0.textColor = DSKitAsset.Colors.blue500.color
+    }
+    
+    private let sharedImage = UIImageView().then {
+        $0.image = DSKitAsset.Assets.selfCareEarth.image
+    }
+    
+    public init() {
+        super.init(frame: .zero)
+        
+        self.layer.cornerRadius = 18.0
+        self.backgroundColor = .white
+        
+        setupUI()
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        addSubviews([sharedLabel, sharedImage])
+        
+        sharedLabel.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(8.0)
+            $0.leading.equalToSuperview().offset(16.0)
+            $0.width.equalTo(42.0)
+            $0.height.equalTo(20.0)
+        }
+        
+        sharedImage.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(10.0)
+            $0.trailing.equalToSuperview().offset(-16.0)
+            $0.width.height.equalTo(16.0)
+        }
+    }
+}
