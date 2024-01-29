74e30f5395da96ffe92d82b7ae0fcf1f7b55aeed
junha <goodjunha@gmail.com>
2024-01-29 13:54:03 +0900

    ⚓️:: [#148] MainNavigationBar 추가

---
+++ Projects/Features/HomeFeature/Sources/HomeScene/NavigationBar/MainNavigationBar.swift
@@ -0,0 +1,81 @@
+//
+//  MainNavigationBar.swift
+//  HomeFeature
+//
+//  Created by 박준하 on 1/29/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import UIKit
+
+import Core
+import DSKit
+
+final class MainNavigationBar: UIView {
+            
+    public var rightButtonTap: (() -> Void)?
+        
+    private let logoImageView = UIImageView().then {
+        $0.image = DSKitAsset.Assets.mainTitle.image.withRenderingMode(.alwaysOriginal)
+        $0.contentMode = .scaleToFill
+    }
+    
+    private let rightButton = UIButton(type: .custom).then {
+        $0.setImage(DSKitAsset.Assets.setting.image, for: .normal)
+    }
+    
+    private lazy var rightItemsStackView = UIStackView(arrangedSubviews: [rightButton]).then {
+        $0.axis = .horizontal
+        $0.spacing = 4
+        $0.distribution = .fillEqually
+    }
+
+    // MARK: - initialization
+    
+    init() {
+        super.init(frame: .zero)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Methods
+
+extension MainNavigationBar {
+    @discardableResult
+    public func setRightButtonImage(image: UIImage) -> Self {
+        self.rightButton.setImage(image, for: .normal)
+        return self
+    }
+}
+
+// MARK: - UI & Layout
+
+extension MainNavigationBar {
+    private func setUI() {
+        self.backgroundColor = DSKitAsset.Colors.gray25.color
+    }
+    
+    private func setLayout() {
+        self.addSubviews([logoImageView, rightItemsStackView])
+        
+        self.snp.makeConstraints { make in
+            make.height.equalTo(42)
+        }
+        
+        logoImageView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.equalToSuperview().inset(20)
+            make.width.equalTo(129)
+        }
+        
+        rightItemsStackView.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.trailing.equalToSuperview().inset(20)
+        }
+    }
+}
