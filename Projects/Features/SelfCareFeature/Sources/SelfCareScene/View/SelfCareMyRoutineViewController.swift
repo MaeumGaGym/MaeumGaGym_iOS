50f0f2be8d7f62ef476ce0550756ae3c8858c2f1
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-01-29 20:13:48 +0900

    🍗 :: [#146] 코드 리펙토링
    없어진 코드 되살림

--- Projects/Features/SelfCareFeature/Sources/SelfCareScene/View/SelfCareMyRoutineViewController.swift
+++ Projects/Features/SelfCareFeature/Sources/SelfCareScene/View/SelfCareMyRoutineViewController.swift
@@ -1,27 +1,26 @@
 import UIKit
-
 import SnapKit
 import Then
-
 import Core
 import DSKit
-
 public class SelfCareMyRoutineViewController: BaseViewController<SelfCareMyRoutineViewModel> {
-    
     private var myRoutineModel = SelfCareMyRoutineModel.myRoutine
     
+    private var containerView = UIView()
     private var headerView = UIView()
     
     private let myRoutineTitleLabel = UILabel().then {
         $0.text = "내 루틴"
         $0.textColor = .black
         $0.contentMode = .left
+        $0.font = UIFont.Pretendard.titleLarge
     }
     
     private let myRoutineSubTitleLabel = UILabel().then {
         $0.text = "나만의 루틴을 구성하여\n규칙적인 운동을 해보세요."
         $0.numberOfLines = 2
         $0.textColor = DSKitAsset.Colors.gray600.color
+        $0.font = UIFont.Pretendard.bodyMedium
     }
     
     private var myRoutineTableView = UITableView().then {
@@ -29,23 +28,41 @@ public class SelfCareMyRoutineViewController: BaseViewController<SelfCareMyRouti
         $0.showsHorizontalScrollIndicator = false
         $0.backgroundColor = .white
         $0.separatorStyle = .none
-        $0.delegate = self
-        $0.dataSource = self
-        
         $0.register(MyRoutineTableViewCell.self,
                     forCellReuseIdentifier: MyRoutineTableViewCell.identifier)
     }
     
     private var plusRoutineButton = MyRoutinePlusButton(text: "루틴 추가하기")
     
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+    
+    public override func attribute() {
+        
+        myRoutineTableView.delegate = self
+        myRoutineTableView.dataSource = self
+    }
+    
     public override func layout() {
         
-        headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100.0))
+        headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 144.0))
+        view.addSubview(headerView)
         
-        headerView.addSubview(myRoutineTitleLabel)
-        headerView.addSubview(myRoutineSubTitleLabel)
+        headerView.addSubview(containerView)
         
-        myRoutineTableView.snp.makeConstraints {
+        containerView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(24.0)
+            $0.leading.trailing.equalToSuperview().inset(20.0)
+            $0.bottom.equalToSuperview().inset(20.0)
+        }
+        
+        containerView.addSubview(myRoutineTitleLabel)
+        containerView.addSubview(myRoutineSubTitleLabel)
+        
+        myRoutineTitleLabel.snp.makeConstraints {
             $0.top.leading.equalToSuperview()
             $0.width.equalToSuperview()
             $0.height.equalTo(48.0)
@@ -57,35 +74,50 @@ public class SelfCareMyRoutineViewController: BaseViewController<SelfCareMyRouti
             $0.height.equalTo(40.0)
         }
         
-        myRoutineTableView.tableHeaderView = headerView
         view.addSubview(myRoutineTableView)
-        
+        myRoutineTableView.tableHeaderView = headerView
         myRoutineTableView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalToSuperview()
-            $0.height.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+        
+        view.addSubview(plusRoutineButton)
+        plusRoutineButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-54.0)
+            $0.leading.trailing.equalToSuperview().inset(20.0)
+            $0.height.equalTo(58.0)
         }
         
     }
 }
-
 extension SelfCareMyRoutineViewController: UITableViewDelegate {
     public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 82
+        if indexPath.row == myRoutineModel.data.count + 1 {
+            return 100 // 마지막 셀의 높이를 100으로 설정
+        } else {
+            return 94
+        }
     }
 }
-
 extension SelfCareMyRoutineViewController: UITableViewDataSource {
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        myRoutineModel.data.count + 1
     }
-    
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(
-            withIdentifier: MyRoutineTableViewCell.identifier,
-            for: indexPath) as? MyRoutineTableViewCell
-        let routine = SelfCareMyRoutineViewModel.data[indexPath.row]
-        cell?.setup(name: routine.name, state: routine.routineState, shared: routine.sharedState)
-        return cell ?? UITableViewCell()
+        if indexPath.row == myRoutineModel.data.count {
+            let cell = UITableViewCell()
+            cell.backgroundColor = .white
+            cell.selectionStyle = .none
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: MyRoutineTableViewCell.identifier,
+                for: indexPath) as? MyRoutineTableViewCell
+            let routine = myRoutineModel.data[indexPath.row]
+            cell?.setup(name: routine.name, state: routine.routineState, shared: routine.sharedState)
+            cell?.selectionStyle = .none
+            return cell ?? UITableViewCell()
+        }
     }
 }
