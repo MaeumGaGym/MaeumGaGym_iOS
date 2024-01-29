efa9dea1f866054301847a824597df4f7b612da6
junha <goodjunha@gmail.com>
2024-01-29 14:09:26 +0900

    🍗:: [#148] HomeViewController 리펙토링

--- Projects/Features/HomeFeature/Sources/HomeScene/View/HomeViewController.swift
+++ Projects/Features/HomeFeature/Sources/HomeScene/View/HomeViewController.swift
@@ -51,109 +51,109 @@ public class HomeViewController: BaseViewController<Any>, Stepper {
                     description: "지금까지 한 운동 시간을\n확인해 보세요.")
     ]
 
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: .zero)
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.backgroundColor = DSKitAsset.Colors.gray25.color
-        tableView.separatorStyle = .none
-        tableView.showsVerticalScrollIndicator = false
-        tableView.register(
-            MotivationMessageTableViewCell.self,
-            forCellReuseIdentifier: MotivationMessageTableViewCell.identifier
-        )
-        tableView.register(
-            StepTableViewCell.self,
-            forCellReuseIdentifier: StepTableViewCell.identifier)
-        tableView.register(
-            RoutineTableViewCell.self,
-            forCellReuseIdentifier: RoutineTableViewCell.identifier)
-        tableView.register(ExtraTableViewCell.self,
-                           forCellReuseIdentifier: ExtraTableViewCell.identifier)
-        return tableView
-    }()
+    private lazy var tableView: UITableView = UITableView().then {
+        $0.delegate = self
+        $0.dataSource = self
+        $0.backgroundColor = DSKitAsset.Colors.gray25.color
+        $0.separatorStyle = .none
+        $0.showsVerticalScrollIndicator = false
 
-    public override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        self.navigationController?.isNavigationBarHidden = true
+        $0.register(MotivationMessageTableViewCell.self,
+                    forCellReuseIdentifier: MotivationMessageTableViewCell.identifier)
+        $0.register(StepTableViewCell.self,
+                    forCellReuseIdentifier: StepTableViewCell.identifier)
+        $0.register(RoutineTableViewCell.self,
+                    forCellReuseIdentifier: RoutineTableViewCell.identifier)
+        $0.register(ExtraTableViewCell.self,
+                    forCellReuseIdentifier: ExtraTableViewCell.identifier)
+    }
+
+    public override func configureNavigationBar() {
+        super.configureNavigationBar()
+        navigationController?.isNavigationBarHidden = true
         tableView.frame = tableView.frame.inset(by: UIEdgeInsets(top: .zero, left: 20, bottom: .zero, right: 20))
     }
 
     public override func attribute() {
-        self.title = "asdfasdfas"
-        self.view.backgroundColor = DSKitAsset.Colors.gray25.color
-
+        view.backgroundColor = DSKitAsset.Colors.gray25.color
         addCells()
     }
 
     public override func layout() {
-        self.view.addSubviews([naviBar, tableView])
+        view.addSubviews([naviBar, tableView])
         
-        naviBar.snp.makeConstraints { make in
-            make.leading.top.trailing.equalTo(view.safeAreaLayoutGuide)
+        naviBar.snp.makeConstraints {
+            $0.leading.top.trailing.equalTo(view.safeAreaLayoutGuide)
         }
         
-        self.tableView.snp.makeConstraints {
+        tableView.snp.makeConstraints {
             $0.top.equalTo(naviBar.snp.bottom).offset(20.0)
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
-            $0.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
         }
     }
 
     private func addCells() {
-        self.cells.append(.motivationMessage)
-        self.cells.append(.stepNumber)
-        self.cells.append(.routine)
-        self.cells.append(.extra)
+        cells.append(.motivationMessage)
+        cells.append(.stepNumber)
+        cells.append(.routine)
+        cells.append(.extra)
     }
 
-    func heightForCell(with routines: [RoutineModel]) -> CGFloat {
+    func routineheightForCell(with routines: [RoutineModel]) -> CGFloat {
         let cellHeight: CGFloat = 64.0
         return max(CGFloat(routines.count) * cellHeight, cellHeight) + 92
     }
 }
 
 extension HomeViewController: UITableViewDelegate {
-    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    public func tableView(
+        _ tableView: UITableView,
+        heightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
         switch self.cells[indexPath.item] {
         case .motivationMessage:
             return 60.0
         case .stepNumber:
             return 120.0
         case .routine:
-            return heightForCell(with: routines)
+            return routineheightForCell(with: routines)
         case .extra:
             return 260.0
         }
     }
 
-    public func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    public func tableView(
+        _ tableView: UITableView,
+        willDisplay cell: UITableViewCell,
+        forRowAt indexPath: IndexPath
+    ) {
         switch self.cells[indexPath.item] {
-        case .motivationMessage:
-            cell.separatorInset = UIEdgeInsets(top: 0, left: 20, bottom: 24, right: 20)
-        case .stepNumber:
-            cell.separatorInset = UIEdgeInsets(top: 24, left: 20, bottom: 0, right: 20)
-        case .routine:
-            cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        case .extra:
+        case .motivationMessage, .stepNumber, .routine, .extra:
             cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         }
     }
 }
 
 extension HomeViewController: UITableViewDataSource {
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.cells.count
+    public func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return cells.count
     }
 
     public func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    public func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
 
-        switch self.cells[indexPath.item] {
+        switch cells[indexPath.item] {
         case .motivationMessage:
             guard let cell = tableView.dequeueReusableCell(
                 withIdentifier: MotivationMessageTableViewCell.identifier
