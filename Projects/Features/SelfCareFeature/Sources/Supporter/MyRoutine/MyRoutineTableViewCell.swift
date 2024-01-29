121df5d787a6adc369e93ad9810560d5c6cba8d5
s <tylereunholee010203@gmail.com>
2024-01-29 14:26:45 +0900

    ⚓️ :: [#146] 자기관리 - 내 루틴 tableViewCell 추가

--- Projects/Features/SelfCareFeature/Sources/Supporter/MyRoutine/MyRoutineTableViewCell.swift
+++ Projects/Features/SelfCareFeature/Sources/Supporter/MyRoutine/MyRoutineTableViewCell.swift
@@ -1,9 +1,115 @@
-//
-//  MyRoutineTableViewCell.swift
-//  SelfCareFeatureInterface
-//
-//  Created by 이은호 on 1/29/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import SnapKit
+import Then
+
+import Core
+import DSKit
+
+public enum SharedState {
+    case yesShared
+    case notShared
+}
+
+public enum RoutineState {
+    case useRoutine
+    case keepRoutine
+}
+
+public class MyRoutineTableViewCell: BaseTableViewCell {
+    
+    static let identifier: String = "MyRoutineTableViewCell"
+    
+    private var routineNameLabel = UILabel().then {
+        $0.font = UIFont.Pretendard.labelLarge
+        $0.textColor = .black
+        $0.numberOfLines = 1
+    }
+    
+    private var routineStateLabel = UILabel().then {
+        $0.font = UIFont.Pretendard.bodySmall
+        $0.numberOfLines = 1
+        $0.textColor = DSKitAsset.Colors.gray400.color
+    }
+    
+    private var sharedView = MGShareStateView().then {
+        $0.isHidden = true
+    }
+    
+    private let dotsButton = UIButton().then {
+        $0.setImage(DSKitAsset.Assets.selfCareDots.image, for: .normal)
+    }
+    
+    public func setup(name: String, state: String, shared: SharedState) {
+        routineNameLabel.text = name
+        routineStateLabel.text = state
+        
+        SharedViewState(sharedState: shared)
+        addViews()
+        layout()
+        
+    }
+    
+    override public func addViews() {
+        
+        [
+            routineNameLabel,
+            routineStateLabel,
+            sharedView,
+            dotsButton
+        ].forEach { contentView.addSubview($0) }
+    }
+    
+    override public func layout() {
+        
+        routineNameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(18.0)
+            $0.leading.equalToSuperview().offset(20.0)
+            $0.width.equalTo(75.0)
+            $0.height.equalTo(24.0)
+        }
+        
+        routineStateLabel.snp.makeConstraints {
+            $0.top.equalTo(routineNameLabel.snp.bottom).offset(4.0)
+            $0.leading.equalToSuperview().offset(20.0)
+            $0.width.equalTo(37.0)
+            $0.height.equalTo(18.0)
+        }
+        
+        sharedView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(23.0)
+            $0.trailing.equalTo(dotsButton.snp.leading).offset(-12.0)
+            $0.width.equalTo(98.0)
+            $0.height.equalTo(36.0)
+        }
+        
+        dotsButton.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(29.0)
+            $0.trailing.equalToSuperview().offset(-20.0)
+        }
+    }
+    
+    public func SharedViewState(sharedState: SharedState) {
+        switch sharedState {
+        case .yesShared:
+            sharedView.isHidden = false
+        case .notShared:
+            sharedView.isHidden = true
+        }
+    }
+    
+    public func routineState(routineState: RoutineState) {
+        switch routineState {
+        case .useRoutine:
+            routineStateLabel.text = "사용중"
+            routineStateLabel.textColor = DSKitAsset.Colors.blue500.color
+        case .keepRoutine:
+            routineStateLabel.text = "보관중"
+            routineStateLabel.textColor = DSKitAsset.Colors.gray400.color
+        }
+    }
+    
+    public func changeRoutineName(text: String) {
+        routineNameLabel.text = text
+    }
+}
