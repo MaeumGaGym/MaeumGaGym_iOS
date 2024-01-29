65c2871e44993af64825df232481b80df4acd11b
s <tylereunholee010203@gmail.com>
2024-01-29 14:27:48 +0900

    ⚓️ :: [#146] 자기관리 - 내 루틴 임시 ViewModel 추가

--- Projects/Features/SelfCareFeature/Sources/SelfCareScene/ViewModel/SelfCareMyRoutine+VM.swift
+++ Projects/Features/SelfCareFeature/Sources/SelfCareScene/ViewModel/SelfCareMyRoutine+VM.swift
@@ -1,9 +1,28 @@
-//
-//  SelfCareMyRoutine+VM.swift
-//  SelfCareFeatureInterface
-//
-//  Created by 이은호 on 1/29/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
-
 import Foundation
+
+import RxFlow
+import RxCocoa
+import RxSwift
+
+import Core
+
+public class SelfCareMyRoutineViewModel: BaseViewModel {
+    public func transform(_ input: Input) -> Output {
+        return Output()
+    }
+
+    public struct Input {
+
+    }
+
+    public struct Output {
+
+    }
+
+    public init() {
+
+    }
+}
+
+
+
