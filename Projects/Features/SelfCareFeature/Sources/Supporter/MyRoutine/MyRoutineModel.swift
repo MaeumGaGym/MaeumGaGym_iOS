6426b4aa719f1b7ac91ac70d371f93e8cf95a0e1
s <tylereunholee010203@gmail.com>
2024-01-29 14:27:15 +0900

    ⚓️ :: [#146] 자기관리 - 내 루틴 model 추가

--- Projects/Features/SelfCareFeature/Sources/Supporter/MyRoutine/MyRoutineModel.swift
+++ Projects/Features/SelfCareFeature/Sources/Supporter/MyRoutine/MyRoutineModel.swift
@@ -1,9 +1,31 @@
-//
-//  MyRoutineModel.swift
-//  SelfCareFeature
-//
-//  Created by 이은호 on 1/29/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import DSKit
+
+public struct SelfCareContent {
+    var name: String
+    var routineState: RoutineState
+    var sharedState: SharedState
+}
+
+public enum SelfCareMyRoutineModel {
+    case myRoutine
+    
+    var data: [SelfCareContent] {
+        switch self {
+        case .myRoutine:
+            return [
+                SelfCareContent(name: "주말 루틴", routineState: .useRoutine, sharedState: .yesShared),
+                SelfCareContent(name: "매일 루틴", routineState: .keepRoutine, sharedState: .notShared),
+                SelfCareContent(name: "평일 루틴", routineState: .useRoutine, sharedState: .notShared),
+                SelfCareContent(name: "말왕 루틴", routineState: .keepRoutine, sharedState: .yesShared),
+                SelfCareContent(name: "루틴 루틴", routineState: .keepRoutine, sharedState: .yesShared),
+                SelfCareContent(name: "이은호 루틴", routineState: .keepRoutine, sharedState: .notShared),
+                SelfCareContent(name: "부현수 루틴", routineState: .useRoutine, sharedState: .notShared),
+                SelfCareContent(name: "이태영 루틴", routineState: .useRoutine, sharedState: .yesShared),
+                SelfCareContent(name: "플러터1 루틴", routineState: .keepRoutine, sharedState: .notShared),
+                SelfCareContent(name: "플러터2 루틴", routineState: .useRoutine, sharedState: .yesShared),
+            ]
+        }
+    }
+}
