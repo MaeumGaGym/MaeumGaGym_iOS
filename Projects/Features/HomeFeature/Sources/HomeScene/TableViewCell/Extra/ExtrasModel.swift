dfacb0d1ccd331a95276017011b8077bded4f0f8
junha <goodjunha@gmail.com>
2024-01-29 13:54:47 +0900

    ⚓️:: [#148] ExtrasModel 추가

---
+++ Projects/Features/HomeFeature/Sources/HomeScene/TableViewCell/Extra/ExtrasModel.swift
@@ -0,0 +1,15 @@
+//
+//  ExtrasModel.swift
+//  HomeFeature
+//
+//  Created by 박준하 on 1/28/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import UIKit
+
+public struct ExtrasModel {
+    public var image: UIImage
+    public var titleName: String
+    public var description: String
+}
