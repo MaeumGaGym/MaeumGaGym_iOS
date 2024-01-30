218cb53621c368338f55ef292e56d1a1806994d7
s <tylereunholee010203@gmail.com>
2024-01-30 15:15:18 +0900

    ⚓️ :: [#152] 자기관리 - 내 루틴 - 디테일
    model 추가

--- Projects/Features/SelfCareFeature/Sources/Supporter/MyRoutine/Detail/MyRoutineDetailModel.swift
+++ Projects/Features/SelfCareFeature/Sources/Supporter/MyRoutine/Detail/MyRoutineDetailModel.swift
@@ -1,9 +1,33 @@
-//
-//  MyRoutineDetailModel.swift
-//  SelfCareFeatureInterface
-//
-//  Created by 이은호 on 1/30/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import DSKit
+
+public struct SelfCareMyRoutineDetail {
+    var image: UIImage
+    var name: String
+    var routine: String
+}
+
+public enum SelfCareMyRoutineDetailModel {
+    case data
+    
+    var data: [SelfCareMyRoutineDetail] {
+        switch self {
+        case .data:
+            return [
+                SelfCareMyRoutineDetail(image: DSKitAsset.Assets.pushUp.image, name: "푸시업", routine: "10개 | 4세트"),
+                SelfCareMyRoutineDetail(image: DSKitAsset.Assets.jumpSqt.image, name: "점프 스쿼트", routine: "20개 | 3세트"),
+                SelfCareMyRoutineDetail(image: DSKitAsset.Assets.dumbelSplitSqt.image, name: "덤벨 스플릿 스쿼트", routine: "30개 | 1세트"),
+                SelfCareMyRoutineDetail(image: DSKitAsset.Assets.pushUp.image, name: "푸시업", routine: "10개 | 4세트"),
+                SelfCareMyRoutineDetail(image: DSKitAsset.Assets.pushUp.image, name: "푸시업", routine: "10개 | 4세트"),
+                SelfCareMyRoutineDetail(image: DSKitAsset.Assets.pushUp.image, name: "푸시업", routine: "10개 | 4세트"),
+                SelfCareMyRoutineDetail(image: DSKitAsset.Assets.pushUp.image, name: "푸시업", routine: "10개 | 4세트"),
+                SelfCareMyRoutineDetail(image: DSKitAsset.Assets.pushUp.image, name: "푸시업", routine: "10개 | 4세트"),
+                SelfCareMyRoutineDetail(image: DSKitAsset.Assets.pushUp.image, name: "푸시업", routine: "10개 | 4세트"),
+                SelfCareMyRoutineDetail(image: DSKitAsset.Assets.pushUp.image, name: "푸시업", routine: "10개 | 4세트")
+
+            ]
+        }
+    }
+}
+
