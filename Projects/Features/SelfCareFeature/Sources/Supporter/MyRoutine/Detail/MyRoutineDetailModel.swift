df5181a176c0e2068402e514839229ae7b4d4b53
s <tylereunholee010203@gmail.com>
2024-01-30 15:14:53 +0900

    ⚓️ :: [#152] 자기관리 - 내 루틴 - 디테일
    model 파일 추가

---
+++ Projects/Features/SelfCareFeature/Sources/Supporter/MyRoutine/Detail/MyRoutineDetailModel.swift
@@ -0,0 +1,9 @@
+//
+//  MyRoutineDetailModel.swift
+//  SelfCareFeatureInterface
+//
+//  Created by 이은호 on 1/30/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
