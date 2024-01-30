fe6dc041a76789d159316bf6274d3045b8b7ffb9
s <tylereunholee010203@gmail.com>
2024-01-30 15:04:05 +0900

    ⚓️ :: [#152] 자기관리 - 내 루틴 - 디테일
    ViewModel 파일 추가

---
+++ Projects/Features/SelfCareFeature/Sources/SelfCareScene/ViewModel/SelfCareMyRoutineDetail+VM.swift
@@ -0,0 +1,9 @@
+//
+//  SelfCareMyRoutineDetail+VM.swift
+//  SelfCareFeatureInterface
+//
+//  Created by 이은호 on 1/30/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
