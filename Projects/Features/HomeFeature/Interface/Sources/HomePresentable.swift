4568d423ab11a768d6d9d95107715a9ba80638fc
junha <goodjunha@gmail.com>
2024-01-30 18:27:23 +0900

    💉:: [#153] HomePresentable 추가

---
+++ Projects/Features/HomeFeature/Interface/Sources/HomePresentable.swift
@@ -0,0 +1,24 @@
+//
+//  HomePresentable.swift
+//  HomeFeatureInterface
+//
+//  Created by 박준하 on 1/30/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
+
+import BaseFeatureDependency
+import Core
+
+public protocol HomeViewControllable: ViewControllable {}
+public protocol HomeCoordintable {
+    var onSettingButtonTap: (() -> Void)? { get set }
+    var onStepNumberButtonTap: (() -> Void)? { get set}
+    var routineButtonTap: (() -> Void)? { get set}
+    var calorieCalculatorButtonTap: (() -> Void)? { get set }
+    var maeumGaGymTimerButtonTap: (() -> Void)? { get set }
+}
+
+public typealias HomeViewModelType = BaseViewModel & HomeCoordintable
+public typealias HomePresentable = (vc: HomeViewControllable, vm: any HomeViewModelType)
