51800ddd4debc6231fa707f7a065004ef89e411e
s <tylereunholee010203@gmail.com>
2024-01-31 20:10:19 +0900

    ⚓️ :: [#155] 자기관리 - 내 루틴 - 루틴 수정
    임시 model 추가

--- Projects/Features/SelfCareFeature/Sources/Supporter/MyRoutine/Edit/MyRoutineEditModel.swift
+++ Projects/Features/SelfCareFeature/Sources/Supporter/MyRoutine/Edit/MyRoutineEditModel.swift
@@ -1,9 +1,28 @@
-//
-//  MyRoutineEditModel.swift
-//  SelfCareFeature
-//
-//  Created by 이은호 on 1/31/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import DSKit
+
+public struct MyRoutineEditContent {
+    var image: UIImage
+    var name: String
+    var numberCount: String
+    var setCount: String
+}
+
+public enum MyRoutineEditModel {
+    case routineData
+    
+    var data: [MyRoutineEditContent] {
+        switch self {
+        case .routineData:
+            return [
+                MyRoutineEditContent(image: DSKitAsset.Assets.pullUp.image, name: "풀업", numberCount: "10", setCount: "20"),
+                MyRoutineEditContent(image: DSKitAsset.Assets.pushUp.image, name: "푸시업", numberCount: "10", setCount: "4"),
+                                     MyRoutineEditContent(image: DSKitAsset.Assets.airSqt.image, name: "에어 스쿼트", numberCount: "10", setCount: "3"),
+                MyRoutineEditContent(image: DSKitAsset.Assets.babelLow.image, name: "바벨 로우", numberCount: "12", setCount: "2")
+            ]
+        }
+    }
+}
+
+
