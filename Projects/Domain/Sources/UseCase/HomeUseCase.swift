5abca8b173f4b66f6e96609bc71a59a2281b2bea
junha <goodjunha@gmail.com>
2024-02-01 15:13:19 +0900

    ⚓️:: [#153] HomeUseCase 추가

--- Projects/Domain/Sources/UseCase/HomeUseCase.swift
+++ Projects/Domain/Sources/UseCase/HomeUseCase.swift
@@ -7,16 +7,20 @@
 ////
 //
 import UIKit
-
 import RxSwift
 import RxCocoa
-
 import Core
 
 public protocol HomeUseCase {
-    var serviceState: PublishSubject<ServiceStateModel> { get }
+    var motivationMessage: PublishSubject<MotivationMessageModel> { get }
+    var stepNumber: PublishSubject<StepModel> { get }
+    var routines: PublishSubject<[RoutineModel]> { get }
+    var extras: PublishSubject<[ExtrasModel]> { get }
     
-    func getServiceState()
+    func getMotivationMessage()
+    func getStepNumber()
+    func getRoutines()
+    func getExtras()
 }
 
 public class DefaultHomeUseCase {
@@ -24,7 +28,10 @@ public class DefaultHomeUseCase {
     private let repository: HomeRepositoryInterface
     private let disposeBag = DisposeBag()
     
-    public let serviceState = PublishSubject<ServiceStateModel>()
+    public let motivationMessage = PublishSubject<MotivationMessageModel>()
+    public let stepNumber = PublishSubject<StepModel>()
+    public let routines = PublishSubject<[RoutineModel]>()
+    public let extras = PublishSubject<[ExtrasModel]>()
     
     public init(repository: HomeRepositoryInterface) {
         self.repository = repository
@@ -32,17 +39,56 @@ public class DefaultHomeUseCase {
 }
 
 extension DefaultHomeUseCase: HomeUseCase {
-    public func getServiceState() {
-        repository.getServiceState()
+    
+    public func getMotivationMessage() {
+        repository.getMotivationMessage()
             .subscribe(
-                onSuccess: { [weak self] serviceStateModel in
-                    self?.serviceState.onNext(serviceStateModel)
+                onSuccess: { [weak self] messageModel in
+                    self?.motivationMessage.onNext(messageModel)
                 },
                 onFailure: { [weak self] error in
-                    print("MainUseCase getServiceState error occurred: \(error)")
+                    print("HomeUseCase getMotivationMessage error occurred: \(error)")
+                }
+            )
+            .disposed(by: disposeBag)
+    }
+    
+    public func getStepNumber() {
+        repository.getStepNumber()
+            .subscribe(
+                onSuccess: { [weak self] stepModel in
+                    self?.stepNumber.onNext(stepModel)
+                },
+                onFailure: { [weak self] error in
+                    print("HomeUseCase getStepNumber error occurred: \(error)")
+                }
+            )
+            .disposed(by: disposeBag)
+    }
+    
+    public func getRoutines() {
+        repository.getRoutines()
+            .subscribe(
+                onSuccess: { [weak self] routines in
+                    self?.routines.onNext(routines)
+                },
+                onFailure: { [weak self] error in
+                    print("HomeUseCase getRoutines error occurred: \(error)")
+                }
+            )
+            .disposed(by: disposeBag)
+    }
+    
+    public func getExtras() {
+        repository.getExtras()
+            .subscribe(
+                onSuccess: { [weak self] extras in
+                    self?.extras.onNext(extras)
+                },
+                onFailure: { [weak self] error in
+                    print("HomeUseCase getExtras error occurred: \(error)")
                 }
             )
             .disposed(by: disposeBag)
     }
 }
-
