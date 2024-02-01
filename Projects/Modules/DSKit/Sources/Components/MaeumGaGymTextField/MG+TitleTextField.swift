9b7210014860ba7dbb94480a2aa5dda6c84ec90d
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-02-01 14:40:54 +0900

    ⚓️ :: [#155] 마음가짐 titleTextField
    추가

--- Projects/Modules/DSKit/Sources/Components/MaeumGaGymTextField/MG+TitleTextField.swift
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymTextField/MG+TitleTextField.swift
@@ -1,9 +1,49 @@
-//
-//  MG+TitleTextField.swift
-//  DSKit
-//
-//  Created by 이은호 on 2/1/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import SnapKit
+import Then
+
+open class MGTitleTextField: UITextField {
+    
+    private let placeholderLabel = UILabel().then {
+        $0.font = UIFont.Pretendard.bodyLarge
+        $0.textColor = DSKitAsset.Colors.gray400.color
+        $0.contentMode = .left
+    }
+    
+    public convenience init(placeholder: String) {
+        self.init(frame: .zero)
+        
+        placeholderLabel.text = placeholder
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+    
+    private func setupUI() {
+        configure()
+
+        tintColor = .black
+        backgroundColor = DSKitAsset.Colors.gray25.color
+        layer.cornerRadius = 8.0
+        layer.borderColor = DSKitAsset.Colors.gray50.color.cgColor
+        layer.borderWidth = 1.0
+    }
+    
+    private func configure() {
+        addSubview(placeholderLabel)
+        
+        placeholderLabel.snp.makeConstraints {
+            $0.top.leading.equalToSuperview().offset(12.0)
+            $0.height.equalTo(24.0)
+        }
+    }
+}
+
