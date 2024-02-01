ce39a7017c18330873928d2bf78c2136ef4cbc0a
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-02-01 17:08:59 +0900

    🍗 :: [#155] ViewModel 변경

--- Projects/Features/SelfCareFeature/Demo/Sources/Application/SceneDelegate.swift
+++ Projects/Features/SelfCareFeature/Demo/Sources/Application/SceneDelegate.swift
@@ -11,7 +11,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: SelfCareMyRoutineEditViewController(SelfCareMyRoutineDetailViewModel()))
+        window?.rootViewController = UINavigationController(rootViewController: SelfCareMyRoutineEditViewController(SelfCareMyRoutineEditViewModel()))
         window?.makeKeyAndVisible()
     }
 }
