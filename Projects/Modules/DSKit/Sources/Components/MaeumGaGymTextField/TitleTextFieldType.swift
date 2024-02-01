9ffaf554f4fdfcfad51bee3375d5726de52d237d
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-02-01 14:40:28 +0900

    ⚓️ :: [#155] titleTextField
    타입 추가

--- Projects/Modules/DSKit/Sources/Components/MaeumGaGymTextField/TitleTextFieldType.swift
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymTextField/TitleTextFieldType.swift
@@ -1,9 +1,34 @@
-//
-//  TitleTextFieldType.swift
-//  DSKit
-//
-//  Created by 이은호 on 2/1/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+public enum TitleTextFieldType {
+    case title
+    case startDate
+    case finishDate
+    case detail
+    
+    var titleText: String {
+        switch self {
+        case .title:
+            return "제목"
+        case .startDate:
+            return "시작 날짜"
+        case .finishDate:
+            return "마감 날짜"
+        case .detail:
+            return "내용"
+        }
+    }
+    
+    var placeHolder: String {
+        switch self {
+        case .title:
+            return "제목을 입력해 주세요."
+        case .startDate:
+            return ""
+        case .finishDate:
+            return ""
+        case .detail:
+            return "내용을 입력해 주세요."
+        }
+    }
+}
