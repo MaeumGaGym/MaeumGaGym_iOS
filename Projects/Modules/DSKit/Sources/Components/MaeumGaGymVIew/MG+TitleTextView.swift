b1fa9db340fbb61e01a461bd5d325e19580f6ac4
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-02-01 15:02:02 +0900

    ⚓️ :: [#155] 마음가짐 titleTextView
    View 구현

--- Projects/Modules/DSKit/Sources/Components/MaeumGaGymVIew/MG+TitleTextView.swift
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymVIew/MG+TitleTextView.swift
@@ -1,9 +1,47 @@
-//
-//  MG+TitleTextView.swift
-//  DSKit
-//
-//  Created by 이은호 on 2/1/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import SnapKit
+import Then
+
+import Core
+
+public class MGTitleTextView: BaseView {
+    
+    private var titleLabel = UILabel().then {
+        $0.font = UIFont.Pretendard.bodyMedium
+        $0.textAlignment = .left
+        $0.textColor = .black
+        $0.numberOfLines = 1
+    }
+    
+    private var textField = MGTitleTextField()
+    
+    public init (
+        type: TitleTextFieldType
+    ) {
+        super.init(frame: .zero)
+        titleLabel.text = type.titleText
+        textField.setPlaceholder(placeholder: type.placeHolder)
+        
+        layout()
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func layout() {
+        addSubviews([titleLabel, textField])
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(20.0)
+        }
+        
+        textField.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(8.0)
+            $0.bottom.equalToSuperview()
+            $0.leading.trailing.equalToSuperview()
+        }
+    }
+}
