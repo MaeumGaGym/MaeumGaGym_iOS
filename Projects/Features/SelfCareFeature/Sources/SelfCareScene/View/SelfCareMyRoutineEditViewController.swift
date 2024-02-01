1bf2a906ea76e844395e45817816bc228fceb053
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-02-01 15:00:45 +0900

    ⚓️ :: [#155] 자기관리 - 내 루틴 - 루틴 수정
    ViewController 추가

--- Projects/Features/SelfCareFeature/Sources/SelfCareScene/View/SelfCareMyRoutineEditViewController.swift
+++ Projects/Features/SelfCareFeature/Sources/SelfCareScene/View/SelfCareMyRoutineEditViewController.swift
@@ -10,23 +10,9 @@ public class SelfCareMyRoutineEditViewController: BaseViewController<SelfCareMyR
     
     private var myRoutineEditModel = MyRoutineEditModel.routineData
     
-    private var containerView = UIView()
     private var headerView = UIView()
     
-    private let myRoutineTitleLabel = UILabel().then {
-        $0.text = "주말 루틴"
-        $0.textColor = .black
-        $0.contentMode = .left
-        $0.font = UIFont.Pretendard.titleLarge
-    }
-    
-    private var routineStateLabel = UILabel().then {
-        $0.font = UIFont.Pretendard.bodyMedium
-        $0.text = "사용중 • 공유됨"
-        $0.textColor = DSKitAsset.Colors.blue500.color
-    }
-    
-    private var sharedView = MGShareStateView()
+    private var titleTextView = MGTitleTextView(type: .title)
     
     private var myRoutineDetailTableView = UITableView().then {
         $0.showsVerticalScrollIndicator = false
@@ -41,7 +27,6 @@ public class SelfCareMyRoutineEditViewController: BaseViewController<SelfCareMyR
         super.viewDidLoad()
         
         view.backgroundColor = .white
-        sharedView.changeView(backColor: DSKitAsset.Colors.gray50.color)
     }
     
     public override func attribute() {
@@ -54,38 +39,20 @@ public class SelfCareMyRoutineEditViewController: BaseViewController<SelfCareMyR
     public override func layout() {
         super.layout()
         
-        //        headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 136.0))
+        headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 132.0))
         view.addSubviews([ myRoutineDetailTableView])
-        //
-        //        headerView.addSubview(containerView)
-        //
-        //        containerView.snp.makeConstraints {
-        //            $0.top.equalToSuperview().offset(24.0)
-        //            $0.leading.trailing.equalToSuperview().inset(20.0)
-        //            $0.bottom.equalToSuperview().inset(32.0)
-        //        }
         
-        //        containerView.addSubviews([myRoutineTitleLabel, routineStateLabel, sharedView])
-        //
-        //        myRoutineTitleLabel.snp.makeConstraints {
-        //            $0.top.leading.equalToSuperview()
-        //            $0.height.equalTo(48.0)
-        //        }
-        //
-        //        routineStateLabel.snp.makeConstraints {
-        //            $0.leading.bottom.equalToSuperview()
-        //            $0.height.equalTo(20.0)
-        //        }
-        //
-        //        sharedView.snp.makeConstraints {
-        //            $0.centerY.trailing.equalToSuperview()
-        //            $0.width.equalTo(98.0)
-        //            $0.height.equalTo(36.0)
-        //        }
+        headerView.addSubview(titleTextView)
         
-        //        myRoutineDetailTableView.tableHeaderView = headerView
+        titleTextView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(24.0)
+            $0.leading.trailing.equalToSuperview().inset(20.0)
+            $0.bottom.equalToSuperview().inset(32.0)
+        }
+        
+        myRoutineDetailTableView.tableHeaderView = headerView
         myRoutineDetailTableView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.top.equalToSuperview().offset(105.0)
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(695.0)
         }
@@ -98,7 +65,7 @@ extension SelfCareMyRoutineEditViewController: UITableViewDelegate {
 }
 extension SelfCareMyRoutineEditViewController: UITableViewDataSource {
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        myRoutineEditModel.data.count
+        myRoutineEditModel.data.count + 1
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
