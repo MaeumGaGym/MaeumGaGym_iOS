9ab0a7b92106976c1581faf6e3860590addddbf9
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-02-01 16:38:29 +0900

    ⚓️ :: [#155] 자기관리
    자기관리 button 파일 추가

---
+++ Projects/Features/SelfCareFeature/Sources/Supporter/SelfCareButton.swift
@@ -0,0 +1,9 @@
+//
+//  SelfCareButton.swift
+//  SelfCareFeatureInterface
+//
+//  Created by 이은호 on 2/1/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
---
+++ Projects/Features/SelfCareFeature/Sources/Supporter/SelfCareButtonType.swift
@@ -0,0 +1,9 @@
+//
+//  SelfCareButtonType.swift
+//  SelfCareFeatureInterface
+//
+//  Created by 이은호 on 2/1/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
