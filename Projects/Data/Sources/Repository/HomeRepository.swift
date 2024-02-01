31e7bbf411ec6ab167ddf36e150fb66ed57f0d95
junha <goodjunha@gmail.com>
2024-02-01 15:08:07 +0900

    ⚓️:: [#153] HomeRepository 추가

--- Projects/Data/Sources/Repository/HomeRepository.swift
+++ Projects/Data/Sources/Repository/HomeRepository.swift
@@ -11,13 +11,32 @@ import RxSwift
 
 import Domain
 
+import MGNetworks
+
 public class HomeRepository: HomeRepositoryInterface {
+    private let networkService: HomeService
+
     public func getServiceState() -> Single<ServiceStateModel> {
         let serviceStateModel = ServiceStateModel(isAvailable: true)
         return Single.just(serviceStateModel)
     }
+    public func getMotivationMessage() -> Single<MotivationMessageModel> {
+        return networkService.requestMotivationMessage()
+    }
+
+    public func getStepNumber() -> Single<StepModel> {
+        return networkService.requestStepNumber()
+    }
+
+    public func getRoutines() -> Single<[RoutineModel]> {
+        return networkService.requestRoutines()
+    }
+
+    public func getExtras() -> Single<[ExtrasModel]> {
+        return networkService.requestExtras()
+    }
     
-    public init() {
-        
+    public init(networkService: HomeService) {
+        self.networkService = networkService
     }
 }
