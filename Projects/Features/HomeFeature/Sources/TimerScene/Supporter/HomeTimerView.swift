ece5d3d55a91ed4d1ff9938c8ef7de308f06e7c7
s <tylereunholee010203@gmail.com>
2024-02-02 18:02:32 +0900

    ⚓️ :: [#158] home Timer View

--- Projects/Features/HomeFeature/Sources/TimerScene/Supporter/HomeTimerView.swift
+++ Projects/Features/HomeFeature/Sources/TimerScene/Supporter/HomeTimerView.swift
@@ -1,9 +1,266 @@
-//
-//  HomeTimerView.swift
-//  HomeFeatureInterface
-//
-//  Created by 이은호 on 2/2/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import RxSwift
+
+import SnapKit
+import Then
+
+import DSKit
+
+public class HomeTimerView: UIView {
+    
+    private var homeTimer = MindGymTimerKit()
+    
+    private var radius: Double!
+    private var colorCircle: UIColor!
+    private var trackLayer: CAShapeLayer!
+    private var circleShapeLayer: CAShapeLayer!
+    
+    private var initTime: Double = 0.0
+    private var cancelTime: Double = 0.0
+    
+    let disposeBag = DisposeBag()
+    
+    private var timerInitTitle = UILabel().then {
+        $0.text = ""
+        $0.textAlignment = .center
+        $0.font = UIFont.Pretendard.bodyLarge
+        $0.textColor = .black
+    }
+    
+    private var timerMainTitle = UILabel().then {
+        $0.text = ""
+        $0.textAlignment = .center
+        $0.font = UIFont.monospacedDigitSystemFont(ofSize: UIFont.Pretendard.light.pointSize, weight: .light)
+        $0.textColor = .black
+    }
+    
+    private var timerAlarmTitle = UILabel().then {
+        $0.text = ""
+        $0.textAlignment = .center
+        $0.font = UIFont.Pretendard.bodyMedium
+        $0.textColor = DSKitAsset.Colors.gray400.color
+    }
+    
+    public init(
+        center: CGPoint,
+        radius: Double,
+        color: UIColor
+    ) {
+        super.init(frame: CGRect(
+            origin: CGPoint(x: center.x - CGFloat(radius), y: center.y - CGFloat(radius)),
+            size: CGSize(width: radius * 2, height: radius * 2))
+        )
+        
+        self.colorCircle = color
+        self.radius = radius
+        self.backgroundColor = .clear
+        
+        self.trackLayer = setTrackLayer()
+        self.layer.addSublayer(trackLayer)
+        
+        self.circleShapeLayer = setCircleLayer()
+        self.layer.addSublayer(circleShapeLayer)
+        
+        layout()
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        addSubviews([timerInitTitle, timerMainTitle, timerAlarmTitle])
+        
+        timerInitTitle.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(24.0)
+            $0.width.equalToSuperview()
+            $0.top.equalToSuperview().offset(89.0)
+        }
+        
+        timerMainTitle.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(64.0)
+            $0.width.equalToSuperview()
+            $0.top.equalTo(timerInitTitle.snp.bottom).offset(32.0)
+        }
+        
+        timerAlarmTitle.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(20.0)
+            $0.width.equalToSuperview()
+            $0.top.equalTo(timerMainTitle.snp.bottom).offset(32.0)
+        }
+    }
+    
+    public func timerSetting(for seconds: Int) {
+        homeTimer.setting(count: Double(seconds))
+        initTime = Double(seconds)
+        cancelTime = initTime
+        setInitialTimeLabel()
+        setTimerTimeLabel()
+        setInitAlarmTimeLabel()
+    }
+    
+    private func setInitialTimeLabel() {
+        if cancelTime == 0 {
+            timerInitTitle.text = "0분"
+            return
+        }
+        var time: [Double] = [0.0, 0.0, 0.0]
+        time[0] = Double(Int(initTime / 3600))
+        time[1] = Double(Int(initTime / 60))
+        time[2] = initTime.truncatingRemainder(dividingBy: 60)
+        
+        if initTime.truncatingRemainder(dividingBy: 3600) == 0 {
+            timerInitTitle.text = "\(Int(time[0]))시간"
+        } else if initTime / 3600 > 1 {
+            timerInitTitle.text = "\(Int(time[0]))시간  \(Int(time[1]))분"
+        } else if initTime.truncatingRemainder(dividingBy: 60) == 0 {
+            timerInitTitle.text = "\(Int(time[1]))분"
+        } else if initTime / 60 > 1 {
+            timerInitTitle.text = "\(Int(time[1]))분  \(Int(time[2]))초"
+        } else {
+            timerInitTitle.text = "\(Int(time[2]))초"
+        }
+    }
+    
+    private func setTimerTimeLabel() {
+        if cancelTime == 0 {
+            timerMainTitle.text = "00 : 00"
+            return
+        }
+        
+        homeTimer.mainTimer.timeUpdate
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] timeString in
+                DispatchQueue.main.async {
+                    self?.timerMainTitle.text = timeString
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func setInitAlarmTimeLabel() {
+        if cancelTime == 0 {
+            timerAlarmTitle.text = ""
+            return
+        }
+        let formatter_time = DateFormatter()
+        formatter_time.dateFormat = "a hh:mm"
+        formatter_time.amSymbol = "오전"
+        formatter_time.pmSymbol = "오후"
+        let current_time_string = formatter_time.string(from: Date().addingTimeInterval(initTime))
+        DispatchQueue.main.async {
+            self.timerAlarmTitle.text = current_time_string
+        }
+    }
+    
+    private func setAlarmTimeLabel() {
+        let formatter_time = DateFormatter()
+        formatter_time.dateFormat = "a hh:mm"
+        formatter_time.amSymbol = "오전"
+        formatter_time.pmSymbol = "오후"
+        let current_time_string = formatter_time.string(from: Date().addingTimeInterval(homeTimer.presentTimer()))
+        DispatchQueue.main.async {
+            self.timerAlarmTitle.text = current_time_string
+        }
+    }
+    
+    public func startTimer() -> Bool {
+        if homeTimer.presentTimer() == 0.0 {
+            return false
+        } else if circleShapeLayer.speed == 0 {
+            restartCricleAnimation()
+            homeTimer.startTimer()
+            setAlarmTimeLabel()
+        } else {
+            circleShapeLayer.add(createCircleAnimation(), forKey: "key")
+            homeTimer.startTimer()
+            setAlarmTimeLabel()
+        }
+        
+        return true
+    }
+    
+    public func stopTimer() {
+        stopCricleAnimation()
+        homeTimer.stopTimer()
+    }
+    
+    public func cancelTimer() {
+        cancelTime = 0.0
+        homeTimer.setting(count: Double(0))
+        circleShapeLayer.add(createCircleAnimation(), forKey: "key")
+        stopCricleAnimation()
+        homeTimer.stopTimer()
+        stopTimer()
+        setInitAlarmTimeLabel()
+        setInitialTimeLabel()
+    }
+    
+    public func restartTimer() {
+        homeTimer.setting(count: Double(initTime))
+        circleShapeLayer.add(createCircleAnimation(), forKey: "key")
+        stopCricleAnimation()
+        homeTimer.stopTimer()
+        setAlarmTimeLabel()
+    }
+
+    private func setCircleLayer() -> CAShapeLayer {
+        let circleLayer = CAShapeLayer().then {
+            $0.path = setCirclePath()
+            $0.strokeColor = colorCircle.cgColor
+            $0.lineWidth = 6
+            $0.lineCap = .round
+            $0.fillColor = UIColor.clear.cgColor
+        }
+        return circleLayer
+    }
+    
+    private func setTrackLayer() -> CAShapeLayer {
+        let trackLayer = CAShapeLayer().then {
+            $0.path = setCirclePath()
+            $0.strokeColor = DSKitAsset.Colors.gray100.color.cgColor
+            $0.lineWidth = 4
+            $0.lineCap = .round
+            $0.fillColor = UIColor.clear.cgColor
+        }
+        return trackLayer
+    }
+    
+    private func stopCricleAnimation() {
+        let pausedTime = circleShapeLayer.convertTime(CACurrentMediaTime(), from: nil)
+        circleShapeLayer.speed = 0
+        circleShapeLayer.timeOffset = pausedTime
+    }
+    
+    private func restartCricleAnimation() {
+        let pausedTime = circleShapeLayer.timeOffset
+        circleShapeLayer.speed = 1.0
+        circleShapeLayer.timeOffset = 0.0
+        circleShapeLayer.beginTime = 0.0
+        let timeSincePause = circleShapeLayer.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
+        circleShapeLayer.beginTime = timeSincePause
+    }
+    
+    private func setCirclePath() -> CGPath {
+        UIBezierPath(arcCenter: CGPoint(x: radius, y: radius),
+                     radius: CGFloat(radius),
+                     startAngle: -CGFloat.pi / 2,
+                     endAngle: 2 * CGFloat.pi - CGFloat.pi / 2,
+                     clockwise: true).cgPath
+    }
+    
+    private func createCircleAnimation() -> CABasicAnimation {
+        let strokeAnimation = CABasicAnimation(keyPath: #keyPath(CAShapeLayer.strokeEnd))
+        
+        strokeAnimation.toValue = 0
+        strokeAnimation.fromValue = 1
+        strokeAnimation.duration = CFTimeInterval(homeTimer.presentTimer())
+        strokeAnimation.isRemovedOnCompletion = true
+        circleShapeLayer.strokeEnd = 0
+        return strokeAnimation
+    }
+}
