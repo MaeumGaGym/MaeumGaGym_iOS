0ac1fc2a75aaf4a2fb8c1436400d3d5a37000d3f
junha <goodjunha@gmail.com>
2024-02-02 16:41:26 +0900

    ⚓️:: [#160] BaseAPI 추가

---
+++ Projects/Modules/MGNetworks/Sources/Foundation/BaseAPI.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+import RxMoya
+
+import Alamofire
+import Moya
+
+import Core
+
+public protocol BaseAPI: TargetType {
+    static var apiType: APIType { get set }
+    var csrfToken: String? { get }
+    var oauthToken: String? { get }
+}
+
+extension BaseAPI {
+    public var baseURL: URL {
+        var appURL = Config.setConfigTypeAndGetBaseURL(.application)
+        var devURL = Config.setConfigTypeAndGetBaseURL(.devTest)
+        
+        switch Self.apiType {
+        case .google:
+            appURL += "/google"
+        case .kakao:
+            appURL += "/kakao"
+        case .apple:
+            appURL += "/apple"
+        }
+        
+        guard let url = URL(string: appURL) else {
+            fatalError("baseURL could not be configured")
+        }
+        
+        return url
+    }
+    
+    public var headers: [String: String]? {
+        return HeaderType.createHeaders(csrfToken: self.csrfToken, oauthToken: self.oauthToken)
+    }
+}
+
+
+public enum HeaderType {
+    static func createHeaders(csrfToken: String?, oauthToken: String?) -> [String: String]? {
+        var headers: [String: String] = [:]
+
+        if let csrfToken = csrfToken {
+            headers["X-XSRF-TOKEN"] = csrfToken
+        }
+        if let oauthToken = oauthToken {
+            headers["Authorization"] = "Bearer \(oauthToken)"
+        }
+
+        return headers.isEmpty ? nil : headers
+    }
+}
