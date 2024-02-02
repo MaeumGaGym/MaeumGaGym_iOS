8a0e0e2d7762b9ebfc0baa5ef1dbd283f3318cf9
s <tylereunholee010203@gmail.com>
2024-02-02 18:03:18 +0900

    ⚓️ :: [#158] 마인드 짐 킷 추가

--- Projects/Features/HomeFeature/Sources/TimerScene/Supporter/MinGymKit/MinGymKit.swift
+++ Projects/Features/HomeFeature/Sources/TimerScene/Supporter/MinGymKit/MinGymKit.swift
@@ -1,9 +1,39 @@
-//
-//  MinGymKit.swift
-//  HomeFeatureInterface
-//
-//  Created by 이은호 on 2/2/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
+import RxSwift
 
-import Foundation
+public protocol TimerControl {
+    var timeUpdate: Observable<String> { get }
+    func start()
+    func stop()
+    func reset()
+    func presentTime() -> Double
+}
+
+open class MindGymTimerKit {
+    public let mainTimer: TimerControl = MindGaGymKitTimer()
+
+    public func setting(count: Double) {
+        (mainTimer as? MindGaGymKitTimer)?.setting(count: count)
+    }
+
+    public func startTimer() {
+        mainTimer.start()
+    }
+
+    public func stopTimer() {
+        mainTimer.stop()
+    }
+
+    public func resetTimer() {
+        mainTimer.reset()
+    }
+
+    public func restartTimer() {
+        (mainTimer as? MindGaGymKitTimer)?.restart()
+    }
+    
+    public func presentTimer() -> Double {
+        let presentTime: Double = mainTimer.presentTime()
+        return presentTime
+    }
+}
