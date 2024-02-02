a17a7d99ae57edbd4060c79162bc095962c993d3
junha <goodjunha@gmail.com>
2024-02-02 16:40:34 +0900

    ⚓️:: [#160] APIType 추가

---
+++ Projects/Modules/MGNetworks/Sources/Foundation/APIType.swift
@@ -0,0 +1,17 @@
+//
+//  APIType.swift
+//  MGNetworks
+//
+//  Created by 박준하 on 2/2/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Alamofire
+import RxMoya
+import Core
+
+public enum APIType {
+    case google
+    case kakao
+    case apple
+}
