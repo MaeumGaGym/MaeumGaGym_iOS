347a3f5f911f8f9b4b418d83e799d38606e22df5
junha <goodjunha@gmail.com>
2024-02-05 16:30:16 +0900

    🎮:: [#164] 카카오 API 호출 준비 설정

--- Projects/Features/AuthFeature/Demo/Sources/SceneDelegate.swift
+++ Projects/Features/AuthFeature/Demo/Sources/SceneDelegate.swift
@@ -4,17 +4,29 @@ import RxFlow
 import Core
 import AuthFeature
 
+import KakaoSDKAuth
+import KakaoSDKCommon
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                _ = AuthController.handleOpenUrl(url: url)
+            }
+        }
+    }
+
     func scene(_ scene: UIScene,
-               willConnectTo session: UISceneSession,
-               options connectionOptions: UIScene.ConnectionOptions
-    ) {
+               willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+        KakaoSDK.initSDK(appKey: "44df4ecfe4e1218c17550a6ab201d87d")
+
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.configure(withRootViewController: IntroViewController(IntroViewModel()))
+        window?.configure(withRootViewController: KakaoOauthTestViewController())
         window?.makeKeyAndVisible()
     }
 }
