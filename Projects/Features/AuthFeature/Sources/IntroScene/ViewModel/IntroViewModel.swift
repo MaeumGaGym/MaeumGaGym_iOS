c4e7502eaf2a1830be29c5b65bf5fe1b8d7139eb
junha <goodjunha@gmail.com>
2024-02-05 21:56:10 +0900

    ⚓️:: [#164] ViewModel 추가

--- Projects/Features/AuthFeature/Sources/IntroScene/ViewModel/IntroViewModel.swift
+++ Projects/Features/AuthFeature/Sources/IntroScene/ViewModel/IntroViewModel.swift
@@ -7,25 +7,69 @@
 //
 
 import Foundation
+
 import RxFlow
 import RxCocoa
 import RxSwift
+
 import Core
 
-public class IntroViewModel: BaseViewModel {
-    public func transform(_ input: Input, action: (Output) -> Void) -> Output {
-        return Output()
-    }
+import AuthFeatureInterface
+import Domain
+
+public class IntroViewModel: AuthViewModelType {
+
+    public var disposeBag: RxSwift.DisposeBag
+
+    private let useCase: AuthUseCase
 
     public struct Input {
-
+        let goolgeButtonTapped: Driver<Void>
+        let appleButtonTapped: Driver<Void>
+        let kakaoButtonTapped: Driver<Void>
     }
 
-    public struct Output {
-
+    public struct Output { 
+        let loginResult: Driver<Result<AuthHandleableType, Error>>
     }
 
-    public init() {
+    public var goolgeButtonTap: (() -> Void)?
 
-    }
+    public var appleButtonTap: (() -> Void)?
+
+    public var kakaoButtonTap: (() -> Void)?
+
+    public init(authUseCase: AuthUseCase) {
+         self.useCase = authUseCase
+        self.disposeBag = DisposeBag()
+     }
+
+    public func transform(_ input: Input, action: (Output) -> Void) -> Output {
+
+           let loginResultSubject = PublishSubject<Result<AuthHandleableType, Error>>()
+
+        input.goolgeButtonTapped
+            .drive(onNext: { [weak self] _ in
+                self?.useCase.requestSignIn(token: "googleToken")
+            })
+            .disposed(by: disposeBag)
+
+           input.kakaoButtonTapped
+               .drive(onNext: { [weak self] _ in
+                   self?.kakaoButtonTap?()
+               })
+               .disposed(by: disposeBag)
+
+           input.appleButtonTapped
+               .drive(onNext: { [weak self] _ in
+                   self?.appleButtonTap?()
+               })
+               .disposed(by: disposeBag)
+
+           let output = Output(loginResult: loginResultSubject.asDriver(onErrorJustReturn: .failure(AuthError.unknown)))
+
+           action(output)
+
+           return output
+       }
 }
