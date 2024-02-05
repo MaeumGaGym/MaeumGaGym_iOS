ed9200a3bbac5f0701db6b2dfbc5f69e2b61cbca
junha <goodjunha@gmail.com>
2024-02-05 21:57:42 +0900

    ⚓️:: [#164] AuthRepository 추가

---
+++ Projects/Data/Sources/Repository/AuthRepository.swift
@@ -0,0 +1,26 @@
+//
+//  AuthRepository.swift
+//  Data
+//
+//  Created by 박준하 on 2/5/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import Domain
+import MGNetworks
+
+public class AuthRepository: AuthRepositoryInterface {
+
+    private let networkService: AuthService
+
+    public init(networkService: AuthService) {
+        self.networkService = networkService
+    }
+    
+    public func requestSignIn(token: String) -> Single<Bool> {
+        return networkService.requestToken()
+    }
+}
