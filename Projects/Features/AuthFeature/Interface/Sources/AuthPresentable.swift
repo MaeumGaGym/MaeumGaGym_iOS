c607d3277a7fc68258e1cc31ffa5b900952649a4
junha <goodjunha@gmail.com>
2024-02-05 21:55:41 +0900

    ⚓️:: [#164] AuthViewControllable 추가

---
+++ Projects/Features/AuthFeature/Interface/Sources/AuthPresentable.swift
@@ -0,0 +1,27 @@
+//
+//  AuthPresentable.swift
+//  AuthFeatureInterface
+//
+//  Created by 박준하 on 2/5/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+import BaseFeatureDependency
+import Core
+
+public protocol AuthViewControllable: ViewControllable {}
+public protocol AuthCoordintable {
+    var goolgeButtonTap: (() -> Void)? { get set }
+    var appleButtonTap: (() -> Void)? { get set }
+    var kakaoButtonTap: (() -> Void)? { get set }
+    
+    var disposeBag: DisposeBag { get set }
+}
+
+public typealias AuthViewModelType = BaseViewModel & AuthCoordintable
+public typealias AuthPresentable = (vc: AuthViewControllable, vm: any AuthViewModelType)
