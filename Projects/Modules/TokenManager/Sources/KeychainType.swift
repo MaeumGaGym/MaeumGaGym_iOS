25ba1d513ccec42aa41476260e2b58d592776fb0
junha <goodjunha@gmail.com>
2024-02-06 23:00:32 +0900

    ⚓️:: [#160] KeychainType 추가

---
+++ Projects/Modules/TokenManager/Sources/KeychainType.swift
@@ -0,0 +1,14 @@
+//
+//  KeychainType.swift
+//  TokenManager
+//
+//  Created by 박준하 on 2/6/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
+
+public enum KeychainType: String {
+    case authorizationToken
+    case CSRFToken
+}
