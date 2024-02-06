3789082502a4480fa3997e5127f6e8c23be7a094
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-02-06 16:22:09 +0900

    ⚓️ :: [#158] 타이머 모델

--- Projects/Features/HomeFeature/Sources/TimerScene/Supporter/TimerModel.swift
+++ Projects/Features/HomeFeature/Sources/TimerScene/Supporter/TimerModel.swift
@@ -1,9 +1,26 @@
-//
-//  TimerModel.swift
-//  HomeFeature
-//
-//  Created by 이은호 on 2/6/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import DSKit
+
+public struct Time {
+    var time: Double
+}
+
+public enum TimerModel {
+    case eunho
+
+    var data: [Time] {
+        switch self {
+        case .eunho:
+            return [
+                Time(time: 60),
+                Time(time: 10),
+                Time(time: 4660),
+                Time(time: 3600),
+                Time(time: 700),
+                Time(time: 5),
+            ]
+        }
+    }
+}
+
