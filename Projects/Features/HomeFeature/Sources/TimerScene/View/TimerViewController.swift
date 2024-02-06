1be35f0d45ecea645bbd93798b5f97ed17cc2d10
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-02-06 14:10:43 +0900

    ⚓️ :: [#158] colletionView 구현

--- Projects/Features/HomeFeature/Sources/TimerScene/View/TimerViewController.swift
+++ Projects/Features/HomeFeature/Sources/TimerScene/View/TimerViewController.swift
@@ -12,6 +12,8 @@ import HomeFeatureInterface
 
 
 public class TimerViewController: BaseViewController<TimerViewModel> {
+    
+    private var timerData: []
         
     lazy var progressBarView = HomeTimerView(center: view.center, radius: 175.0, color: DSKitAsset.Colors.blue500.color)
     
@@ -19,12 +21,26 @@ public class TimerViewController: BaseViewController<TimerViewModel> {
     private let stopButton = MGTimerButton(type: .stop, radius: 40.0)
     private let startButton = MGTimerButton(type: .start, radius: 40.0)
     private let restartButton = MGTimerButton(type: .restart)
+    
+    private lazy var collectionViewLayout = UICollectionViewFlowLayout().then {
+        $0.scrollDirection = .horizontal
+        $0.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+
+    private lazy var timerCollectionView = UICollectionView(frame: .zero, collectionViewLayout: self.collectionViewLayout).then {
+        $0.register(TimerCollectionViewCell.self, forCellWithReuseIdentifier: TimerCollectionViewCell.identifier)
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.showsHorizontalScrollIndicator = false
+        $0.showsVerticalScrollIndicator = false
+        $0.dataSource = self
+        $0.delegate = self
+    }
 
     
     public override func attribute() {
         view.backgroundColor = .white
         
-        progressBarView.timerSetting(for: 10)
+        progressBarView.timerSetting(for: 3670)
         buttonTap()
     }
 
@@ -68,6 +84,7 @@ public class TimerViewController: BaseViewController<TimerViewModel> {
     }
     
     private func buttonTap() {
+        
         startButton.rx.tap
             .subscribe(onNext: { [self] in
                 if progressBarView.startTimer() {
@@ -105,3 +122,43 @@ public class TimerViewController: BaseViewController<TimerViewModel> {
     }
 }
 
+extension TimerViewController: UICollectionViewDelegateFlowLayout {
+    public func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        return CGSize(width: 148.0, height: 200.0)
+    }
+}
+
+extension TimerViewController: UICollectionViewDelegate {
+    public func collectionView(
+        _ collectionView: UICollectionView,
+        didSelectItemAt indexPath: IndexPath
+    ) {
+        _ = data[indexPath.row]
+    }
+}
+
+extension TimerViewController: UICollectionViewDataSource {
+    public func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return data.count
+    }
+
+    public func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: PostureRecommandCollectionViewCell.identifier,
+            for: indexPath
+        ) as? PostureRecommandCollectionViewCell
+        let model = data[indexPath.row]
+        cell?.setup(exerciseImage: model.image, exerciseNameText: model.name, exercisePartText: model.part)
+        return cell ?? UICollectionViewCell()
+    }
+}
