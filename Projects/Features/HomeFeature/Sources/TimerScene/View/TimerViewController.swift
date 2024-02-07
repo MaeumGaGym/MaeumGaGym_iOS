012cc69915c20fe39684895a84e46d965fcf2a39
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-02-07 16:37:46 +0900

    ⚓️ :: [#158] 타이머 VC 기능 추가

--- Projects/Features/HomeFeature/Sources/TimerScene/View/TimerViewController.swift
+++ Projects/Features/HomeFeature/Sources/TimerScene/View/TimerViewController.swift
@@ -14,7 +14,7 @@ import HomeFeatureInterface
 public class TimerViewController: BaseViewController<TimerViewModel> {
     
     private var timerData = TimerModel.eunho
-        
+    
     lazy var progressBarView = HomeTimerView(center: view.center, radius: 175.0, color: DSKitAsset.Colors.blue500.color)
     
     private let closeButton = MGTimerButton(type: .close)
@@ -26,8 +26,9 @@ public class TimerViewController: BaseViewController<TimerViewModel> {
         $0.scrollDirection = .horizontal
         $0.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
-
+    
     private lazy var timerCollectionView = UICollectionView(frame: .zero, collectionViewLayout: self.collectionViewLayout).then {
+        $0.register(TimerHeaderCollectionViewCell.self, forCellWithReuseIdentifier: TimerHeaderCollectionViewCell.identifier)
         $0.register(TimerCollectionViewCell.self, forCellWithReuseIdentifier: TimerCollectionViewCell.identifier)
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.showsHorizontalScrollIndicator = false
@@ -35,15 +36,15 @@ public class TimerViewController: BaseViewController<TimerViewModel> {
         $0.dataSource = self
         $0.delegate = self
     }
-
+    
     
     public override func attribute() {
         view.backgroundColor = .white
         
-        progressBarView.timerSetting(for: 3670)
+        progressBarView.timerSetting(for: 3601)
         buttonTap()
     }
-
+    
     public override func layout() {
         view.addSubviews([progressBarView, closeButton, stopButton, startButton, restartButton, timerCollectionView])
         
@@ -67,13 +68,13 @@ public class TimerViewController: BaseViewController<TimerViewModel> {
         stopButton.snp.makeConstraints {
             $0.top.equalTo(progressBarView.snp.bottom).offset(219.0)
             $0.leading.equalTo(closeButton.snp.trailing).offset(24.0)
-
+            
         }
         
         startButton.snp.makeConstraints {
             $0.top.equalTo(progressBarView.snp.bottom).offset(219.0)
             $0.leading.equalTo(closeButton.snp.trailing).offset(24.0)
-
+            
         }
         
         restartButton.snp.makeConstraints {
@@ -128,16 +129,10 @@ extension TimerViewController: UICollectionViewDelegateFlowLayout {
         layout collectionViewLayout: UICollectionViewLayout,
         sizeForItemAt indexPath: IndexPath
     ) -> CGSize {
+        if indexPath.row == 0 {
+            return CGSize(width: 40.0, height: 120.0)
+        }
         return CGSize(width: 120.0, height: 120.0)
-    }
-}
-
-extension TimerViewController: UICollectionViewDelegate {
-    public func collectionView(
-        _ collectionView: UICollectionView,
-        didSelectItemAt indexPath: IndexPath
-    ) {
-        _ = timerData.data[indexPath.row]
     }
 }
 
@@ -146,19 +141,40 @@ extension TimerViewController: UICollectionViewDataSource {
         _ collectionView: UICollectionView,
         numberOfItemsInSection section: Int
     ) -> Int {
-        return timerData.data.count
+        return timerData.data.count + 1
     }
-
     public func collectionView(
         _ collectionView: UICollectionView,
         cellForItemAt indexPath: IndexPath
     ) -> UICollectionViewCell {
+        if indexPath.row == 0 {
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: TimerHeaderCollectionViewCell.identifier,
+                for: indexPath
+            ) as? TimerHeaderCollectionViewCell
+            cell?.setup()
+            return cell ?? UICollectionViewCell()
+        }
         let cell = collectionView.dequeueReusableCell(
             withReuseIdentifier: TimerCollectionViewCell.identifier,
             for: indexPath
         ) as? TimerCollectionViewCell
-        let model = timerData.data[indexPath.row]
-        cell?.setup(time: model.time)
+        let model = timerData.data[indexPath.row - 1]
+        cell?.setup(time: Double(model.time))
         return cell ?? UICollectionViewCell()
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        if indexPath.row == 0 {
+            return false
+        }
+        
+        let time = timerData.data[indexPath.row - 1].time
+        progressBarView.timerSetting(for: time)
+        
+        stopButton.isHidden = true
+        startButton.isHidden = false
+        
+        return true
     }
 }
