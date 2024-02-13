09455c4f85fbc31ad23fe46da7bb319032f695fa
s <tylereunholee010203@gmail.com>
2024-02-13 15:10:47 +0900

    🍗 :: [#171] 마음가짐 타이머 킷
    Double자료형으로 변경, 애니메이션과 UILabel 싱크 맞춤

--- Projects/Features/HomeFeature/Sources/TimerScene/Supporter/MinGymKit/MinGaGymTimerKit.swift
+++ Projects/Features/HomeFeature/Sources/TimerScene/Supporter/MinGymKit/MinGaGymTimerKit.swift
@@ -4,63 +4,79 @@ import RxSwift
 
 open class MindGaGymKitTimer: NSObject, TimerControl {
     
-    private var initTime: Int = 0
-    private var time: Double = 0
+    private var initCounter: Double = 0.0
+    private var counter: Double = 0.0
     private var timer: Timer?
-    
     private let timerSubject = PublishSubject<String>()
     public var timeUpdate: Observable<String> {
         return timerSubject.asObservable()
     }
 
-    public func setting(initialTime: Int) {
-        initTime = initialTime
-        time = Double(initialTime)
-        setTimerString()
+    public func setting(count: Double) {
+        initCounter = count
+        counter = count
+        setInitTimeString()
     }
 
     public func start() {
-       reduceTime()
-    }
-    
-    public func stop() {
-        timer?.invalidate()
-        timer = nil
-    }
-    
-    public func restart() {
-        time = Double(initTime)
-        setTimerString()
-    }
-    
-    public func reset() {
-        time = 0
-        initTime = 0
-        setTimerString()
-    }
-    
-    public func setTimerString() {
-        let timeString = timerString(count: Int(time))
-        timerSubject.onNext(timeString)
-    }
-    
-    public func presentTime() -> Int {
-        return time
-    }
-    
-    private func reduceTime() {
         timer = Timer.scheduledTimer(withTimeInterval: 0.001, repeats: true) { [self] _ in
-            if time <= 0 {
+            if  counter <= 0 {
                 stop()
             } else {
-                time -= 0.001
-                setTimerString()
+                counter -= 0.001
+                setTimeString()
             }
         }
     }
+
+    public func stop() {
+        timer?.invalidate()
+        timer = nil
+        setInitTimeString()
+    }
+
+    public func restart() {
+        counter = initCounter
+        setInitTimeString()
+    }
+
+    public func reset() {
+        counter = 0.0
+        initCounter = 0.0
+        setInitTimeString()
+    }
     
-    private func timerString(count: Int) -> String {
-        let totalSeconds: Int = count
+    public func presentTime() -> Double {
+        return self.counter
+    }
+    
+    public func setTimeString() {
+        let timeString = timeString(from: counter)
+        timerSubject.onNext(timeString)
+    }
+    
+    public func setInitTimeString() {
+        let timeString = initTimeString(from: counter)
+        timerSubject.onNext(timeString)
+    }
+    
+    private func timeString(from counter: Double) -> String {
+        let totalSeconds = Int(counter) + 1
+        let hours: String = String(format: "%02d", totalSeconds / 3600)
+        let minutes: String = String(format: "%02d", (totalSeconds % 3600) / 60)
+        let seconds: String = String(format: "%02d", totalSeconds % 60)
+
+        if totalSeconds / 3600 >= 1 {
+            return "\(hours) : \(minutes) : \(seconds)"
+        } else if totalSeconds / 60 >= 1 {
+            return "\(minutes) : \(seconds)"
+        } else {
+            return "00 : \(seconds)"
+        }
+    }
+    
+    private func initTimeString(from counter: Double) -> String {
+        let totalSeconds = Int(counter)
         let hours: String = String(format: "%02d", totalSeconds / 3600)
         let minutes: String = String(format: "%02d", (totalSeconds % 3600) / 60)
         let seconds: String = String(format: "%02d", totalSeconds % 60)
@@ -74,4 +90,3 @@ open class MindGaGymKitTimer: NSObject, TimerControl {
         }
     }
 }
-
