6b3e5d650a42276a3cb25562a1862ee4e01a456f
junha <goodjunha@gmail.com>
2024-02-13 23:01:31 +0900

    🍗:: [#174] 전체적인 리펙토링

--- Projects/Features/HomeFeature/Sources/MetronomeScene/MetronomeViewController.swift
+++ Projects/Features/HomeFeature/Sources/MetronomeScene/MetronomeViewController.swift
@@ -1,6 +1,15 @@
 import UIKit
+
+import RxSwift
+
 import SnapKit
+import Then
+
+import DSKit
+import Core
+
 import AVFoundation
+
 import AudioToolbox
 
 public class MetronomeViewController: UIViewController {
@@ -13,11 +22,16 @@ public class MetronomeViewController: UIViewController {
         return label
     }()
 
-    private let tempoStepper: UIStepper = {
-        let stepper = UIStepper()
-        stepper.minimumValue = 10
-        stepper.maximumValue = 300
-        return stepper
+    private let tempoIncrementButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("+", for: .normal)
+        return button
+    }()
+
+    private let tempoDecrementButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("-", for: .normal)
+        return button
     }()
 
     private let startButton: UIButton = {
@@ -58,7 +72,8 @@ public class MetronomeViewController: UIViewController {
 
     private func setupViews() {
         view.addSubview(tempoLabel)
-        view.addSubview(tempoStepper)
+        view.addSubview(tempoIncrementButton)
+        view.addSubview(tempoDecrementButton)
         view.addSubview(startButton)
         view.addSubview(stopButton)
         view.addSubview(vibrateButton)
@@ -68,19 +83,24 @@ public class MetronomeViewController: UIViewController {
             $0.top.equalToSuperview().offset(100)
         }
 
-        tempoStepper.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.top.equalTo(tempoLabel.snp.bottom).offset(20)
+        tempoIncrementButton.snp.makeConstraints {
+            $0.centerY.equalTo(tempoLabel)
+            $0.leading.equalTo(tempoLabel.snp.trailing).offset(10)
+        }
+
+        tempoDecrementButton.snp.makeConstraints {
+            $0.centerY.equalTo(tempoLabel)
+            $0.trailing.equalTo(tempoLabel.snp.leading).offset(-10)
         }
 
         startButton.snp.makeConstraints {
             $0.centerX.equalToSuperview().offset(-50)
-            $0.top.equalTo(tempoStepper.snp.bottom).offset(20)
+            $0.top.equalTo(tempoLabel.snp.bottom).offset(20)
         }
 
         stopButton.snp.makeConstraints {
             $0.centerX.equalToSuperview().offset(50)
-            $0.top.equalTo(tempoStepper.snp.bottom).offset(20)
+            $0.top.equalTo(tempoLabel.snp.bottom).offset(20)
         }
 
         vibrateButton.snp.makeConstraints {
@@ -90,14 +110,20 @@ public class MetronomeViewController: UIViewController {
     }
 
     private func setupActions() {
-        tempoStepper.addTarget(self, action: #selector(stepperValueChanged), for: .valueChanged)
+        tempoIncrementButton.addTarget(self, action: #selector(incrementButtonTapped), for: .touchUpInside)
+        tempoDecrementButton.addTarget(self, action: #selector(decrementButtonTapped), for: .touchUpInside)
         startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
         stopButton.addTarget(self, action: #selector(stopButtonTapped), for: .touchUpInside)
         vibrateButton.addTarget(self, action: #selector(vibrateButtonTapped), for: .touchUpInside)
     }
 
-    @objc private func stepperValueChanged() {
-        viewModel.tempo = Int(tempoStepper.value)
+    @objc private func incrementButtonTapped() {
+        viewModel.tempo += 1
+        tempoLabel.text = "Tempo: \(viewModel.tempo)"
+    }
+
+    @objc private func decrementButtonTapped() {
+        viewModel.tempo -= 1
         tempoLabel.text = "Tempo: \(viewModel.tempo)"
     }
 
