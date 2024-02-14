f5b9cedb8baa0877483a1d3da590514931313fec
s <tylereunholee010203@gmail.com>
2024-02-14 14:08:39 +0900

    ⚓️ :: [#171] 멀티 타이머 적용

--- Projects/Features/HomeFeature/Sources/TimerScene/Supporter/MinGymKit/MinGymKit.swift
+++ Projects/Features/HomeFeature/Sources/TimerScene/Supporter/MinGymKit/MinGymKit.swift
@@ -3,37 +3,48 @@ import RxSwift
 
 public protocol TimerControl {
     var timeUpdate: Observable<String> { get }
+    func setting(settingTime: Double)
     func start()
     func stop()
     func reset()
+    func restart()
     func presentTime() -> Double
 }
 
 open class MindGymTimerKit {
-    public let mainTimer: TimerControl = MindGaGymKitTimer()
-
-    public func setting(count: Double) {
-        (mainTimer as? MindGaGymKitTimer)?.setting(count: count)
+    public var timers: [TimerControl] = []
+    
+    public func setTimer(index: Int, settingTime: Double) {
+        timers[index].setting(settingTime: settingTime)
     }
 
-    public func startTimer() {
-        mainTimer.start()
+    public func startTimer(index: Int) {
+        timers[index].start()
     }
 
-    public func stopTimer() {
-        mainTimer.stop()
+    public func stopTimer(index: Int) {
+        timers[index].stop()
     }
 
-    public func resetTimer() {
-        mainTimer.reset()
+    public func resetTimer(index: Int) {
+        timers[index].reset()
     }
 
-    public func restartTimer() {
-        (mainTimer as? MindGaGymKitTimer)?.restart()
+    public func restartTimer(index: Int) {
+        timers[index].restart()
     }
     
-    public func presentTimer() -> Double {
-        let presentTime: Double = mainTimer.presentTime()
-        return presentTime
+    public func addRearTimer(time: Double) {
+        let newTimer = MindGaGymKitTimer()
+        newTimer.setting(settingTime: time)
+        timers.append(newTimer)
+    }
+    
+    public func deleteTimer(index: Int) {
+        timers.remove(at: index)
+    }
+
+    public func presentTime(index: Int) -> Double {
+        return timers[index].presentTime()
     }
 }
