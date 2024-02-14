a51a96a0be1a7ef30fcc47e22518724b9137e58e
junha <goodjunha@gmail.com>
2024-02-14 13:18:01 +0900

    ⚓️:: [#176] 네비게이션 추가

---
+++ Projects/Features/HomeFeature/Sources/MetronomeScene/View/NavigationBar/MetronomeNavigationBar.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+import Core
+import DSKit
+
+final class MetronomeNavigationBar: UIView {
+ 
+    public var rightButtonTap: ControlEvent<Void> {
+        return rightButton.rx.tap
+    }
+
+    private let rightButton = UIButton(type: .custom).then {
+        $0.setImage(DSKitAsset.Assets.timerNavDots.image, for: .normal)
+    }
+
+    private lazy var rightItemsStackView = UIStackView(arrangedSubviews: [rightButton]).then {
+        $0.axis = .horizontal
+        $0.spacing = 4
+        $0.distribution = .fillEqually
+    }
+
+    init() {
+        super.init(frame: .zero)
+        setUI()
+        setLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MetronomeNavigationBar {
+    private func setUI() {
+        self.backgroundColor = .white
+    }
+
+    private func setLayout() {
+        self.addSubview(rightItemsStackView)
+
+        self.snp.makeConstraints {
+            $0.height.equalTo(42)
+        }
+
+        rightItemsStackView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(20)
+        }
+    }
+}
