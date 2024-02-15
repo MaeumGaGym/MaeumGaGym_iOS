295217301254d76d6e7eccdd34ca664dba7f1646
junha <goodjunha@gmail.com>
2024-02-15 15:02:20 +0900

    💉:: [#176] viewmodel 로직 수정

--- Projects/Features/HomeFeature/Sources/MetronomeScene/MetronomeViewModel.swift
+++ Projects/Features/HomeFeature/Sources/MetronomeScene/MetronomeViewModel.swift
@@ -14,35 +14,49 @@ public protocol MetronomeViewModelDelegate: AnyObject {
 
 public class MetronomeViewModel {
     weak var delegate: MetronomeViewModelDelegate?
+    
+    public var isPlaying = false
 
     private var metronome: MetronomeType
     private let disposeBag = DisposeBag()
 
     var tempoRelay = BehaviorRelay<Int>(value: 120)
     var tempo: Int {
-           get { tempoRelay.value }
-           set { tempoRelay.accept(newValue) }
-       }
+        get { tempoRelay.value }
+        set { tempoRelay.accept(newValue) }
+    }
+    var beatsRelay = BehaviorRelay<Int>(value: 1)
+    var beats: Int {
+        get { beatsRelay.value }
+        set { beatsRelay.accept(newValue) }
+    }
 
-    public init(metronome: MetronomeType) {
+    public init(metronome: MetronomeType, beats: Int = 1) {
         self.metronome = metronome
+        self.beats = beats
+
         tempoRelay
             .subscribe(onNext: { [weak self] tempo in
                 self?.delegate?.didUpdateTempo(tempo: tempo)
-                // 기존의 start, stop 함수는 tempo의 변화를 감지하지 않으므로, 이 부분에서 재생/정지 로직을 수행해야 함
                 if self?.metronome.isPlaying ?? false {
                     self?.metronome.stop()
-                    self?.metronome.play(bpm: Double(tempo))
+                    self?.metronome.play(bpm: Double(tempo), beats: self?.beats ?? 1)
                 }
             })
             .disposed(by: disposeBag)
     }
 
     func start() {
-        metronome.play(bpm: Double(tempo))
+        guard !isPlaying else { return }
+
+        isPlaying = true
+        metronome.play(bpm: Double(tempo), beats: beats)
     }
 
     func stop() {
+        guard isPlaying else { return }
+               
+        isPlaying = false
         metronome.stop()
     }
 }
