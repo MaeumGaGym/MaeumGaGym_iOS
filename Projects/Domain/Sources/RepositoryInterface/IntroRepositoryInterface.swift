a9f88a8d9d49241eb50068f1460f1d36b9cba40a
s <tylereunholee010203@gmail.com>
2024-02-15 21:22:20 +0900

    🍗 :: [#185] 인트로 레포지토리 인터페이스 추가

--- Projects/Domain/Sources/RepositoryInterface/IntroRepositoryInterface.swift
+++ Projects/Domain/Sources/RepositoryInterface/IntroRepositoryInterface.swift
@@ -7,4 +7,5 @@ public protocol IntroRepositoryInterface {
     func requestSignIn(token: String) -> Single<Bool>
     func kakaoToken(access_token: String) -> Single<String>
     func getCSRFToken() -> Single<String>
+    func getIntroData() -> Single<IntroModel>
 }
