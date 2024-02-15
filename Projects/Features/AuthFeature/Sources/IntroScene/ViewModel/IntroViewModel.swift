1e1970e7ea3bf3c083ba6a68cf96f130fc1f88ea
s <tylereunholee010203@gmail.com>
2024-02-15 21:24:20 +0900

    ⚓️ :: [#185] 인트로 viewModel
    getData 추가

--- Projects/Features/AuthFeature/Sources/IntroScene/ViewModel/IntroViewModel.swift
+++ Projects/Features/AuthFeature/Sources/IntroScene/ViewModel/IntroViewModel.swift
@@ -1,11 +1,3 @@
-//
-//  IntroViewModel.swift
-//  AuthFeatureInterface
-//
-//  Created by 박준하 on 2/2/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
-
 import Foundation
 
 import RxFlow
@@ -28,38 +20,43 @@ import MGLogger
 import TokenManager
 
 public class IntroViewModel: AuthViewModelType {
-
-    let provider = MoyaProvider<CsrfAPI>()
-
+    
     public var disposeBag: RxSwift.DisposeBag
-
+    
     private let useCase: AuthUseCase
-
+    
     let keychainCSRF = KeychainType.CSRFToken
-
+    
     public struct Input {
         let goolgeButtonTapped: Driver<Void>
         let appleButtonTapped: Driver<Void>
         let kakaoButtonTapped: Driver<Void>
+        let getIntroData: Driver<Void>
     }
-
-    public struct Output { 
-        let loginResult: Driver<Result<AuthHandleableType, Error>>
+    
+    public struct Output {
+        var introDatas: Observable<IntroModel>
     }
-
+    
     public var goolgeButtonTap: (() -> Void)?
     public var appleButtonTap: (() -> Void)?
     public var kakaoButtonTap: (() -> Void)?
-
+    
+    private let introModelSubject = PublishSubject<IntroModel>()
+    
     public init(authUseCase: AuthUseCase) {
-         self.useCase = authUseCase
+        self.useCase = authUseCase
         self.disposeBag = DisposeBag()
-     }
-
+    }
+    
     public func transform(_ input: Input, action: (Output) -> Void) -> Output {
-
-           let loginResultSubject = PublishSubject<Result<AuthHandleableType, Error>>()
-
+        
+        let output = Output(introDatas: introModelSubject.asObservable())
+        
+        action(output)
+        
+        bindOutput(output: output)
+        
         useCase.getCSRFToken()
             .subscribe(onSuccess: { token in
                 if TokenManagerImpl().save(token: token, with: self.keychainCSRF) {
@@ -71,45 +68,53 @@ public class IntroViewModel: AuthViewModelType {
                 print("Error: \(error)")
             })
             .disposed(by: disposeBag)
-
+        
         input.goolgeButtonTapped
             .drive(onNext: { [weak self] _ in
                 self?.useCase.requestSignIn(token: "googleToken")
             })
             .disposed(by: disposeBag)
-
-           input.kakaoButtonTapped
-               .drive(onNext: { [weak self] _ in
-                   self?.useCase.kakaoButtonTap()
-               })
-               .disposed(by: disposeBag)
-
-           input.appleButtonTapped
-               .drive(onNext: { [weak self] _ in
-                   self?.appleButtonTap?()
-               })
-               .disposed(by: disposeBag)
-
-           let output = Output(loginResult: loginResultSubject.asDriver(onErrorJustReturn: .failure(AuthError.unknown)))
-
-           action(output)
-
-           return output
-       }
+        
+        input.kakaoButtonTapped
+            .drive(onNext: { [weak self] _ in
+                self?.useCase.kakaoButtonTap()
+            })
+            .disposed(by: disposeBag)
+        
+        input.appleButtonTapped
+            .drive(onNext: { [weak self] _ in
+                self?.appleButtonTap?()
+            })
+            .disposed(by: disposeBag)
+        
+        input.getIntroData
+            .drive(onNext: { [weak self] _ in
+                self?.useCase.getIntroData()
+            }).disposed(by: disposeBag)
+        
+        return output
+    }
+    
+    private func bindOutput(output: Output) {
+        useCase.introData
+            .subscribe(onNext: { introData in
+                self.introModelSubject.onNext(introData)
+            }).disposed(by: disposeBag)
+    }
 }
 
 private extension IntroViewModel {
-//    func kakaoGetUserInfo() {
-//        UserApi.shared.me() { (user, error) in
-//            if let error = error {
-//                print(error)
-//            }
-//
-//            let userName = user?.kakaoAccount?.name
-//
-//            _ = "user name : \(String(describing: userName))"
-//
-//            print("user - \(String(describing: user))")
-//        }
-//    }
+    //    func kakaoGetUserInfo() {
+    //        UserApi.shared.me() { (user, error) in
+    //            if let error = error {
+    //                print(error)
+    //            }
+    //
+    //            let userName = user?.kakaoAccount?.name
+    //
+    //            _ = "user name : \(String(describing: userName))"
+    //
+    //            print("user - \(String(describing: user))")
+    //        }
+    //    }
 }
