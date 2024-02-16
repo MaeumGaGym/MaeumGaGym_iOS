c4e861876debae4e9c99e7915c204ac3e3de2303
junha <goodjunha@gmail.com>
2024-02-16 22:34:01 +0900

    🍗:: [#188] apple service 추가

--- Projects/Modules/MGNetworks/Sources/Service/IntroService.swift
+++ Projects/Modules/MGNetworks/Sources/Service/IntroService.swift
@@ -8,10 +8,15 @@ import Moya
 
 import Domain
 import DSKit
+import AuthenticationServices
 
-public class IntroService {
+import MGLogger
+
+public class IntroService: NSObject {
     
     let provider = MoyaProvider<CsrfAPI>()
+    
+    private let appleLoginSubject = PublishSubject<String>()
     
     public func requestToken() -> Single<Bool> {
         return Single.just(true)
@@ -49,7 +54,41 @@ public class IntroService {
         return Single.just(IntroModel(image: DSKitAsset.Assets.blueHome.image, mainTitle: "이제 헬창이 되어보세요!", subTitle: "저희의 좋은 서비스를 통해 즐거운 생활을\n즐겨보세요!"))
     }
     
-    public init() {
+    public func appleLogin() -> Single<String> {
+        let appleProvider = ASAuthorizationAppleIDProvider()
+        let request = appleProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.performRequests()
+
+        controller.delegate = self
+
+        return appleLoginSubject.take(1).asSingle()
+    }
+    
+    public override init() {
         
+    }
+}
+
+extension IntroService: ASAuthorizationControllerDelegate {
+    public func authorizationController(controller: ASAuthorizationController,
+                                        didCompleteWithAuthorization authorization: ASAuthorization
+    ) {
+        switch authorization.credential {
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            if let identityToken = appleIDCredential.identityToken,
+               let tokenString = String(data: identityToken, encoding: .utf8) {
+                appleLoginSubject.onNext(tokenString)
+                appleLoginSubject.onCompleted()
+            }
+        default:
+            break
+        }
+    }
+
+    public func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        appleLoginSubject.onError(error)
     }
 }
