411092139b0ff872442c6f3c156eb3ef665fc826
s <tylereunholee010203@gmail.com>
2024-02-19 15:28:29 +0900

    ⚓️ :: [#190] 자세 - service
    자세 - 추천 탭 requestdata함수 적용

--- Projects/Modules/MGNetworks/Sources/Service/PostureService.swift
+++ Projects/Modules/MGNetworks/Sources/Service/PostureService.swift
@@ -5,18 +5,31 @@ import RxSwift
 import Domain
 import DSKit
 
-public class PostureRecommandService {
-    public func requestRecommandTitle() ->
-    Single<[PostureRecommandTitleModel]> {
-        let titles: [PostureRecommandTitleModel] = [
-            PostureRecommandTitleModel(image: DSKitAsset.Assets.postureArmLogo.image, title: "맨몸 운동"),
-            PostureRecommandTitleModel(image: DSKitAsset.Assets.postureDumbelLogo.image, title: "기구 운동")
+public class PostureService {
+    public func requestRecommandData() ->
+    Single<[PostureRecommandModel]> {
+        let postureRecommandData: [PostureRecommandModel] = [
+            PostureRecommandModel(
+                titleImage: DSKitAsset.Assets.postureArmLogo.image,
+                titleText: "맨몸 운동",
+                exerciseData: [
+                    PostureRecommandExerciseModel(image: DSKitAsset.Assets.pushUp.image, name: "푸시업", part: "가슴"),
+                    PostureRecommandExerciseModel(image: DSKitAsset.Assets.bodySplitSqt.image, name: "맨몸 스플릿 스쿼트", part: "하체"),
+                    PostureRecommandExerciseModel(image: DSKitAsset.Assets.backExtension.image, name: "백 익스텐션", part: "등")
+                ]
+            ),
+            PostureRecommandModel(
+                titleImage: DSKitAsset.Assets.postureDumbelLogo.image,
+                titleText: "기구 운동",
+                exerciseData: [
+                    PostureRecommandExerciseModel(image: DSKitAsset.Assets.deeps.image, name: "딥스", part: "가슴"),
+                    PostureRecommandExerciseModel(image: DSKitAsset.Assets.benchPress.image, name: "벤치프레스", part: "가슴"),
+                    PostureRecommandExerciseModel(image: DSKitAsset.Assets.runge.image, name: "런지", part: "팔")
+                ]
+            )
         ]
-        return Single.just(titles)
+        return Single.just(postureRecommandData)
     }
-    
-    public func requesRecommandFirstExercise() ->
-    Single
     
     public init() {
         
