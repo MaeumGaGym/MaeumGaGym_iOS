4224428f652efb8db58476ce2f61752f509c72e8
s <tylereunholee010203@gmail.com>
2024-02-19 15:55:04 +0900

    ⚓️ :: [#190] 자세 - UseCase
    추천 탭 정보 가져오는 함수 추가

--- Projects/Domain/Sources/UseCase/PostureUseCase.swift
+++ Projects/Domain/Sources/UseCase/PostureUseCase.swift
@@ -1,9 +1,32 @@
-//
-//  PostureUseCase.swift
-//  Domain
-//
-//  Created by 이은호 on 2/19/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import RxSwift
+
+public protocol PostureUseCase {
+    var recommandData: PublishSubject<[PostureRecommandModel]> { get }
+    
+    func getRecommandData()
+}
+
+public class DefaultPostureUseCase {
+    private let repository: PostureRepositoryInterface
+    private let disposeBag = DisposeBag()
+    
+    public let recommandData = PublishSubject<[PostureRecommandModel]>()
+    
+    public init(repository: PostureRepositoryInterface) {
+        self.repository = repository
+    }
+}
+
+extension DefaultPostureUseCase: PostureUseCase {
+    public func getRecommandData() {
+        repository.getRecommandData()
+            .subscribe(onSuccess: { [weak self] recommandData in
+                self?.recommandData.onNext(recommandData)
+            },
+            onFailure: { error in
+                print("PostureUseCase getRecommandData error occurred: \(error)")
+            }).disposed(by: disposeBag)
+    }
+}
