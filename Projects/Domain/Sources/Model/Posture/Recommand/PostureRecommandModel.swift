cc1556a8dc4cc6719e2498da76a35ef96ea617f9
s <tylereunholee010203@gmail.com>
2024-02-19 15:27:52 +0900

    ⚓️ :: [#190] 자세 - 추천 탭
    model 추가

--- Projects/Domain/Sources/Model/Posture/Recommand/PostureRecommandModel.swift
+++ Projects/Domain/Sources/Model/Posture/Recommand/PostureRecommandModel.swift
@@ -1,9 +1,25 @@
-//
-//  PostureRecommandModel.swift
-//  Domain
-//
-//  Created by 이은호 on 2/19/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+public struct PostureRecommandModel {
+    public var titleImage: UIImage
+    public var titleText: String
+    public var exerciseData: [PostureRecommandExerciseModel]
+    
+    public init(titleImage: UIImage, titleText: String, exerciseData: [PostureRecommandExerciseModel]) {
+        self.titleImage = titleImage
+        self.titleText = titleText
+        self.exerciseData = exerciseData
+    }
+}
+
+public struct PostureRecommandExerciseModel {
+    public var image: UIImage
+    public var name: String
+    public var part: String
+    
+    public init(image: UIImage, name: String, part: String) {
+        self.image = image
+        self.name = name
+        self.part = part
+    }
+}
