b152d9020af9d2cecb5003176a2b9daf1bdfea04
s <tylereunholee010203@gmail.com>
2024-02-19 17:32:03 +0900

    ⚓️ :: [#190] 자세 - 추천 탭 ViewModel
    model 받아오는 코드 추가

--- Projects/Features/PostureFeature/Sources/PostureScene/ViewModel/Tab/PostureRecommandViewModel.swift
+++ Projects/Features/PostureFeature/Sources/PostureScene/ViewModel/Tab/PostureRecommandViewModel.swift
@@ -5,21 +5,52 @@ import RxCocoa
 import RxSwift
 
 import Core
+import Domain
+
 
 public class PostureRecommandViewModel: BaseViewModel {
-    public func transform(_ input: Input, action: (Output) -> Void) -> Output {
-        return Output()
-    }
+    
+    public var disposeBag: DisposeBag = DisposeBag()
+    
+    private let useCase: PostureUseCase
+
 
     public struct Input {
-
+        let getRecommandData: Driver<Void>
     }
 
     public struct Output {
-
+        var recommandData: Observable<[PostureRecommandModel]>
     }
 
-    public init() {
+    public init(useCase: PostureUseCase) {
+        self.useCase = useCase
+    }
+    
+    private let recommandDataSubject = PublishSubject<[PostureRecommandModel]>()
+    
+    public func transform(_ input: Input, action: (Output) -> Void) -> Output {
+        let output = Output(
+            recommandData: recommandDataSubject.asObservable()
+        )
+        
+        action(output)
+        
+        self.bindOutput(output: output)
+        
+        input.getRecommandData
+            .drive(onNext: { [weak self] _ in
+                self?.useCase.getRecommandData()
+            }).disposed(by: disposeBag)
+        
+        return output
+    }
+    
+    private func bindOutput(output: Output) {
 
+        useCase.recommandData
+            .subscribe(onNext: { recommandData in
+                self.recommandDataSubject.onNext(recommandData)
+            }).disposed(by: disposeBag)
     }
 }
