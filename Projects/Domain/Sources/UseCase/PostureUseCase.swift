1340de4a5487675d6e5481188e45678f909c16d7
s <tylereunholee010203@gmail.com>
2024-02-19 23:16:56 +0900

    ⚓️ :: [#196] 자세 - UseCase
    부위별 탭 데이터 가져오는 함수 추가

--- Projects/Domain/Sources/UseCase/PostureUseCase.swift
+++ Projects/Domain/Sources/UseCase/PostureUseCase.swift
@@ -5,15 +5,20 @@ import RxSwift
 public protocol PostureUseCase {
     var recommandData: PublishSubject<[PostureRecommandModel]> { get }
     
+    var partData: PublishSubject<PosturePartModel> { get }
+
     func getRecommandData()
+    func getPartData(type: PosturePartType)
 }
 
 public class DefaultPostureUseCase {
     private let repository: PostureRepositoryInterface
     private let disposeBag = DisposeBag()
-    
+
     public let recommandData = PublishSubject<[PostureRecommandModel]>()
-    
+
+    public let partData = PublishSubject<PosturePartModel>()
+
     public init(repository: PostureRepositoryInterface) {
         self.repository = repository
     }
@@ -27,6 +32,16 @@ extension DefaultPostureUseCase: PostureUseCase {
             },
             onFailure: { error in
                 print("PostureUseCase getRecommandData error occurred: \(error)")
+            }).disposed(by: disposeBag)
+    }
+
+    public func getPartData(type: PosturePartType) {
+        repository.getPartData(type: type)
+            .subscribe(onSuccess: { [weak self] partData in
+                self?.partData.onNext(partData)
+            },
+            onFailure: { error in
+                print("PostureUseCase getPartData error occurred: \(error)")
             }).disposed(by: disposeBag)
     }
 }
