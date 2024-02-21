6497cb6db6ee97cbcd8ba8dd631c8d95403f8b34
junha <goodjunha@gmail.com>
2024-02-21 14:11:04 +0900

    ⚓️:: [#202] navigationBar 추가

---
+++ Projects/Features/SelfCareFeature/Sources/CameraScene/View/NavigationBar/CameraNavigationBar.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+import Core
+import DSKit
+
+final public class CameraNavigationBar: UIView {
+
+    public var leftButtonTap: ControlEvent<Void> {
+        return leftButton.rx.tap
+    }
+
+    private let leftButton = UIButton(type: .custom).then {
+        $0.setImage(DSKitAsset.Assets.pickleCancel.image, for: .normal)
+    }
+
+    private let leftLabel = UILabel().then {
+        $0.font = UIFont.Pretendard.titleMedium
+        $0.text = "사진 촬영"
+    }
+
+    private lazy var leftItemsStackView = UIStackView(arrangedSubviews: [leftButton, leftLabel]).then {
+        $0.axis = .horizontal
+        $0.spacing = 0
+        $0.distribution = .fillEqually
+    }
+
+    public init() {
+        super.init(frame: .zero)
+        setUI()
+        setLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension CameraNavigationBar {
+    func setUI() {
+        self.backgroundColor = .black
+    }
+
+    func setLayout() {
+        self.addSubview(leftItemsStackView)
+
+        self.snp.makeConstraints {
+            $0.height.equalTo(48.0)
+        }
+
+        leftItemsStackView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalToSuperview()
+        }
+    }
+}
