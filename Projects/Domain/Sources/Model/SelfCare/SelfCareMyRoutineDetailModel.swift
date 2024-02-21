ca7a871c7db3023429be4141944c9a8782eabe60
s <tylereunholee010203@gmail.com>
2024-02-21 13:17:47 +0900

    ⚓️ :: [#204] 자기관리 - 내 루틴 - 디테일 - Model
    model 추가

--- Projects/Domain/Sources/Model/SelfCare/SelfCareMyRoutineDetailModel.swift
+++ Projects/Domain/Sources/Model/SelfCare/SelfCareMyRoutineDetailModel.swift
@@ -1,9 +1,25 @@
-//
-//  SelfCareMyRoutineDetailModel.swift
-//  Domain
-//
-//  Created by 이은호 on 2/21/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+public struct SelfCareMyRoutineDetailModel {
+    public var routineTitleData: SelfCareRoutineModel
+    public var routinesData: [SelfCareMyRoutineDetailExerciseModel]
+
+    public init(routineTitleData: SelfCareRoutineModel, routinesData: [SelfCareMyRoutineDetailExerciseModel]) {
+        self.routineTitleData = routineTitleData
+        self.routinesData = routinesData
+    }
+}
+
+public struct SelfCareMyRoutineDetailExerciseModel {
+    public var exericseImage: UIImage
+    public var exerciseTitle: String
+    public var exerciseCount: Int
+    public var exerciseSet: Int
+
+    public init(exericseImage: UIImage, exerciseTitle: String, exerciseCount: Int, exerciseSet: Int) {
+        self.exericseImage = exericseImage
+        self.exerciseTitle = exerciseTitle
+        self.exerciseCount = exerciseCount
+        self.exerciseSet = exerciseSet
+    }
+}
