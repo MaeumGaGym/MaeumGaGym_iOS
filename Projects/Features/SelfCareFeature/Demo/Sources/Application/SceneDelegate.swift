5e93ad684bfd24e5a9bd9bd63f7ba77cb27e4dab
s <tylereunholee010203@gmail.com>
2024-02-21 11:36:12 +0900

    🍗 :: [#200] 자기관리 - SceneDelegate
    클린아키텍쳐 적용

--- Projects/Features/SelfCareFeature/Demo/Sources/Application/SceneDelegate.swift
+++ Projects/Features/SelfCareFeature/Demo/Sources/Application/SceneDelegate.swift
@@ -2,18 +2,25 @@ import UIKit
 
 import RxFlow
 
+import Domain
+import Data
+import MGNetworks
+
 import SelfCareFeature
-import Core
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
-            
+
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: SelfCareMyRoutineEditViewController(SelfCareMyRoutineEditViewModel()))
+        let useCase = DefaultSelfCareUseCase(repository: SelfCareRepository(networkService: SelfCareService()))
+        let viewModel = SelfCareMyRoutineViewModel(useCase: useCase)
+        let viewController = SelfCareMyRoutineViewController(viewModel)
+        window?.configure(withRootViewController: viewController)
         window?.makeKeyAndVisible()
     }
 }
