2f5ab88f628f22d6e09860513ec41f4fb3233daa
박준하 <goodjunha@gmail.com>
2024-02-22 13:51:00 +0900

    ⚓️:: [#214] SelfCareHomeViewController 추가

---
+++ Projects/Features/SelfCareFeature/Sources/HomeScene/View/Cell/SelfCareIntroductTableViewCell.swift
@@ -0,0 +1,75 @@
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+import Then
+import SnapKit
+
+import Core
+import DSKit
+
+import Domain
+
+public class SelfCareIntroductTableViewCell: BaseTableViewCell {
+
+    static public var identifier: String = "SelfCareIntroductTableViewCell"
+
+    private let titleImageView = MGProfileView(
+        profileImage: MGProfileImage(type: .custom,
+                                     customImage: DSKitAsset.Assets.selfCareMainImage.image),
+        profileType: .smallProfile
+    )
+
+    private let mainTitle = UILabel().then {
+        $0.font = UIFont.Pretendard.titleLarge
+    }
+
+    private let subTitle = UILabel().then {
+        $0.font = UIFont.Pretendard.bodyMedium
+        $0.textColor = DSKitAsset.Colors.gray600.color
+        $0.numberOfLines = 2
+    }
+
+    public func configure(with message: SelfCareIntroductModel) {
+        titleImageView.configureImage(with: MGProfileImage(type: .custom,
+                                                       customImage: message.image))
+        mainTitle.text = "\"\(message.mainText)\""
+        subTitle.text = "\(message.subText)"
+    }
+
+    public override func attribute() {
+        super.attribute()
+
+        backgroundColor = .white
+        setupRxBindings()
+    }
+
+    public override func layout() {
+        super.layout()
+
+        addSubviews([titleImageView, mainTitle, subTitle])
+        titleImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(40.0)
+            $0.leading.equalToSuperview().inset(20.0)
+        }
+
+        mainTitle.snp.makeConstraints {
+            $0.top.equalTo(titleImageView.snp.bottom).offset(12.0)
+            $0.leading.equalTo(titleImageView.snp.leading)
+        }
+
+        subTitle.snp.makeConstraints {
+            $0.top.equalTo(mainTitle.snp.bottom).offset(12.0)
+            $0.leading.equalTo(mainTitle.snp.leading)
+        }
+    }
+
+    private func setupRxBindings() {
+        rx.deallocated
+            .subscribe(onNext: { [weak self] in
+                self?.disposeBag = DisposeBag()
+            })
+            .disposed(by: disposeBag)
+    }
+}
