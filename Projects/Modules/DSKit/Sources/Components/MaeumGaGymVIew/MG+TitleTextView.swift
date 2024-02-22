d84c8c59531ae31f97616d1cec56efd0dd44411e
s <tylereunholee010203@gmail.com>
2024-02-22 15:55:07 +0900

    🍗 :: [#211] 레이아웃 리펙토링

--- Projects/Modules/DSKit/Sources/Components/MaeumGaGymVIew/MG+TitleTextView.swift
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymVIew/MG+TitleTextView.swift
@@ -6,7 +6,7 @@ import Then
 import Core
 
 public class MGTitleTextView: BaseView {
-    
+
     private var titleLabel = UILabel().then {
         $0.font = UIFont.Pretendard.bodyMedium
         $0.textAlignment = .left
@@ -14,34 +14,44 @@ public class MGTitleTextView: BaseView {
         $0.numberOfLines = 1
     }
     
+    private var containerView = UIView().then {
+        $0.backgroundColor = DSKitAsset.Colors.gray25.color
+        $0.layer.cornerRadius = 8.0
+        $0.layer.borderColor = DSKitAsset.Colors.gray50.color.cgColor
+        $0.layer.borderWidth = 1.0
+    }
+
     private var textField = MGTitleTextField()
-    
-    public init (
-        type: TitleTextFieldType
-    ) {
-        super.init(frame: .zero)
-        titleLabel.text = type.titleText
-        textField.setPlaceholder(placeholder: type.placeHolder)
-        
-        layout()
-    }
-    
-    required public init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+
     public override func layout() {
-        addSubviews([titleLabel, textField])
-        
+        addSubviews([titleLabel, containerView])
+        containerView.addSubviews([textField])
+
         titleLabel.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
             $0.height.equalTo(20.0)
         }
         
-        textField.snp.makeConstraints {
+        containerView.snp.makeConstraints {
             $0.top.equalTo(titleLabel.snp.bottom).offset(8.0)
             $0.bottom.equalToSuperview()
             $0.leading.trailing.equalToSuperview()
         }
+
+        textField.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(12.0)
+            $0.trailing.leading.equalToSuperview().inset(12.0)
+        }
+    }
+}
+
+public extension MGTitleTextView {
+    func setup(
+        titleText: String,
+        placeholder: String,
+        text: String
+    ) {
+        titleLabel.text = titleText
+        textField.text = text
     }
 }
