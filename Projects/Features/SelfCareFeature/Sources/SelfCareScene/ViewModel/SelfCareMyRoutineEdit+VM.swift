842a5fe8275673226182318424c5b69f9f5d90da
s <tylereunholee010203@gmail.com>
2024-02-22 15:56:14 +0900

    ⚓️ :: [#211] 자기관리 - 내 루틴 - 루틴 수정하기 - ViewModel

--- Projects/Features/SelfCareFeature/Sources/SelfCareScene/ViewModel/SelfCareMyRoutineEdit+VM.swift
+++ Projects/Features/SelfCareFeature/Sources/SelfCareScene/ViewModel/SelfCareMyRoutineEdit+VM.swift
@@ -5,21 +5,49 @@ import RxCocoa
 import RxSwift
 
 import Core
+import Domain
+import MGLogger
 
 public class SelfCareMyRoutineEditViewModel: BaseViewModel {
-    public func transform(_ input: Input, action: (Output) -> Void) -> Output {
-        return Output()
-    }
+    private let disposeBag = DisposeBag()
+
+    private let useCase: SelfCareUseCase
 
     public struct Input {
-
+        let getMyRoutineEditData: Driver<Void>
     }
 
     public struct Output {
-
+        let myRoutineEditData: Observable<SelfCareMyRoutineEditModel>
     }
 
-    public init() {
+    private let myRoutineEditDataSubject = PublishSubject<SelfCareMyRoutineEditModel>()
 
+    public init(useCase: SelfCareUseCase) {
+        self.useCase = useCase
+    }
+
+    public func transform(_ input: Input, action: (Output) -> Void) -> Output {
+        let output = Output(
+            myRoutineEditData: myRoutineEditDataSubject.asObservable()
+        )
+
+        action(output)
+
+        self.bindOutput(output: output)
+
+        input.getMyRoutineEditData
+            .drive(onNext: { [weak self] _ in
+                self?.useCase.getMyRoutineEditData()
+            }).disposed(by: disposeBag)
+
+        return output
+    }
+
+    private func bindOutput(output: Output) {
+        useCase.myRoutineEditData
+            .subscribe(onNext: { myRoutineEditData in
+                self.myRoutineEditDataSubject.onNext(myRoutineEditData)
+            }).disposed(by: disposeBag)
     }
 }
