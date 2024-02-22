9d066eebe0465df2710a44922096ffd57c03a204
s <tylereunholee010203@gmail.com>
2024-02-22 15:57:28 +0900

    🍗 :: [#211] conflict 해결

--- Projects/Features/SelfCareFeature/Demo/Sources/Application/SceneDelegate.swift
+++ Projects/Features/SelfCareFeature/Demo/Sources/Application/SceneDelegate.swift
@@ -18,8 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         window = UIWindow(windowScene: windowScene)
         let useCase = DefaultSelfCareUseCase(repository: SelfCareRepository(networkService: SelfCareService()))
-        let viewModel = SelfCareMyRoutineEditViewModel(useCase: useCase)
-        let viewController = SelfCareMyRoutineEditViewController(viewModel)
+        let viewModel = SelfCareMyRoutineDetailViewModel(useCase: useCase)
+        let viewController = SelfCareMyRoutineDetailViewController(viewModel)
         window?.configure(withRootViewController: viewController)
         window?.makeKeyAndVisible()
     }
