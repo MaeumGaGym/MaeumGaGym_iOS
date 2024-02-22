24cb70c6f7c8cbcdf3ccb7555ef41462eed8f17a
박준하 <goodjunha@gmail.com>
2024-02-22 13:48:42 +0900

    ⚓️:: [#214] SelfCareMenuCollectionCell 추가

---
+++ Projects/Features/SelfCareFeature/Sources/HomeScene/View/Cell/SelfCareMenuCollectionCell.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+import Then
+import SnapKit
+
+import Core
+import DSKit
+
+import Domain
+
+public class SelfCareMenuCollectionCell: UICollectionViewCell {
+
+    static let identifier: String = "SelfCareMenuCollectionCell"
+
+    private var menuImageView = UIImageView().then {
+        $0.backgroundColor = .red
+        $0.layer.cornerRadius = 8.0
+    }
+
+    private var menuLabel = UILabel().then {
+        $0.textColor = .black
+        $0.textAlignment = .center
+    }
+// 보류
+//    private var arrowImageView = UIImageView().then {
+//        $0.image = UIImage(systemName: "chevron.right")
+//        $0.tintColor = .gray
+//    }
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        contentView.addSubviews([menuImageView,
+                     menuLabel])
+
+        menuImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(20.0)
+            $0.height.width.equalTo(40.0)
+        }
+
+        menuLabel.snp.makeConstraints {
+            $0.leading.equalTo(menuImageView.snp.trailing).offset(16.0)
+            $0.centerY.equalTo(menuImageView.snp.centerY)
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public func configure(with menus: SelfCareMenuModel) {
+        menuImageView.image = menus.menuImage
+        menuLabel.text = menus.menuName
+    }
+}
