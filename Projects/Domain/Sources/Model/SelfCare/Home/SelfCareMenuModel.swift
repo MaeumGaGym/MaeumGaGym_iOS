9e5dfb636377963bcec992cc31a7f39d92929c2a
박준하 <goodjunha@gmail.com>
2024-02-22 13:45:32 +0900

    ⚓️:: [#214] model 추가

---
+++ Projects/Domain/Sources/Model/SelfCare/Home/SelfCareMenuModel.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+public struct SelfCareMenuModel {
+    public var menuImage: UIImage
+    public var menuName: String
+
+    public init(menuImage: UIImage, menuName: String) {
+        self.menuImage = menuImage
+        self.menuName = menuName
+    }
+}
