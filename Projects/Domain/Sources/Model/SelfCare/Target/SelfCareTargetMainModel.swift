49aa3aa8b27016dec6b3e76d78dcfc4e9c58bbb2
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-02-25 12:33:10 +0900

    ⚓️ :: [#217] 자기관리 - 목표 - 메인
    model, tableViewCell 파일 추가

---
+++ Projects/Domain/Sources/Model/SelfCare/Target/SelfCareTargetMainModel.swift
@@ -0,0 +1,9 @@
+//
+//  SelfCareTargetMainModel.swift
+//  Domain
+//
+//  Created by 이은호 on 2/25/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
---
+++ Projects/Features/SelfCareFeature/Sources/Supporter/Target/Main/TargetMainTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  TargetMainTableViewCell.swift
+//  SelfCareFeatureInterface
+//
+//  Created by 이은호 on 2/25/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
