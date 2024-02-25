c985288d67956124507da407d79e118528edb15d
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-02-25 12:34:29 +0900

    ⚓️ :: [#217] 자기관리 - 목표 - tableViewCell 추가

--- Projects/Features/SelfCareFeature/Sources/Supporter/Target/Main/TargetMainTableViewCell.swift
+++ Projects/Features/SelfCareFeature/Sources/Supporter/Target/Main/TargetMainTableViewCell.swift
@@ -1,9 +1,84 @@
-//
-//  TargetMainTableViewCell.swift
-//  SelfCareFeatureInterface
-//
-//  Created by 이은호 on 2/25/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import SnapKit
+import Then
+
+import Core
+import DSKit
+import Domain
+
+public class TargetMainTableViewCell: BaseTableViewCell {
+
+    public static let identifier: String = "TargetMainTableViewCell"
+
+    private var containerView = UIView().then {
+        $0.backgroundColor = DSKitAsset.Colors.blue50.color
+        $0.layer.cornerRadius = 16.0
+    }
+
+    private var targetNameLabel = UILabel().then {
+        $0.font = UIFont.Pretendard.labelLarge
+        $0.textColor = .black
+        $0.numberOfLines = 1
+    }
+
+    private var targetPeriodLabel = UILabel().then {
+        $0.font = UIFont.Pretendard.bodySmall
+        $0.numberOfLines = 1
+        $0.textColor = DSKitAsset.Colors.gray400.color
+    }
+
+    private let dotsButton = UIButton().then {
+        $0.setImage(DSKitAsset.Assets.selfCareDots.image, for: .normal)
+    }
+
+    public func setup(with model: TargetContentModel) {
+        changeTargetName(text: model.targetTitle)
+        changeTargetDate(
+            startDate: model.targetStartData,
+            endDate: model.targetEndData
+        )
+    }
+
+    override public func layout() {
+        super.layout()
+
+        contentView.addSubviews([containerView])
+        containerView.addSubviews([targetNameLabel, targetPeriodLabel, dotsButton])
+
+        containerView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(12.0)
+            $0.leading.trailing.equalToSuperview().inset(20.0)
+        }
+
+        targetNameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(18.0)
+            $0.leading.equalToSuperview().offset(20.0)
+            $0.width.equalToSuperview()
+            $0.height.equalTo(24.0)
+        }
+
+        targetPeriodLabel.snp.makeConstraints {
+            $0.top.equalTo(targetNameLabel.snp.bottom).offset(4.0)
+            $0.leading.equalToSuperview().offset(20.0)
+            $0.height.equalTo(18.0)
+        }
+
+        dotsButton.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(29.0)
+            $0.trailing.equalToSuperview().offset(-20.0)
+            $0.width.height.equalTo(24.0)
+        }
+    }
+}
+
+public extension TargetMainTableViewCell {
+
+    func changeTargetName(text: String) {
+        targetNameLabel.text = text
+    }
+
+    func changeTargetDate(startDate: String, endDate: String) {
+        targetPeriodLabel.text = "\(startDate) ~ \(endDate)"
+    }
+}
