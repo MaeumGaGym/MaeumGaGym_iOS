e8aa55bb23aafa34634e6ea780c796829aa04cb3
박준하 <goodjunha@gmail.com>
2024-02-25 14:10:04 +0900

    🍗:: [#224] BaseLabel 리펙토링

--- Projects/Core/Sources/Base/BaseLabel.swift
+++ Projects/Core/Sources/Base/BaseLabel.swift
@@ -27,4 +27,21 @@ open class BaseLabel: UILabel {
     open func layout() {
         // 서브뷰를 구성하고 SnapKit을 사용해서 layout을 하는 함수입니다.
     }
+    
+    public func setPretendardFont(font: UIFont) {
+        // PretendardFont를 받습니다.
+        self.font = font
+    }
+    
+    public func setColor(textColor: UIColor? = .black, backgroudColor: UIColor? = .black) {
+        // DSKit의 color를 받아줍니다.
+        self.textColor = textColor
+        self.backgroundColor = backgroudColor
+    }
+
+    public func setTextAlignmentAndNumberOfLines(alignment: NSTextAlignment, numberOfLines: Int) {
+        // numberOfLines와 Alignment를 받아줍니다.
+        self.numberOfLines = numberOfLines
+        self.textAlignment = alignment
+    }
 }
