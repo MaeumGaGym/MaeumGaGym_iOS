ad950aaea6d77bc5a214451b9613752cf0cfd18c
박준하 <goodjunha@gmail.com>
2024-02-25 16:20:23 +0900

    ⚓️:: [#228] homeResurcesService 추가

---
+++ Projects/Modules/MGNetworks/Sources/ResourcesService/HomeResourcesService.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+import DSKit
+
+public enum HomeResourcesService {
+    public enum Colors {
+        public static let red = UIColor(named: "red")!
+    }
+
+    public enum Assets {
+        public static let rightNVButton = DSKitAsset.Assets.settingActIcon.image
+    }
+    public enum Title {
+        public static let help = "살려줘"
+    }
+}
