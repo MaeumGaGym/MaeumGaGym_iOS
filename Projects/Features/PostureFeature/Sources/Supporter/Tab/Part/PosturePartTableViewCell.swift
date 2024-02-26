993c15fe33321ce1f15467135a11f15cbd5848fd
s <tylereunholee010203@gmail.com>
2024-02-26 16:25:03 +0900

    🍗 :: [#234] conflict 해결

--- Projects/Features/PostureFeature/Sources/Supporter/Tab/Part/PosturePartTableViewCell.swift
+++ Projects/Features/PostureFeature/Sources/Supporter/Tab/Part/PosturePartTableViewCell.swift
@@ -7,23 +7,19 @@ import DSKit
 import Core
 import Domain
 
-import MGNetworks
-
 public class PosturePartTableViewCell: BaseTableViewCell {
 
-    static let identifier: String = PostureResourcesService.Identifier.posturePartTableViewCell
+    static let identifier: String = "PostureChestTableViewCell"
 
     private let postureImageView = UIImageView().then {
         $0.backgroundColor = .clear
         $0.layer.cornerRadius = 8.0
     }
 
-    private let exerciseNameLabel = UILabel().then {
-        $0.textColor = .black
-        $0.backgroundColor = .clear
-        $0.textAlignment = .left
-        $0.font = UIFont.Pretendard.bodyMedium
-    }
+    private let exerciseNameLabel = MGLabel(font: UIFont.Pretendard.bodyMedium,
+                                            textColor: .black,
+                                            isCenter: false
+    )
 
     public override func layout() {
         super.layout()
