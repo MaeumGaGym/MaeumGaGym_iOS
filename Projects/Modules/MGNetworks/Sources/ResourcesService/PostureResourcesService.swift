3f55557eecffe3adf02aec3d8df3547fb367d473
s <tylereunholee010203@gmail.com>
2024-02-26 14:43:00 +0900

    ⚓️ :: [#234] 자세 - PostureResourcesService
    자세 데이터 정리

--- Projects/Modules/MGNetworks/Sources/ResourcesService/PostureResourcesService.swift
+++ Projects/Modules/MGNetworks/Sources/ResourcesService/PostureResourcesService.swift
@@ -1,9 +1,95 @@
-//
-//  PostureResourcesService.swift
-//  MGNetworks
-//
-//  Created by 이은호 on 2/25/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import DSKit
+
+public enum PostureResourcesService {
+    public enum Assets {
+        public static let blueArmIcon = DSKitAsset.Assets.blueArmIcon.image
+        public static let blueDumbelIcon = DSKitAsset.Assets.blueDumbelIcon.image
+        public static let airSqt = DSKitAsset.Assets.airSqt.image
+        public static let archPushUp = DSKitAsset.Assets.archPushUp.image
+        public static let babelBackSqt = DSKitAsset.Assets.babelBackSqt.image
+        public static let babelBgrSplitSqt = DSKitAsset.Assets.babelBgrSplitSqt.image
+        public static let babelLow = DSKitAsset.Assets.babelLow.image
+        public static let backExtension = DSKitAsset.Assets.backExtension.image
+        public static let benchPress = DSKitAsset.Assets.benchPress.image
+        public static let bodyOverheadSqt = DSKitAsset.Assets.bodyOverheadSqt.image
+        public static let bodySplitSqt = DSKitAsset.Assets.bodySplitSqt.image
+        public static let clapPushUp = DSKitAsset.Assets.clapPushUp.image
+        public static let closeGripPushUp = DSKitAsset.Assets.closeGripPushUp.image
+        public static let deeps = DSKitAsset.Assets.deeps.image
+        public static let dumbelBenchPress = DSKitAsset.Assets.dumbelBenchPress.image
+        public static let dumbelBgrSplitSqt = DSKitAsset.Assets.dumbelBgrSplitSqt.image
+        public static let dumbelGoblinSqt = DSKitAsset.Assets.dumbelGoblinSqt.image
+        public static let dumbelLow = DSKitAsset.Assets.dumbelLow.image
+        public static let dumbelSplitSqt = DSKitAsset.Assets.dumbelSplitSqt.image
+        public static let dumbelSqzPress = DSKitAsset.Assets.dumbelSqzPress.image
+        public static let frontSqt = DSKitAsset.Assets.frontSqt.image
+        public static let goodMorningExercise = DSKitAsset.Assets.goodMorningExercise.image
+        public static let hinduPushUp = DSKitAsset.Assets.hinduPushUp.image
+        public static let hyperExtension = DSKitAsset.Assets.hyperExtension.image
+        public static let inclineBenchPress = DSKitAsset.Assets.inclineBenchPress.image
+        public static let inclineDumbelBenchPress = DSKitAsset.Assets.inclineDumbelBenchPress.image
+        public static let inclineDumbelLow = DSKitAsset.Assets.inclineDumbelLow.image
+        public static let invertedLow = DSKitAsset.Assets.invertedLow.image
+        public static let jumpSqt = DSKitAsset.Assets.jumpSqt.image
+        public static let lyingHeapAvdt = DSKitAsset.Assets.lyingHeapAvdt.image
+        public static let pullUp = DSKitAsset.Assets.pullUp.image
+        public static let pushUp = DSKitAsset.Assets.pushUp.image
+        public static let ratPullDown = DSKitAsset.Assets.ratPullDown.image
+        public static let runge = DSKitAsset.Assets.runge.image
+        public static let weightDeeps = DSKitAsset.Assets.weightDeeps.image
+        public static let posturePickleTest1 = DSKitAsset.Assets.posturePickleTest1.image
+        public static let posturePickleTest2 = DSKitAsset.Assets.posturePickleTest2.image
+        public static let posturePickleTest3 = DSKitAsset.Assets.posturePickleTest3.image
+        public static let posturePickleTest4 = DSKitAsset.Assets.posturePickleTest4.image
+        public static let postureChestTest1 = DSKitAsset.Assets.postureChestTest1.image
+        public static let postureChestTest2 = DSKitAsset.Assets.postureChestTest2.image
+        public static let postureChestTest3 = DSKitAsset.Assets.postureChestTest3.image
+        public static let postureChestTest4 = DSKitAsset.Assets.postureChestTest4.image
+        public static let postureChestTest5 = DSKitAsset.Assets.postureChestTest5.image
+        public static let postureChestTest6 = DSKitAsset.Assets.postureChestTest6.image
+        public static let postureChestTest7 = DSKitAsset.Assets.postureChestTest7.image
+    }
+
+    public enum Colors {
+        public static let gray25 = DSKitAsset.Colors.gray25.color
+        public static let gray50 = DSKitAsset.Colors.gray50.color
+        public static let gray400 = DSKitAsset.Colors.gray400.color
+        public static let gray600 = DSKitAsset.Colors.gray600.color
+    }
+
+    public enum Title {
+        public static let postureTitle = "자세"
+        public static let postureBodyTitle = "맨몸 운동"
+        public static let postureMachineTitle = "기구 운동"
+        public static let bodyTitle = "맨몸"
+        public static let machineTitle = "기구"
+        public static let execiseWayTitle = "운동 방법"
+        public static let cautionTitle = "주의 사항"
+        public static let relatedPickleTitle = "관련 피클"
+        public static let postureMainTitle1 = "추천"
+        public static let postureMainTitle2 = "가슴"
+        public static let postureMainTitle3 = "등"
+        public static let postureMainTitle4 = "어깨"
+        public static let postureMainTitle5 = "팔"
+        public static let postureMainTitle6 = "복근"
+        public static let postureMainTitle7 = "앞 허벅지"
+
+    }
+
+    public enum Identifier {
+        public static let postureRecommandCollectionViewCell: String = "PostureRecommandCollectionViewCell"
+        public static let postureRecommandTableViewCell: String = "PostureRecommandTableViewCell"
+        public static let posturePartTableViewCell: String = "PosturePartTableViewCell"
+        public static let postureSearchTableViewCell: String = "PostureSearchTableViewCell"
+        public static let postureDetailPickeTableViewCell: String = "PostureDetailPickeTableViewCell"
+        public static let postureDetailPickleCollectionViewCell: String = "PostureDetailPickleCollectionViewCell"
+        public static let postureDetailCautionTableViewCell: String = "PostureDetailCautionTableViewCell"
+        public static let postureDetailImageTableViewCell: String = "PostureDetailImageTableViewCell"
+        public static let postureDetailExerciseInfoTableViewCell: String = "PostureDetailExerciseInfoTableViewCell"
+        public static let postureDetailTagCollectionViewCell: String = "PostureDetailTagCollectionViewCell"
+        public static let postureDetailTagTableViewCell: String = "PostureDetailTagTableViewCell"
+        public static let postureDetailTitleTableViewCell: String = "PostureDetailTitleTableViewCell"
+    }
+}
