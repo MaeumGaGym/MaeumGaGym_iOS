a678f031427adb91095e480c0534bbbf51b31f2c
s <tylereunholee010203@gmail.com>
2024-02-27 10:06:09 +0900

    🍗 :: [#248] MG PostureInfoLabel
    유연한 코드로 변경

--- Projects/Modules/DSKit/Sources/Components/MaeumGaGymLabel /MG+PostureInfoLabel.swift
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymLabel /MG+PostureInfoLabel.swift
@@ -4,96 +4,57 @@ import Then
 import Core
 
 open class MGPostureInfoLabel: BaseLabel {
+
+    private var containerView = UIView()
+
     private var titleNumberLabel = UILabel().then {
         $0.textAlignment = .left
         $0.numberOfLines = 1
-        $0.backgroundColor = .clear
         $0.textColor = DSKitAsset.Colors.gray200.color
         $0.font = UIFont.Pretendard.titleMedium
     }
+
     private var textLabel = UILabel().then {
         $0.textAlignment = .left
-        $0.backgroundColor = .clear
         $0.textColor = .black
         $0.font = UIFont.Pretendard.bodyMedium
     }
-    
-    public init(
-        titleNumber: String,
-        text: String,
-        numberOfLines: Int? = 1
-    ) {
+
+    public init() {
         super.init(frame: .zero)
-        self.textLabel.numberOfLines = numberOfLines ?? 1
-        self.titleNumberLabel.text = titleNumber
-        self.textLabel.text = text
-        
-        addViews()
-        setupViews(lines: numberOfLines ?? 1)
     }
-    
+
     required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    private func addViews() {
-        self.addSubview(titleNumberLabel)
-        self.addSubview(textLabel)
 
-    }
-    
-    private func setupViews(lines: Int) {
-        switch lines {
-        case 1:
-            self.snp.makeConstraints {
-                $0.height.equalTo(32.0)
-            }
-            titleNumberLabel.snp.makeConstraints {
-                $0.height.equalTo(32.0)
-            }
-            textLabel.snp.makeConstraints {
-                $0.height.equalTo(32.0)
-            }
-        case 2:
-            self.snp.makeConstraints {
-                $0.height.equalTo(52.0)
-            }
-            titleNumberLabel.snp.makeConstraints {
-                $0.height.equalTo(32.0)
-            }
-            textLabel.snp.makeConstraints {
-                $0.height.equalTo(52.0)
-            }
-        default:
-            break
+    public override func layout() {
+        addSubviews([containerView])
+        containerView.addSubviews([titleNumberLabel, textLabel])
+
+        containerView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
         }
-        
+
         titleNumberLabel.snp.makeConstraints {
-            $0.width.equalTo(52.0)
             $0.top.leading.equalToSuperview()
+            $0.width.equalTo(52.0)
+            $0.height.equalTo(32.0)
         }
-        
+
         textLabel.snp.makeConstraints {
-            $0.width.equalToSuperview()
+            $0.top.bottom.trailing.equalToSuperview()
             $0.leading.equalTo(titleNumberLabel.snp.trailing)
-            $0.top.bottom.equalToSuperview()
         }
     }
-    
-    public func updateData(textNum: String,
-                           text: String,
-                           textcolor: UIColor? = .black,
-                           backgroundColor: UIColor? = .clear,
-                           font: UIFont? = UIFont.Pretendard.bodyMedium,
-                           numberOfLines: Int? = 1
-    ) {
-        self.titleNumberLabel.text = textNum
-        self.textLabel.text = text
-        self.textLabel.textColor = textcolor
-        self.backgroundColor = backgroundColor
-        self.textLabel.font = font
-        
-        addViews()
-        setupViews(lines: numberOfLines ?? 1)
+}
+
+public extension MGPostureInfoLabel {
+    func setup(index: Int, text: String) {
+        titleNumberLabel.text = "0\(index)"
+        let lineCount = text.components(separatedBy: "\n").count
+        textLabel.numberOfLines = lineCount
+        textLabel.reloadInputViews()
+        textLabel.text = text
     }
 }
