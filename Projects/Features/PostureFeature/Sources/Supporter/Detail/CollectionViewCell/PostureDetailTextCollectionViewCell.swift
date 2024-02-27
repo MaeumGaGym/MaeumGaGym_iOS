c04afadf57da950e24d58ce9934cd96c28ede278
s <tylereunholee010203@gmail.com>
2024-02-27 10:04:29 +0900

    ⚓️ :: [#248] 자세 - 디테일 - textCollectionViewCell
    추가

--- Projects/Features/PostureFeature/Sources/Supporter/Detail/CollectionViewCell/PostureDetailTextCollectionViewCell.swift
+++ Projects/Features/PostureFeature/Sources/Supporter/Detail/CollectionViewCell/PostureDetailTextCollectionViewCell.swift
@@ -1,9 +1,31 @@
-//
-//  PostureDetailTextCollectionViewCell.swift
-//  PostureFeature
-//
-//  Created by 이은호 on 2/27/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import SnapKit
+import Then
+
+import DSKit
+import Domain
+import MGNetworks
+
+public class PostureDetailTextCollectionViewCell: UICollectionViewCell {
+
+    static let identifier: String = PostureResourcesService.Identifier.postureDetailTextViewCollectionViewCell
+
+    private var detailInfoLabel = MGPostureInfoLabel()
+
+    private func layout() {
+        contentView.addSubviews([detailInfoLabel])
+
+        detailInfoLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+public extension PostureDetailTextCollectionViewCell {
+    func setup(index: Int, with model: [PostureDetailInfoTextModel]) {
+        detailInfoLabel.setup(index: index, text: model[index - 1].text)
+
+        layout()
+    }
+}
