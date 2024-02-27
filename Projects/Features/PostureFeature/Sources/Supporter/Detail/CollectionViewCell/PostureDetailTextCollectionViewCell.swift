a5205b3b6667c4c57c1f1068a68db711e4f7f480
s <tylereunholee010203@gmail.com>
2024-02-27 10:02:34 +0900

    ⚓️ :: [#248] 자세 - 디테일 - TextCollectionViewCell
    파일 추가

---
+++ Projects/Features/PostureFeature/Sources/Supporter/Detail/CollectionViewCell/PostureDetailTextCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  PostureDetailTextCollectionViewCell.swift
+//  PostureFeature
+//
+//  Created by 이은호 on 2/27/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
