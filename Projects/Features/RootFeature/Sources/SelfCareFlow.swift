8a723fb4e39a15d3b13c2e5fe895a773168e42fb
박준하 <goodjunha@gmail.com>
2024-02-28 00:43:46 +0900

    🍗:: [#255] selfCareFlow 수정

--- Projects/Features/RootFeature/Sources/SelfCareFlow.swift
+++ Projects/Features/RootFeature/Sources/SelfCareFlow.swift
@@ -5,21 +5,41 @@ import RxSwift
 import RxCocoa
 import DSKit
 
+import Core
+
 class SelfCareFlow: Flow {
     var root: Presentable {
         return self.rootViewController
     }
 
-    private let rootViewController = UINavigationController()
+    private let rootViewController = UINavigationController(
+        rootViewController: SelfCareHomeViewController(SelfCareHomeViewModel()))
 
     init() {
-        let viewController = SelfCareViewController()
+        let viewController = SelfCareHomeViewController(SelfCareHomeViewModel())
         viewController.view.backgroundColor = .white
-        viewController.tabBarItem = UITabBarItem(title: "자기관리", image: DSKitAsset.Assets.blackMuscle.image, selectedImage: DSKitAsset.Assets.blueMuscle.image)
+        viewController.tabBarItem = UITabBarItem(
+            title: "자기관리",
+            image: DSKitAsset.Assets.blackMuscleTapBar.image,
+            selectedImage: DSKitAsset.Assets.blackMuscleTapBar.image)
         rootViewController.setViewControllers([viewController], animated: false)
     }
 
     func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? AppStep else { return .none }
+
+        switch step {
+        case .myProfileRequired:
+            return navigateToProfileViewScreen()
+        default:
+            return .none
+        }
+    }
+
+    private func navigateToProfileViewScreen() -> FlowContributors {
+        let viewcontroller = SelfCareProfileViewController(ProfileEditViewModel())
+        rootViewController.pushViewController(viewcontroller, animated: true)
+//        MainTabBarContoller.shared.tabBar.isHidden = true
         return .none
     }
 }
