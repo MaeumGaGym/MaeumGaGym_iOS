655ab69502e097a03cb7194738330e39063227d9
s <tylereunholee010203@gmail.com>
2024-02-28 14:21:40 +0900

    ⚓️ :: [#261] 자기관리 - 목표 - 디테일 Model
    추가

--- Projects/Domain/Sources/Model/SelfCare/Target/SelfCareTargetDetailModel.swift
+++ Projects/Domain/Sources/Model/SelfCare/Target/SelfCareTargetDetailModel.swift
@@ -1,9 +1,19 @@
-//
-//  SelfCareTargetDetailModel.swift
-//  Domain
-//
-//  Created by 이은호 on 2/28/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+public struct SelfCareTargetDetailModel {
+    public var titleTextData: String
+    public var startDateData: String
+    public var endDateData: String
+    public var textData: String
+
+    public init(titleTextData: String,
+                startDateData: String,
+                endDateData: String,
+                textData: String
+    ) {
+        self.titleTextData = titleTextData
+        self.startDateData = startDateData
+        self.endDateData = endDateData
+        self.textData = textData
+    }
+}
