37d04ad8f6391c114762cf781a909278c3914e7b
박준하 <goodjunha@gmail.com>
2024-03-01 16:35:57 +0900

    🎮:: [#264] rxflow 연습

--- Projects/Features/ShopFeature/Sources/Coordinator/FlowSugar.swift
+++ Projects/Features/ShopFeature/Sources/Coordinator/FlowSugar.swift
@@ -1,27 +1,28 @@
 import RxFlow
+import Core
 import UIKit
 import Then
 
-typealias ViewModelStepper = BaseViewModel & Stepper
-typealias ViewModelController = ViewModelProtocol & UIViewController
+public typealias ViewModelStepper = BaseViewModel & Stepper
+public typealias ViewModelController = ViewModelProtocol & UIViewController
 
-class FlowSugar<VM, VC>: NSObject where VM: ViewModelStepper, VC: ViewModelController {
+public class FlowSugar<VM, VC>: NSObject where VM: ViewModelStepper, VC: ViewModelController {
     var vm: VM!
     var vc: VC?
 
-    override init() {
+    public override init() {
         fatalError("init() has not been supported")
     }
 
-    init(viewModel vm: VM) {
+    public init(viewModel vm: VM) {
         self.vm = vm
         super.init()
     }
 
-    init(_ flow: Flow, _ step: Step) {
+    public init(_ flow: Flow, _ step: Step) {
     }
 
-    init(_ vm: VM, _ vc: VC.Type) {
+    public init(_ vm: VM, _ vc: VC.Type) {
         self.vm = vm
         self.vc = VC().then {
             $0.viewModel = vm as? VC.ViewModelType
@@ -29,27 +30,27 @@ class FlowSugar<VM, VC>: NSObject where VM: ViewModelStepper, VC: ViewModelContr
         super.init()
     }
 
-    func presentable(_ vc: VC.Type) -> Self {
+    public func presentable(_ vc: VC.Type) -> Self {
         self.vc = VC().then {
             $0.viewModel = vm as? VC.ViewModelType
         }
         return self
     }
     
-    func presentableForStoryBoard(_ vc: VC) -> Self {
+    public func presentableForStoryBoard(_ vc: VC) -> Self {
         self.vc = vc
         vc.viewModel = vm as? VC.ViewModelType
         return self
     }
 
-    func navigationItem(with block: @escaping (UIViewController) -> Void) -> Self {
+    public func navigationItem(with block: @escaping (UIViewController) -> Void) -> Self {
         if let vc = self.vc {
             block(vc)
         }
         return self
     }
 
-    func getViewController() -> VC? {
+    public func getViewController() -> VC? {
         if let vc = self.vc {
             return vc
         } else {
@@ -57,21 +58,21 @@ class FlowSugar<VM, VC>: NSObject where VM: ViewModelStepper, VC: ViewModelContr
         }
     }
 
-    func setVCProperty(viewControllerBlock block: @escaping (VC) -> Void) -> Self {
+    public func setVCProperty(viewControllerBlock block: @escaping (VC) -> Void) -> Self {
         if let vc = self.vc {
             block(vc)
         }
         return self
     }
 
-    func justRegiste() -> FlowContributor? {
+    public func justRegiste() -> FlowContributor? {
         if let vc = self.vc {
             return .contribute(withNextPresentable: vc, withNextStepper: vm)
         }
         return nil
     }
 
-    func oneStepPushBy(_ navi: UINavigationController, isHideBottombar: Bool = false, includeOpaqueBar: Bool = false, animation: Bool = true) -> FlowContributors {
+    public func oneStepPushBy(_ navi: UINavigationController, isHideBottombar: Bool = false, includeOpaqueBar: Bool = false, animation: Bool = true) -> FlowContributors {
         if let vc = self.vc {
             vc.hidesBottomBarWhenPushed = isHideBottombar
             vc.extendedLayoutIncludesOpaqueBars = includeOpaqueBar
@@ -81,7 +82,7 @@ class FlowSugar<VM, VC>: NSObject where VM: ViewModelStepper, VC: ViewModelContr
         return .none
     }
     
-    func oneStepModalPresent(_ parentVC: UIViewController, _ modalStyle: UIModalPresentationStyle = .fullScreen, _ animated: Bool = true) -> FlowContributors {
+    public func oneStepModalPresent(_ parentVC: UIViewController, _ modalStyle: UIModalPresentationStyle = .fullScreen, _ animated: Bool = true) -> FlowContributors {
         if let vc = self.vc {
             vc.modalPresentationStyle = modalStyle
             parentVC.present(vc, animated: animated)
@@ -90,7 +91,7 @@ class FlowSugar<VM, VC>: NSObject where VM: ViewModelStepper, VC: ViewModelContr
         return .none
     }
     
-    func oneStepPopoverPresent(_ parentVC: UIViewController, _ modalStyle: UIModalPresentationStyle = .popover, _ animated: Bool = true) -> FlowContributors {
+    public func oneStepPopoverPresent(_ parentVC: UIViewController, _ modalStyle: UIModalPresentationStyle = .popover, _ animated: Bool = true) -> FlowContributors {
         if let vc = self.vc {
             vc.modalPresentationStyle = modalStyle
             parentVC.present(vc, animated: animated)
@@ -99,7 +100,7 @@ class FlowSugar<VM, VC>: NSObject where VM: ViewModelStepper, VC: ViewModelContr
         return .none
     }
 
-    func oneStepModalPresentMakeNavi(_ parentVC: UIViewController, _ modalStyle: UIModalPresentationStyle = .fullScreen, _ animated: Bool = true) -> FlowContributors {
+    public func oneStepModalPresentMakeNavi(_ parentVC: UIViewController, _ modalStyle: UIModalPresentationStyle = .fullScreen, _ animated: Bool = true) -> FlowContributors {
         if let vc = self.vc {
             let navigationController = UINavigationController(rootViewController: vc)
             navigationController.setNavigationBarHidden(true, animated: true)
