a973d40c66b62f7b012db8c96c6f0d636443312a
s <tylereunholee010203@gmail.com>
2024-03-17 21:26:35 +0900

    ⚓️ :: [#IOS-37] repositoryInterface 추가

--- Projects/Domain/Sources/RepositoryInterface/AuthRepositoryInterface.swift
+++ Projects/Domain/Sources/RepositoryInterface/AuthRepositoryInterface.swift
@@ -8,4 +8,5 @@ public protocol AuthRepositoryInterface {
     func getCSRFToken() -> Single<String>
     func getIntroData() -> Single<IntroModel>
     func appleSignup() -> Single<String>
+    func appleSingup(nickname: String, accessToken: String) -> Single<String>
 }
