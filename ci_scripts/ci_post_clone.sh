7674bb4bbddd76e6819df985cadd155f79966796
HyunSu1768 <azxcv1768@gmail.com>
2024-03-20 08:21:43 +0900

    FIX :: 스크립트 수정

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,13 +3,15 @@
 GITHUB_TOKEN=${GITHUB_TOKEN}
 
 git clone https://$GITHUB_TOKEN@github.com/MaeumGaGym/MaeumagGym-iOS-ignoreds.git
-
+pwd
 cp -R MaeumagGym-iOS-ignoreds/xcconfigs ../
+rm -rf MaeumagGym-iOS-ignoreds
 
 cd ..
 
-curl -Ls https://install.tuist.io | bash
+curl https://mise.jdx.dev/install.sh | sh
+mise install
 
-tuist fetch
+mise x tuist fetch
 
-tuist generate
+mise x tuist generate
