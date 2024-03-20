215aaf5aec2ff806ecb13d6bd7eae51fee0b1864
HyunSu1768 <azxcv1768@gmail.com>
2024-03-20 12:28:14 +0900

    FIX :: moya 버전 수정

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -9,7 +9,7 @@ let dependencies = Dependencies(
         .remote(url: "https://github.com/ReactiveX/RxSwift.git", requirement: .upToNextMinor(from: "6.5.0")),
         .remote(url: "https://github.com/RxSwiftCommunity/RxFlow.git", requirement: .upToNextMajor(from: "2.13.0")),
         .remote(url: "https://github.com/Swinject/Swinject.git", requirement: .upToNextMajor(from: "2.8.3")),
-        .remote(url: "https://github.com/Moya/Moya.git", requirement: .upToNextMajor(from: "15.0.0")),
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0")),
         .remote(url: "https://github.com/airbnb/lottie-ios", requirement: .upToNextMajor(from: "4.3.3")),
         .remote(url: "https://github.com/onevcat/Kingfisher", requirement: .upToNextMajor(from: "7.0.0")),
         .remote(url: "https://github.com/MaeumgaGym/MindGymKit", requirement: .upToNextMajor(from: "0.2.4")),
