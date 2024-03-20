5f425d81c2365d8f5abda659e6c0cbcc9a5cb969
HyunSu1768 <azxcv1768@gmail.com>
2024-03-20 14:56:20 +0900

    FIX :: targetVersion 수정

--- Plugins/EnvPlugin/ProjectDescriptionHelpers/Environment.swift
+++ Plugins/EnvPlugin/ProjectDescriptionHelpers/Environment.swift
@@ -7,7 +7,7 @@ public enum Environment {
 public extension Project {
     enum Environment {
         public static let workspaceName = "MaeumGaGym-iOS"
-        public static let deploymentTarget = DeploymentTarget.iOS(targetVersion: "12.0", devices: .iphone)
+        public static let deploymentTarget = DeploymentTarget.iOS(targetVersion: "11.0", devices: .iphone)
         public static let platform = Platform.iOS
         public static let bundlePrefix = "com.maeumGaGym-health-iOS"
         public static let baseSetting: SettingsDictionary = SettingsDictionary()
