374dfea4d6ce3045e2e44ac5b9904a96a2823cbf
s <tylereunholee010203@gmail.com>
2024-03-23 10:16:16 +0900

    FIX :: [IOS-47] Aut Repository
    DTO 적용

--- Projects/Data/Sources/Repository/AuthRepository.swift
+++ Projects/Data/Sources/Repository/AuthRepository.swift
@@ -1,4 +1,5 @@
 import UIKit
+import MGNetworks
 
 import RxSwift
 import RxCocoa
@@ -7,32 +8,48 @@ import Moya
 import RxMoya
 
 import Domain
-import MGNetworks
 
 public class AuthRepository: AuthRepositoryInterface {
+
     private let networkService: AuthService
+
+    public func googleToken() -> Single<Bool> {
+        return networkService.kakaoTokenState()
+    }
+
+    public func kakaoToken() -> Single<Bool> {
+        return networkService.kakaoTokenState()
+    }
+
+    public func appleToken() -> Single<Bool> {
+        return networkService.kakaoTokenState()
+    }
+
+    public func oauthSignup(nickname: String, accessToken: String, oauth: OauthType) -> Observable<SignupResponseDTO> {
+        return networkService.oauthSingup(nickname: nickname, accessToken: accessToken, oauth: oauth)
+    }
+
+    public func oauthLogin(accessToken: String, oauth: OauthType) -> Observable<LoginResponseDTO> {
+        return networkService.oauthLogin(accessToken: accessToken, oauth: oauth)
+    }
+    
+    public func oauthRecovery(accessToken: String, oauth: OauthType) -> Observable<RecoveryResponseDTO> {
+        return networkService.oauthRecovery(accessToken: accessToken, oauth: oauth)
+    }
+
+    public func getIntroData() -> Single<IntroModel>  {
+        return networkService.requestIntroData()
+    }
+
+    public func appleButtonTap() -> Single<String> {
+        return networkService.appleButtonTap()
+    }
 
     public init(networkService: AuthService) {
         self.networkService = networkService
     }
 
-    public func kakaoToken() -> Single<Bool> {
-        networkService.kakaoTokenState()
-    }
-    
-    public func getCSRFToken() -> Single<String> {
-        return networkService.getCSRFToken()
-    }
-    
-    public func getIntroData() -> Single<IntroModel>  {
-        return networkService.requestIntroData()
-    }
-
-    public func appleSignup() -> RxSwift.Single<String> {
-        return networkService.appleSignup()
-    }
-
-    public func appleSingup(nickname: String, accessToken: String) -> Single<String> {
-        return networkService.appleSignup(nickname: nickname, accessToken: accessToken)
-    }
+//    public func appleSingup(nickname: String, accessToken: String) -> Single<String> {
+//        return networkService.appleSignup(nickname: nickname, accessToken: accessToken)
+//    }
 }
