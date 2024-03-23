b6778ef801c5c7d08b5cf9203eafa5dfd668581b
s <tylereunholee010203@gmail.com>
2024-03-23 10:15:49 +0900

    ADD :: [IOS-47] Auth Repository Interface
    DTO 반환 값 추가

--- Projects/Domain/Sources/RepositoryInterface/AuthRepositoryInterface.swift
+++ Projects/Domain/Sources/RepositoryInterface/AuthRepositoryInterface.swift
@@ -3,10 +3,33 @@ import UIKit
 import RxSwift
 import RxCocoa
 
+public enum OauthType {
+    case google
+    case kakao
+    case apple
+}
+
+public struct LoginResponseDTO: Decodable {
+    public let status: Int
+    public let accessToken: String
+    public let refreshToken: String
+}
+
+public struct SignupResponseDTO: Decodable {
+    public let status: Int
+}
+
+public struct RecoveryResponseDTO: Decodable {
+    public let status: Int
+}
+
 public protocol AuthRepositoryInterface {
+    func googleToken() -> Single<Bool>
     func kakaoToken() -> Single<Bool>
-    func getCSRFToken() -> Single<String>
+    func appleToken() -> Single<Bool>
+    func appleButtonTap() -> Single<String>
+    func oauthSignup(nickname: String, accessToken: String, oauth: OauthType) -> Observable<SignupResponseDTO>
+    func oauthLogin(accessToken: String, oauth: OauthType) -> Observable<LoginResponseDTO>
+    func oauthRecovery(accessToken: String, oauth: OauthType) -> Observable<RecoveryResponseDTO>
     func getIntroData() -> Single<IntroModel>
-    func appleSignup() -> Single<String>
-    func appleSingup(nickname: String, accessToken: String) -> Single<String>
 }
