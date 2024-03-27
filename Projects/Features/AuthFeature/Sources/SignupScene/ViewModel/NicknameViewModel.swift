104a30dd8cdabb294b3645cf67a6b1786c5082c4
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-03-27 16:34:18 +0900

    FIX :: [IOS-47] Nickname ViewModel

--- Projects/Features/AuthFeature/Sources/SignupScene/ViewModel/NicknameViewModel.swift
+++ Projects/Features/AuthFeature/Sources/SignupScene/ViewModel/NicknameViewModel.swift
@@ -1,53 +1,42 @@
-//import Foundation
-//
-//import RxFlow
-//import RxCocoa
-//import RxSwift
-//
-//import Core
-//import Domain
-//import MGNetworks
-//
-//public class NicknameViewModel: BaseViewModel {
-//
-//    public typealias ViewModel = NicknameViewModel
-//
-//    private let useCase: AuthUseCase
-//
-//    public var disposeBag: DisposeBag = DisposeBag()
-//
-//    public struct Input {
-//        let navButtonTapped: Driver<Void>
-//        let nextButtonTap: Driver<Void>
-//    }
-//
-//    public struct Output {
-//        let navButtonTapped: Driver<Void>
-//        let nextButtonClicked: Driver<Bool>
-//    }
-//
-//    public init(useCase: AuthUseCase) {
-//        self.useCase = useCase
-//    }
-//
-//    public func transform(_ input: Input, action: (Output) -> Void) -> Output {
-//
-////        let nextButtonClicked = input.nextButtonTap.map { true }.asDriver(onErrorJustReturn: false)
-//
-//        let output = Output(navButtonTapped: input.navButtonTapped.asDriver(), nextButtonClicked: input.nextButtonTap.asDriver())
-//
-//        action(output)
-//        
-//        input.nextButtonTap
-//            .drive(onNext: { _ in
-//                useCase.appleNickNameButtonTap()
-//            }).disposed(by: disposeBag)
-//
-//        input.navButtonTapped
-//            .drive(onNext: { _ in
-//                AuthStepper.shared.steps.accept(MGStep.authBack)
-//            }).disposed(by: disposeBag)
-//
-//        return output
-//    }
-//}
+import Foundation
+
+import RxFlow
+import RxCocoa
+import RxSwift
+
+import Core
+import Domain
+import MGLogger
+
+import MGNetworks
+
+public class NicknameViewModel: BaseViewModel {
+
+    public typealias ViewModel = NicknameViewModel
+
+    private let useCase: AuthUseCase
+
+    public var disposeBag: DisposeBag = DisposeBag()
+
+    public struct Input {
+        let navButtonTap: Driver<Void>
+        let nextButtonTap: Driver<Void>
+    }
+
+    public struct Output {
+        let navButtonTap: Driver<Void>
+        let nextButtonTap: Driver<Void>
+    }
+
+    public init(useCase: AuthUseCase) {
+        self.useCase = useCase
+    }
+
+    public func transform(_ input: Input, action: (Output) -> Void) -> Output {
+
+        let output = Output(navButtonTap: input.navButtonTap.asDriver(), nextButtonTap: input.nextButtonTap.asDriver())
+
+        action(output)
+        return output
+    }
+}
