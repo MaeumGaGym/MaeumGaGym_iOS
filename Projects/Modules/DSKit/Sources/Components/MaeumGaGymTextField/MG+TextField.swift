a2b1b4aedb133010724e01ea1428e4a9b66be4ab
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-03-27 16:29:24 +0900

    FIX :: [IOS-47] rxcocoa 적용

--- Projects/Modules/DSKit/Sources/Components/MaeumGaGymTextField/MG+TextField.swift
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymTextField/MG+TextField.swift
@@ -1,6 +1,8 @@
 import UIKit
-import Then
+
 import SnapKit
+import Then
+
 import Core
 import RxSwift
 import RxCocoa
@@ -37,7 +39,7 @@ open class MGTextField: UITextField {
             errorLabel.text = errorMessage
         }
     }
-  
+
     public var nameErrorType: TextFieldErrorType.Name? {
         didSet {
             if let errorType = nameErrorType {
@@ -46,17 +48,17 @@ open class MGTextField: UITextField {
             }
         }
     }
-    
+
     public override var placeholder: String? {
         didSet {
             placeholderLabel.text = placeholder
             super.placeholder = ""
         }
     }
-    
+
     public convenience init(placeholder: String) {
         self.init(frame: .zero)
-        
+
         placeholderLabel.text = placeholder
     }
 
@@ -64,25 +66,24 @@ open class MGTextField: UITextField {
         super.init(frame: frame)
         setupUI()
     }
-    
+
     required public init?(coder: NSCoder) {
         super.init(coder: coder)
         setupUI()
     }
-    
+
     private func setupUI() {
         configure()
-
-        delegate = self
+        bind()
 
         self.tintColor = .black
     }
-    
+
     private func configure() {
-        addSubview(placeholderLabel)
-        addSubview(underlineView)
-        addSubview(errorLabel)
-        
+        addSubviews([placeholderLabel,
+                     underlineView,
+                     errorLabel])
+
         placeholderLabel.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview()
             $0.bottom.equalTo(underlineView.snp.top).offset(-8.0)
@@ -95,45 +96,40 @@ open class MGTextField: UITextField {
             $0.bottom.equalToSuperview()
             $0.height.equalTo(1)
         }
-        
+
         errorLabel.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview()
             $0.top.equalTo(underlineView.snp.bottom).offset(8.0)
         }
-        
     }
-}
 
-extension MGTextField: UITextFieldDelegate {
-    public func textFieldDidBeginEditing(_ textField: UITextField) {
-        UIView.animate(withDuration: 0.3) {
-            
-            self.underlineView.backgroundColor = DSKitAsset.Colors.blue500.color
-            self.placeholderLabel.font = UIFont.Pretendard.bodySmall
-            self.placeholderLabel.snp.updateConstraints {
-                $0.bottom.equalTo(self.underlineView.snp.top).offset(-(self.placeholderLabel.frame.size.height + 4))
-            }
-            self.layoutIfNeeded()
-        }
-    }
-    
-    public func textFieldDidEndEditing(_ textField: UITextField) {
-        if textField.text == "" {
-            UIView.animate(withDuration: 0.3) {
-                self.underlineView.backgroundColor = DSKitAsset.Colors.gray400.color
-                self.placeholderLabel.font = UIFont.Pretendard.bodyLarge
-                self.placeholderLabel.snp.updateConstraints {
-                    $0.bottom.equalTo(self.underlineView.snp.top).offset(-4)
+    private func bind() {
+        self.rx.controlEvent(.editingDidBegin)
+            .asObservable()
+            .subscribe(onNext: { [weak self] _ in
+                UIView.animate(withDuration: 0.3) {
+                    self?.underlineView.backgroundColor = DSKitAsset.Colors.blue500.color
+                    self?.placeholderLabel.font = UIFont.Pretendard.bodySmall
+                    self?.placeholderLabel.snp.updateConstraints {
+                        $0.bottom.equalTo(self!.underlineView.snp.top).offset(-(self!.placeholderLabel.frame.size.height + 4))
+                    }
+                    self?.layoutIfNeeded()
                 }
-                self.layoutIfNeeded()
-            }
-        }
-    }
-    
-    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        if let touch = touches.first {
-            _ = touch.location(in: self)
-        }
+            }).disposed(by: disposeBag)
+
+        self.rx.controlEvent(.editingDidEnd)
+            .asObservable()
+            .subscribe(onNext: { [weak self] _ in
+                if self?.text == "" {
+                    UIView.animate(withDuration: 0.3) {
+                        self?.underlineView.backgroundColor = DSKitAsset.Colors.gray400.color
+                        self?.placeholderLabel.font = UIFont.Pretendard.bodyLarge
+                        self?.placeholderLabel.snp.updateConstraints {
+                            $0.bottom.equalTo(self!.underlineView.snp.top).offset(-4)
+                        }
+                        self?.layoutIfNeeded()
+                    }
+                }
+            }).disposed(by: disposeBag)
     }
 }
