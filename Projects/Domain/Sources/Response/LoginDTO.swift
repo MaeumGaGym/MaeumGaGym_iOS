79c2b0a3e7f5e9ec9bffcd4775771fc563575094
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-04-16 08:01:41 +0900

    ADD :: LoginDTO 추가

--- Projects/Domain/Sources/Response/LoginDTO.swift
+++ Projects/Domain/Sources/Response/LoginDTO.swift
@@ -1,20 +1,59 @@
 import Foundation
 
-public struct LoginResponseDTO: Decodable {
+public struct SignupRequestDTO: Codable {
+    public let oauthToken: String
+    public let nickname: String
+}
+
+public struct LoginRequestDTO: Codable {
+    public let oauthToken: String
+}
+
+public struct RecoveryRequestDTO: Codable {
+    public let oauthToken: String
+}
+
+public struct SignupResponseDTO: Codable {
+    public let status: Int
+}
+
+public struct LoginResponseDTO: Codable {
     public let status: Int
     public let accessToken: String
     public let refreshToken: String
 }
 
-public struct SignupResponseDTO: Decodable {
+public struct RecoveryResponseDTO: Codable {
     public let status: Int
 }
 
-public struct RecoveryResponseDTO: Decodable {
+public struct DeleteRequestDTO: Codable {
+    public let accessToken: String
+}
+
+public struct DeleteResponseDTO: Codable {
     public let status: Int
 }
 
-enum AuthErrorType: Error {
+public struct tokenRefreshRequestDTO: Codable {
+    public let refreshToken: String
+}
+
+public struct tokenRefreshResponseDTO: Codable {
+    public let status: Int
+    public let accessToken: String
+    public let refreshToken: String
+}
+
+public struct CheckNicknameRequestDTO: Codable {
+    public let nickname: String
+}
+
+public struct CheckNicknameResponseDTO: Codable {
+    public let status: Int
+}
+
+public enum AuthErrorType: Error {
     case notFound400
     case notInt
 }
