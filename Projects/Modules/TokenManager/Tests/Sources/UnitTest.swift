9634f0e9181e34f5f89b2710e3700c88705902d4
박준하 <goodjunha@gmail.com>
2024-04-20 13:40:38 +0900

    Fix:: [#299] 옵셔널 테스트 성공

--- Projects/Modules/TokenManager/Tests/Sources/UnitTest.swift
+++ Projects/Modules/TokenManager/Tests/Sources/UnitTest.swift
@@ -3,7 +3,7 @@ import TokenManager
 
 class TokenManagerTests: XCTestCase {
     
-    var tokenManager: TokenManagerImpl!
+    var tokenManager: TokenManagerImpl?
     
     override func setUp() {
         super.setUp()
@@ -21,19 +21,19 @@ class TokenManagerTests: XCTestCase {
         let testKey = KeychainType.test
         
         // 토큰 저장 테스트
-        _ = tokenManager.save(token: testToken, with: testKey)
+        _ = tokenManager?.save(token: testToken, with: testKey)
         
         // 토큰 가져오기 테스트
-        _ = tokenManager.get(key: testKey)
+        _ = tokenManager?.get(key: testKey)
         
         // 토큰 업데이트 테스트
         let newToken = "newTestToken"
-        _ = tokenManager.update(token: newToken, with: testKey)
-        _ = tokenManager.get(key: testKey)
+        _ = tokenManager?.update(token: newToken, with: testKey)
+        _ = tokenManager?.get(key: testKey)
         
         // 토큰 삭제 테스트
-        _ = tokenManager.delete(key: testKey)
-        let deletedToken = tokenManager.get(key: testKey)
+        _ = tokenManager?.delete(key: testKey)
+        let deletedToken = tokenManager?.get(key: testKey)
         XCTAssertNil(deletedToken, "Token should be nil after deletion.")
     }
 }
