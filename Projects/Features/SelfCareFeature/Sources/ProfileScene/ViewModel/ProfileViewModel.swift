49b74b84f1f40b444025f33fedbe52452e54b634
Choyoungjun <cyj070513@gmail.com>
2024-05-05 01:28:08 +0900

    ADD :: [#312] 자기관리-프로필 뷰모델 추가

---
+++ Projects/Features/SelfCareFeature/Sources/ProfileScene/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+import RxFlow
+import RxCocoa
+import RxSwift
+
+import Core
+
+public class SelfCareProfileViewModel: BaseViewModel {
+
+    public typealias ViewModel = SelfCareProfileViewModel
+
+    public func transform(_ input: Input, action: (Output) -> Void) -> Output {
+        return Output()
+    }
+
+    public struct Input {
+
+    }
+
+    public struct Output {
+
+    }
+
+    public init() {
+
+    }
+}
