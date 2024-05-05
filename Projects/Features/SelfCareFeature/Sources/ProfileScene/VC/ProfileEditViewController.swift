51a3f130d63641fd8457250207659ba30323500b
Choyoungjun <cyj070513@gmail.com>
2024-05-05 17:34:03 +0900

    ADD :: [#312] 프로필 수정 VC 생성

---
+++ Projects/Features/SelfCareFeature/Sources/ProfileScene/VC/ProfileEditViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+import Then
+import SnapKit
+
+import Core
+import DSKit
+
+class SelfCareProfileEditViewController: BaseViewController<SelfCareProfileEditViewModel> {
+
+    override func attribute() {
+        view.backgroundColor = .red
+    }
+
+}
