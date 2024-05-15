09a2bc7c8e029ab78a222a183bc96b33815b52f1
Choyoungjun <cyj070513@gmail.com>
2024-05-16 08:54:36 +0900

    ADD :: [#327] UIColor Extension 추가

---
+++ Projects/Modules/DSKit/Sources/Extension/Color/UIColor+.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+public extension UIColor {
+    
+    //MARK: blue
+    static let blue50 = DSKitAsset.Colors.blue50.color
+    static let blue100 = DSKitAsset.Colors.blue100.color
+    static let blue200 = DSKitAsset.Colors.blue200.color
+    static let blue300 = DSKitAsset.Colors.blue300.color
+    static let blue400 = DSKitAsset.Colors.blue400.color
+    static let blue500 = DSKitAsset.Colors.blue500.color
+    static let blue600 = DSKitAsset.Colors.blue600.color
+    static let blue700 = DSKitAsset.Colors.blue700.color
+    static let blue800 = DSKitAsset.Colors.blue800.color
+    static let blue900 = DSKitAsset.Colors.blue900.color
+
+    //MARK: gray
+    static let gray25 = DSKitAsset.Colors.gray25.color
+    static let gray50 = DSKitAsset.Colors.gray50.color
+    static let gray100 = DSKitAsset.Colors.gray100.color
+    static let gray200 = DSKitAsset.Colors.gray200.color
+    static let gray300 = DSKitAsset.Colors.gray300.color
+    static let gray400 = DSKitAsset.Colors.gray400.color
+    static let gray500 = DSKitAsset.Colors.gray500.color
+    static let gray600 = DSKitAsset.Colors.gray600.color
+    static let gray700 = DSKitAsset.Colors.gray700.color
+    static let gray800 = DSKitAsset.Colors.gray800.color
+    static let gray900 = DSKitAsset.Colors.gray900.color
+    
+    //MARK: red
+    static let red50 = DSKitAsset.Colors.red50.color
+    static let red100 = DSKitAsset.Colors.red100.color
+    static let red200 = DSKitAsset.Colors.red200.color
+    static let red300 = DSKitAsset.Colors.red300.color
+    static let red400 = DSKitAsset.Colors.red400.color
+    static let red500 = DSKitAsset.Colors.red500.color
+    static let red600 = DSKitAsset.Colors.red600.color
+    static let red700 = DSKitAsset.Colors.red700.color
+    static let red800 = DSKitAsset.Colors.red800.color
+    static let red900 = DSKitAsset.Colors.red900.color
+    
+    //MARK: yellow
+    static let yellow50 = DSKitAsset.Colors.yellow50.color
+    static let yellow100 = DSKitAsset.Colors.yellow100.color
+    static let yellow200 = DSKitAsset.Colors.yellow200.color
+    static let yellow300 = DSKitAsset.Colors.yellow300.color
+    static let yellow400 = DSKitAsset.Colors.yellow400.color
+    static let yellow500 = DSKitAsset.Colors.yellow500.color
+    static let yellow600 = DSKitAsset.Colors.yellow600.color
+    static let yellow700 = DSKitAsset.Colors.yellow700.color
+    static let yellow800 = DSKitAsset.Colors.yellow800.color
+    static let yellow900 = DSKitAsset.Colors.yellow900.color
+    
+}
