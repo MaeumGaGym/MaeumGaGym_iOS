c9ba0cec82e1d28376ab3b38396f63e40d2b1de9
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-05-30 23:36:30 +0900

    ADD :: [#334] 자세 - 디테일 파일 추가

---
+++ Projects/Features/PostureFeature/Sources/Supporter/Detail/PostureDetailExplanView.swift
@@ -0,0 +1,9 @@
+//
+//  PostureDetailExplanView.swift
+//  PostureFeatureInterface
+//
+//  Created by 이은호 on 5/27/24.
+//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
+//
+
+import Foundation
