3a367858307a8b85d8279f2a76beca15abc984d3
은호 <127753071+Eunho0922@users.noreply.github.com>
2024-05-30 23:38:35 +0900

    ADD :: [#334] 자세 - 디테일 설명 View

--- Projects/Features/PostureFeature/Sources/Supporter/Detail/PostureDetailExplanView.swift
+++ Projects/Features/PostureFeature/Sources/Supporter/Detail/PostureDetailExplanView.swift
@@ -1,9 +1,30 @@
-//
-//  PostureDetailExplanView.swift
-//  PostureFeatureInterface
-//
-//  Created by 이은호 on 5/27/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import SnapKit
+import Then
+
+import DSKit
+import Core
+
+public class PostureDetailExplanView: BaseView {
+
+    private var titleLabel = MGLabel(
+        font: UIFont.Pretendard.titleMedium, 
+        isCenter: false
+    )
+    
+    private var contentsLabel = MGLabel(
+        font: UIFont.Pretendard.bodyMedium,
+        isCenter: false
+    )
+    
+    public override func layout() {
+        addSubviews([titleLabel])
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(12)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(32)
+        }
+    }
+}
