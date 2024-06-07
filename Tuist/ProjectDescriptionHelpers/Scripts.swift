448213463ad39a06ebffec98d3160cda0d890c42
Choyoungjun <cyj070513@gmail.com>
2024-06-08 04:42:19 +0900

    BURN :: [#336] Lint 주석 처리

--- Tuist/ProjectDescriptionHelpers/Scripts.swift
+++ Tuist/ProjectDescriptionHelpers/Scripts.swift
@@ -1,21 +1,21 @@
-import ProjectDescription
-
-public extension TargetScript {
-    static let swiftLintScript = TargetScript.pre(
-        script: """
-                if test -d "/opt/homebrew/bin/"; then
-                    PATH="/opt/homebrew/bin/:${PATH}"
-                fi
-
-                export PATH
-
-                if which swiftlint > /dev/null; then
-                    swiftlint lint && swiftlint
-                else
-                    echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-                fi
-                """,
-        name: "SwiftLint",
-        basedOnDependencyAnalysis: false
-    )
-}
+//import ProjectDescription
+//
+//public extension TargetScript {
+//    static let swiftLintScript = TargetScript.pre(
+//        script: """
+//                if test -d "/opt/homebrew/bin/"; then
+//                    PATH="/opt/homebrew/bin/:${PATH}"
+//                fi
+//
+//                export PATH
+//
+//                if which swiftlint > /dev/null; then
+//                    swiftlint lint && swiftlint
+//                else
+//                    echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+//                fi
+//                """,
+//        name: "SwiftLint",
+//        basedOnDependencyAnalysis: false
+//    )
+//}
