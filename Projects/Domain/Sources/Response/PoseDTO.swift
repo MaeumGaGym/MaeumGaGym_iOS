8a01257ee0c3e944bd4814d72f4b2b266c7c9905
s <tylereunholee010203@gmail.com>
2024-06-08 10:47:28 +0900

    ADD :: [#340] PoseDTO
    포즈 전체 조회 DTO 추가

--- Projects/Domain/Sources/Response/PoseDTO.swift
+++ Projects/Domain/Sources/Response/PoseDTO.swift
@@ -1,9 +1,34 @@
-//
-//  PoseDTO.swift
-//  Domain
-//
-//  Created by 이은호 on 6/8/24.
-//  Copyright © 2024 MaeumGaGym-iOS. All rights reserved.
-//
-
 import Foundation
+
+struct PostureAllDTO: Decodable {
+    let responses: [PostureAllResponse]
+}
+
+struct PostureAllResponse: Decodable {
+    let id: Int
+    let category: [String]
+    let needMachine: Bool
+    let name: String
+    let simplePart, exactPart: [String]
+    let thumbnail: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, category
+        case needMachine = "need_machine"
+        case name
+        case simplePart = "simple_part"
+        case exactPart = "exact_part"
+        case thumbnail
+    }
+}
+
+extension PostureAllDTO {
+//    func toDomain() -> SelfCareDetailProfileModel {
+//        return .init(
+//            userImage: profileImage,
+//            userName: nickName,
+//            userWakaTime: userWakaTime,
+//            userBageLevel: userBageLevel
+//        )
+//    }
+}
