2f7d03d5eff3d9057c34dcfb59042d55e3fa3a01
Choyoungjun <cyj070513@gmail.com>
2024-06-11 20:25:31 +0900

    ADD :: [#336] UseCase 추가

--- Projects/Domain/Sources/UseCase/SelfCareUseCase.swift
+++ Projects/Domain/Sources/UseCase/SelfCareUseCase.swift
@@ -21,16 +21,18 @@ public protocol SelfCareUseCase {
 
     //MARK: Target
     var myTargetData: PublishSubject<SelfCareTargetMainModel> { get }
-    var targetDetailData: PublishSubject<SelfCareTargetDetailModel> { get }
+    var targetDetailData: PublishSubject<TargetContentModel> { get }
 
-    func getMyTargetData()
-//    func getTargetDetailData()
-    func addTargetData(title: String, content: String, startDate: String, endDate: String)
+    func getTargetDetailData(id: Int)
+    func getMyTargetData(page: Int)
+    func getMonthTargetData(date: String)
+    func addTargetData(title: String, content: String, startDate: Date, endDate: Date)
     func modifyTargetData(title: String, content: String, startDate: String, endDate: String, id: Int)
     func deleteTargetData(id: Int)
     
     //MARK: Profile
     var profileData: PublishSubject<SelfCareDetailProfileModel> { get }
+    var nickNameData: PublishSubject<SelfCareModifyProfileModel> { get }
     
     func getProfileData(nickName: String)
     func requestProfileModify(nickName: String, height: Double, weight: Double, gender: String)
@@ -44,10 +46,12 @@ public class DefaultSelfCareUseCase {
     public let myRoutineDetailData = PublishSubject<SelfCareMyRoutineDetailModel>()
     public let myRoutineEditData = PublishSubject<SelfCareMyRoutineEditModel>()
 
+    public let monthTargetData = PublishSubject<SelfCareTargetMainModel>()
     public let myTargetData = PublishSubject<SelfCareTargetMainModel>()
-    public let targetDetailData = PublishSubject<SelfCareTargetDetailModel>()
+    public let targetDetailData = PublishSubject<TargetContentModel>()
     
     public let profileData = PublishSubject<SelfCareDetailProfileModel>()
+    public let nickNameData = PublishSubject<SelfCareModifyProfileModel>()
 
     public init(repository: SelfCareRepositoryInterface) {
         self.repository = repository
@@ -86,30 +90,48 @@ extension DefaultSelfCareUseCase: SelfCareUseCase {
             }).disposed(by: disposeBag)
     }
 
-    public func getMyTargetData() {
+    public func getTargetDetailData(id: Int) {
         guard let token = TokenManagerImpl().get(key: .accessToken) else {
             return
         }
-        repository.getMyTarget(accessToken: token)
-            .subscribe(onSuccess: { [weak self] targetMainData in
-                self?.myTargetData.onNext(targetMainData)
-            },
-                       onFailure: { error in
+        repository.getTargetDetailData(accessToken: token, id: id)
+            .subscribe(onSuccess: { [weak self] targetDetailData in
+                self?.targetDetailData.onNext(targetDetailData)
+            }, onFailure: { error in
+                print("SelfCareUseCase getDetailTargetData error occured: \(error)")
+            }).disposed(by: disposeBag)
+    }
+    
+    public func getMyTargetData(page: Int) {
+        guard let token = TokenManagerImpl().get(key: .accessToken) else {
+            return
+        }
+        repository.getMyTarget(accessToken: token, page: page)
+            .subscribe(onSuccess: { [weak self] myTargetData in
+                self?.myTargetData.onNext(myTargetData)
+            }, onFailure: { error in
                 print("SelfCareUseCase getMyTargetData error occured: \(error)")
             }).disposed(by: disposeBag)
     }
     
-//    public func getTargetDetailData() {
-//        <#code#>
-//    }
-    public func addTargetData(title: String, content: String, startDate: String, endDate: String) {
+    public func getMonthTargetData(date: String) {
         guard let token = TokenManagerImpl().get(key: .accessToken) else {
             return
         }
-        
+        repository.getMonthTarget(accessToken: token, date: date)
+            .subscribe(onSuccess: { [weak self] monthTargetData in
+                self?.monthTargetData.onNext(monthTargetData)
+            }, onFailure: { error in
+                print("SelfCareUseCase getMonthTargetData error occured: \(error)")
+            }).disposed(by: disposeBag)
+    }
+    public func addTargetData(title: String, content: String, startDate: Date, endDate: Date) {
+        guard let token = TokenManagerImpl().get(key: .accessToken) else {
+            return
+        }
         repository.addTarget(accessToken: token, title: title, content: content, startDate: startDate, endDate: endDate)
-            .subscribe(onSuccess: { [weak self] data in
-                print("profileData: \(data)")
+            .subscribe(onSuccess: { data in
+                print("profileData Success: \(data)")
             }, onFailure: { error in
                 print("SelfCareUseCase addTarget error occured: \(error)")
             }).disposed(by: disposeBag)
@@ -146,7 +168,6 @@ extension DefaultSelfCareUseCase: SelfCareUseCase {
         }
         repository.getProfileData(accessToken: token, userName: nickName)
             .subscribe(onSuccess: { [weak self] profileData in
-                print("profileData: \(profileData)")
                 self?.profileData.onNext(profileData)
             }, onFailure: { error in
                 print("SelfCareUseCase getProfileData error occured: \(error)")
@@ -156,10 +177,10 @@ extension DefaultSelfCareUseCase: SelfCareUseCase {
     public func requestProfileModify(nickName: String, height: Double, weight: Double, gender: String) {
         guard let token = TokenManagerImpl().get(key: .accessToken) else { return }
         repository.requestProfileModify(accessToken: token, nickName: nickName, height: height, weight: weight, gender: gender)
-            .subscribe(onSuccess: { [weak self] _ in
-                print("success")
+            .subscribe(onSuccess: { [weak self] nickNameData in
+                self?.nickNameData.onNext(nickNameData)
             }, onFailure: { error in
-                    print("SelfCareUseCase getTargetDetailData error occured: \(error)")
+                print("SelfCareUseCase profileModify error occured: \(error)")
             }).disposed(by: disposeBag)
         }
 
