e0198abe638099a01fa8d4a8dab797ab8ae12cbc
Choyoungjun <cyj070513@gmail.com>
2024-06-17 08:56:21 +0900

    REFACT :: [#336] 텍스트 기본 컬러 추가

--- Projects/Modules/DSKit/Sources/Components/MaeumGaGymTextField/MGSelfCareTextField.swift
+++ Projects/Modules/DSKit/Sources/Components/MaeumGaGymTextField/MGSelfCareTextField.swift
@@ -52,6 +52,7 @@ open class MGSelfCareTextField: UITextField {
     }
 
     private func setup() {
+        self.typeLabel.textColor = .black
         self.autocapitalizationType = .none
         self.autocorrectionType = .no
         self.layer.borderColor = DSKitAsset.Colors.gray50.color.cgColor
@@ -86,6 +87,7 @@ extension MGSelfCareTextField {
     private func bind() {
         self.rx.controlEvent(.allEditingEvents)
             .subscribe(onNext: { [weak self] in
+                self?.typeLabel.textColor = .blue500
                 self?.layer.borderColor = DSKitAsset.Colors.blue100.color.cgColor
                 self?.backgroundColor = DSKitAsset.Colors.blue50.color
             }).disposed(by: disposeBag)
