8888abdf2e0b9538383a201b1024897525dafcc8
박준하 <goodjunha@gmail.com>
2024-06-26 11:39:34 +0900

    REFACT:: [#350] Auth - scenDelegate 리펙토링
    함수형으로 뺐습니다.

--- Projects/Features/AuthFeature/Demo/Sources/Application/SceneDelegate.swift
+++ Projects/Features/AuthFeature/Demo/Sources/Application/SceneDelegate.swift
@@ -17,44 +17,35 @@ import AuthFeatureInterface
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-        if let url = URLContexts.first?.url {
-            if (AuthApi.isKakaoTalkLoginUrl(url)) {
-                _ = AuthController.handleOpenUrl(url: url)
-            }
-        }
-    }
-
     var coordinator = FlowCoordinator()
     var mainFlow: AuthFlow!
 
-    func scene(_ scene: UIScene,
-               willConnectTo session: UISceneSession,
-               options connectionOptions: UIScene.ConnectionOptions) {
-        guard let scene = (scene as? UIWindowScene) else { return }
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            handleURL(url)
+        }
+    }
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = scene as? UIWindowScene else { return }
 
         window = UIWindow(frame: scene.coordinateSpace.bounds)
         window?.windowScene = scene
-
-        mainFlow = AuthFlow()
-
-        coordinator.coordinate(flow: mainFlow, with: OneStepper(withSingleStep: MGStep.authSplashIsRequired))
-        Flows.use(mainFlow, when: .created) { root in
-            self.window?.rootViewController = root
-            self.window?.makeKey()
-        }
+        configureMainFlow()
         window?.makeKeyAndVisible()
     }
 
-//    private func coordinatorLogStart() {
-//        coordinator.rx.willNavigate
-//            .subscribe(onNext: { flow, step in
-//                let currentFlow = "\(flow)".split(separator: ".").last ?? "no flow"
-//                print("➡️ will navigate to flow = \(currentFlow) and step = \(step)")
-//            })
-//            .disposed(by: disposeBag)
-//        
-//        // didNavigate
-//    }
+    private func handleURL(_ url: URL) {
+        if AuthApi.isKakaoTalkLoginUrl(url) {
+            _ = AuthController.handleOpenUrl(url: url)
+        }
+    }
+
+    private func configureMainFlow() {
+        mainFlow = AuthFlow()
+        coordinator.coordinate(flow: mainFlow, with: OneStepper(withSingleStep: MGStep.authSplashIsRequired))
+        Flows.use(mainFlow, when: .created) { [weak self] root in
+            self?.window?.rootViewController = root
+        }
+    }
 }
