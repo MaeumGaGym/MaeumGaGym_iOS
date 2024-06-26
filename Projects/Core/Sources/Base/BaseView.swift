2db9d0dc6b547180074f66c572731cc377e82ae0
박준하 <goodjunha@gmail.com>
2024-06-26 11:27:58 +0900

    DOC:: [#348] BaseView 수정

--- Projects/Core/Sources/Base/BaseView.swift
+++ Projects/Core/Sources/Base/BaseView.swift
@@ -4,6 +4,7 @@ import SnapKit
 import RxSwift
 import RxCocoa
 
+/// BaseView는 UIView를 확장하여 범용적으로 사용할 수 있는 기본 뷰 클래스입니다.
 open class BaseView: UIView {
     public let disposeBag = DisposeBag()
     
@@ -19,15 +20,12 @@ open class BaseView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    open func attribute() {
-        // 부가적인 것을 여기에 넣습니다.
-    }
+    /// 뷰의 기본 속성을 설정하는 함수입니다.
+    open func attribute() { }
     
-    open func layout() {
-        // 서브뷰를 구성하고 SnapKit을 사용해서 layout을 하는 함수입니다.
-    }
+    /// 이 함수에서는 각 서브뷰의 위치와 크기를 결정합니다.
+    open func layout() { }
 
-    open func bind() {
-        // 서브뷰를 구성하고 SnapKit을 사용해서 layout을 하는 함수입니다.
-    }
+    /// 이 함수에서는 RxCocoa를 활용하여 이벤트 처리 및 데이터 스트림을 관리합니다.
+    open func bind() { }
 }
