5bc0e3114614a13d60ee41f24782a489db57a782
박준하 <goodjunha@gmail.com>
2024-06-26 11:37:07 +0900

    DEL:: [#350] auth - appDelegate 삭제

--- Projects/Features/AuthFeature/Demo/Sources/Application/AppDelegate.swift
+++ Projects/Features/AuthFeature/Demo/Sources/Application/AppDelegate.swift
@@ -8,13 +8,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-//    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-//        if (AuthApi.isKakaoTalkLoginUrl(url)) {
-//            return AuthController.handleOpenUrl(url: url)
-//        }
-//        return false
-//    }
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         KakaoSDK.initSDK(appKey: "dcfcd3ab4a997c5a53e2ab26a8ec2a63")
         return true
