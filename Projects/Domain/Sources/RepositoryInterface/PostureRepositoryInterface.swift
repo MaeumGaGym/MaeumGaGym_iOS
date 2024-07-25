141f9d53b8874118a6889af2812bfe540bb7ff1f
s <tylereunholee010203@gmail.com>
2024-07-25 20:24:21 +0900

    FEAT :: [#365] 자세 - 부분별 검색 API 연동

--- Projects/Domain/Sources/RepositoryInterface/PostureRepositoryInterface.swift
+++ Projects/Domain/Sources/RepositoryInterface/PostureRepositoryInterface.swift
@@ -9,7 +9,7 @@ public enum PosturePartType {
 
 public protocol PostureRepositoryInterface {
     func getRecommandData(accessToken: String) -> Single<PoseRecommandModel>
-    func getPartData(type: PosturePartType) -> Single<PosturePartModel>
+    func getPartData(accessToken: String, category: String) -> Single<PosePartModel>
     func getDetailData(accessToken: String, id: Int) -> Single<PostureDetailModel>
     func getSearchData() -> Single<PostureSearchModel>
     func getAllPoseData(accessToken: String, lastUpdated: String) -> Single<PostureAllModel>
