acc02feadd5b1b4ea1a16f91c6cee053b111df98
s <tylereunholee010203@gmail.com>
2024-07-26 20:25:52 +0900

    FEAT :: [#369] 자기관리 - 메인 - 프로필 기능구현

--- Projects/Features/SelfCareFeature/Sources/HomeScene/VC/SelfCareHomeViewController.swift
+++ Projects/Features/SelfCareFeature/Sources/HomeScene/VC/SelfCareHomeViewController.swift
@@ -24,35 +24,38 @@ enum SelfCareCell {
 
 public class SelfCareHomeViewController: BaseViewController<SelfCareHomeViewModel>, Stepper {
     
-    private let clickProfileRelay = PublishRelay<Void>()
+    private let clickProfileRelay = PublishRelay<String>()
     private let clickRoutineRelay = PublishRelay<Void>()
     private let clickTargetRelay = PublishRelay<Void>()
     private let clickMealRelay = PublishRelay<Void>()
     private let clickTodyExeRelay = PublishRelay<Void>()
-
+    private let loadProfileRelay = PublishRelay<Void>()
+    
     private var cellList: [UITableViewCell] = []
     private var cells: [SelfCareCell] = []
-
+    
+    private var nickname: String = ""
+    
     var introducts = SelfCareIntroductModel(
         image: DSKitAsset.Assets.blueDumbelIcon.image,
         mainText: "자기관리",
         subText: "나만의 루틴과 목표를 설정하여\n자기관리에 도전해보세요."
     )
-
-    var profiles = SelfCareDetailProfileModel(
-        userImage: nil,
-        userName: "",
-        userWakaTime: 0,
-        userBageLevel: 0
+    
+    var profiles = SelfCareProfileInfoModel(
+        nickname: "",
+        profileImage: "",
+        level: 0,
+        wakaTime: 0
     )
-
+    
     var menus = [
         SelfCareMenuModel(menuImage: DSKitAsset.Assets.introIcon.image, menuName: "내 루틴"),
         SelfCareMenuModel(menuImage: DSKitAsset.Assets.goalIcon.image, menuName: "목표"),
         SelfCareMenuModel(menuImage: DSKitAsset.Assets.dietIcon.image, menuName: "식단"),
         SelfCareMenuModel(menuImage: DSKitAsset.Assets.albumIcon.image, menuName: "오운완")
     ]
-
+    
     private lazy var tableView: UITableView = UITableView().then {
         $0.delegate = self
         $0.dataSource = self
@@ -72,22 +75,31 @@ public class SelfCareHomeViewController: BaseViewController<SelfCareHomeViewMode
             forCellReuseIdentifier: SelfCareMenuTableViewCell.identifier
         )
     }
-
+    
     public override func configureNavigationBar() {
         super.configureNavigationBar()
-
+        
         navigationController?.isNavigationBarHidden = true
     }
-
+    
     public override func attribute() {
         super.attribute()
-
+        
         view.backgroundColor = DSKitAsset.Colors.gray25.color
         addCells()
     }
+    
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = false
+        
+        loadProfileRelay.accept(())
+
+    }
+    
     public override func bindViewModel() {
         let input = SelfCareHomeViewModel.Input(
-            loadProfile: Driver.just("조영준"),
+            loadProfile: loadProfileRelay.asDriver(onErrorJustReturn: ()),
             clickProfileButton: clickProfileRelay.asObservable(),
             clickRoutineButton: clickRoutineRelay.asObservable(),
             clickTargetButton: clickTargetRelay.asObservable(),
@@ -96,36 +108,35 @@ public class SelfCareHomeViewController: BaseViewController<SelfCareHomeViewMode
         )
         
         _ = viewModel.transform(input, action: { output in
-            print("프로필 데이터 : \(output.profileData)")
             output.profileData.asObservable()
                 .subscribe(onNext: { data in
-                    print("프로필 데이터 : \(data)")
                     self.profiles = data
+                    self.nickname = self.profiles.nickname
                     self.tableView.reloadData()
                 }).disposed(by: disposeBag)
         })
     }
     public override func layout() {
         view.addSubviews([tableView])
-
+        
         tableView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.top.equalToSuperview()
             $0.leading.trailing.equalToSuperview()
             $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
         }
     }
-
+    
     private func addCells() {
         cells.append(.introductMessage)
         cells.append(.profile)
         cells.append(.selfCare)
     }
-
+    
     func menuheightForCell(with menus: [SelfCareMenuModel]) -> CGFloat {
         let cellHeight: CGFloat = 64.0
         return max(CGFloat(menus.count) * cellHeight, cellHeight) + 92
     }
-
+    
 }
 
 extension SelfCareHomeViewController: UITableViewDelegate {
@@ -139,7 +150,7 @@ extension SelfCareHomeViewController: UITableViewDelegate {
             return menuheightForCell(with: menus)
         }
     }
-
+    
     public func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         switch cells[indexPath.item] {
         case .introductMessage, .profile, .selfCare:
@@ -152,11 +163,11 @@ extension SelfCareHomeViewController: UITableViewDataSource {
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cells.count
     }
-
+    
     public func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch cells[indexPath.item] {
         case .introductMessage:
@@ -167,7 +178,7 @@ extension SelfCareHomeViewController: UITableViewDataSource {
             }
             cell.configure(with: introducts)
             cell.isUserInteractionEnabled = false
-
+            
             return cell
         case .profile:
             guard let cell = tableView.dequeueReusableCell(
@@ -177,7 +188,7 @@ extension SelfCareHomeViewController: UITableViewDataSource {
             }
             cell.configure(with: profiles)
             cell.selectionStyle = .none
-
+            
             return cell
         case .selfCare:
             guard let cell = tableView.dequeueReusableCell(
@@ -201,38 +212,38 @@ extension SelfCareHomeViewController: UITableViewDataSource {
             }
             
             cell.selectionStyle = .none
-
+            
             return cell
         }
     }
-
+    
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        let useCase = DefaultSelfCareUseCase(repository: SelfCareRepository(networkService: DefaultSelfCareService()))
+        //        let useCase = DefaultSelfCareUseCase(repository: SelfCareRepository(networkService: DefaultSelfCareService()))
         
         switch cells[indexPath.row] {
         case .profile:
-            self.clickProfileRelay.accept(())
-//            let viewModel = SelfCareProfileViewModel(useCase: useCase)
-//            let viewController = SelfCareProfileViewController(viewModel)
-//            self.navigationController?.pushViewController(viewController, animated: true)
+            self.clickProfileRelay.accept(nickname)
+            //            let viewModel = SelfCareProfileViewModel(useCase: useCase)
+            //            let viewController = SelfCareProfileViewController(viewModel)
+            //            self.navigationController?.pushViewController(viewController, animated: true)
             return
-//        case .selfCare:
-//            self.rx.d
-//            MGStep.targetHomeRequired
-//            let viewModel = SelfCareTargetMainViewModel(useCase: useCase)
-//            self.navigationController?.pushViewController(SelfCareTargetMainViewController(viewModel), animated: true)
-//            switch menus[indexPath.row].menuImage {
-//            case .goalIcon:
-//                let useCase = DefaultSelfCareUseCase(repository: SelfCareRepository(networkService: SelfCareService()))
-//                let viewModel = SelfCareTargetMainViewModel(useCase: useCase)
-//                self.navigationController?.pushViewController(
-//                    SelfCareTargetMainViewController(viewModel)
-//                    , animated: true
-//                )
-//                return
-//            default:
-//                return
-//            }
+            //        case .selfCare:
+            //            self.rx.d
+            //            MGStep.targetHomeRequired
+            //            let viewModel = SelfCareTargetMainViewModel(useCase: useCase)
+            //            self.navigationController?.pushViewController(SelfCareTargetMainViewController(viewModel), animated: true)
+            //            switch menus[indexPath.row].menuImage {
+            //            case .goalIcon:
+            //                let useCase = DefaultSelfCareUseCase(repository: SelfCareRepository(networkService: SelfCareService()))
+            //                let viewModel = SelfCareTargetMainViewModel(useCase: useCase)
+            //                self.navigationController?.pushViewController(
+            //                    SelfCareTargetMainViewController(viewModel)
+            //                    , animated: true
+            //                )
+            //                return
+            //            default:
+            //                return
+            //            }
         default:
             return
         }
